305ebfa918703456a2aaae50056cbfb034201b30
MAIN6419 <dark9737@gmail.com>
2024-04-06 19:24:25 +0900

    Feat: _document meta 데이터 및 favicon 추가, global.css 스타일 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/styles/global.css";
 import "swiper/css";
 import "react-toastify/dist/ReactToastify.css";
 import "@/styles/toast.css";
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className={"flex flex-col min-h-screen pb-16 md:pb-0"}>
+    <Html lang="ko">
+      <Head title="아이템을-잇다 잇템.">
+        <meta
+          name="description"
+          content="중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요."
+          key="desc"
+        />
+        <link rel="icon" href="/icons/logo.svg" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
