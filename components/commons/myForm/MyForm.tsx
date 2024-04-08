72dccd8941d8a3f0c4f9ad8521c7f9380d76756a
MAIN6419 <dark9737@gmail.com>
2024-04-08 15:07:53 +0900

    Feat: react-hook-form를 이용하여 MyForm 컴포넌트 생성 - Form value를 하위에서 사용하기 위해 FormProvider 사용

---
+++ components/commons/myForm/MyForm.tsx
@@ -0,0 +1,46 @@
+import dynamic from "next/dynamic";
+import React from "react";
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  UseFormProps,
+  FieldValues,
+} from "react-hook-form";
+const DevTool = dynamic(
+  () =>
+    import("@hookform/devtools").then((module) => {
+      return module.DevTool;
+    }),
+  { ssr: false }
+);
+
+interface GenericFormInterface<TFormData extends FieldValues> {
+  children: React.ReactNode;
+  onSubmit: SubmitHandler<TFormData>;
+  formOptions?: UseFormProps<TFormData>;
+  className?: string;
+}
+
+export const MyForm = <TFormData extends FieldValues>({
+  children,
+  onSubmit,
+  formOptions,
+  className,
+}: GenericFormInterface<TFormData>) => {
+  const methods = useForm<TFormData>(formOptions);
+  return (
+    <>
+      <FormProvider {...methods}>
+        <form
+          className={className}
+          onSubmit={methods.handleSubmit(onSubmit)}
+          noValidate
+        >
+          {children}
+        </form>
+        <DevTool control={methods.control as any} />
+      </FormProvider>
+    </>
+  );
+};
