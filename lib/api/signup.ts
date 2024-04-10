c2543cc039dddb62f140205560501c23da682d27
MAIN6419 <dark9737@gmail.com>
2024-04-10 19:54:41 +0900

    Feat: 이메일 중복 검사, 닉네임 중복 검사 api 함수 및 type 생성

--- lib/api/signup.ts
+++ lib/api/signup.ts
@@ -1,4 +1,8 @@
-import { EmailVerifyData } from "@/types/apiTypes";
+import {
+  CheckEmailDuplicationData,
+  CheckNicknameDuplicationData,
+  EmailVerifyData,
+} from "@/types/apiTypes";
 import axios, { AxiosResponse } from "axios";
 
 export async function sendToVerifyEmail(
@@ -22,6 +26,32 @@ export async function verifyEmail(
     const response = await axios.post("/api/auth/verifyEmail", {
       email,
       verifyNumber,
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function checkEmailDuplication(
+  email: string
+): Promise<AxiosResponse<CheckEmailDuplicationData>> {
+  try {
+    const response = await axios.post("/api/auth/duplication/email", {
+      email,
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function checkNicknameDuplication(
+  nickname: string
+): Promise<AxiosResponse<CheckNicknameDuplicationData>> {
+  try {
+    const response = await axios.post("/api/auth/duplication/nickname", {
+      nickname,
     });
     return response;
   } catch (error) {
--- types/apiTypes.ts
+++ types/apiTypes.ts
@@ -1,5 +1,14 @@
 export interface EmailVerifyData {
   message: string;
   ok: boolean;
-};
+}
 
+export interface CheckEmailDuplicationData {
+  message: string;
+  ok: boolean;
+}
+
+export interface CheckNicknameDuplicationData {
+  message: string;
+  ok: boolean;
+}
