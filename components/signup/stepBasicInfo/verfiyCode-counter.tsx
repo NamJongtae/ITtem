5b9cc94ba32d6736f2628bd850334d9dcb866cf0
MAIN6419 <dark9737@gmail.com>
2024-04-13 17:25:08 +0900

    HTML&CSS : counter가 10 이하인 경우 counter 색상을 변경하도록 스타일 수정

--- components/signup/stepBasicInfo/verfiyCode-counter.tsx
+++ components/signup/stepBasicInfo/verfiyCode-counter.tsx
@@ -1,10 +1,14 @@
-import useVerifyEmailCounter from '@/hooks/useVerifyEmailCounter';
+import useVerifyEmailCounter from "@/hooks/useVerifyEmailCounter";
 import React from "react";
 
 export default function VerfiyCodeCounter() {
   const { counter } = useVerifyEmailCounter();
   return (
-    <span className="text-sm mr-2 text-gray-400">
+    <span
+      className={`${
+        counter <= 10 && "text-red-400"
+      } text-sm mr-2 text-gray-400`}
+    >
       {String(Math.floor(counter / 60)).padStart(2, "0")}:
       {String(counter % 60).padStart(2, "0")}
     </span>
