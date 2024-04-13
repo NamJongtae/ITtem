7698e36a587aed9c183d705941bff06ac0e68f0e
MAIN6419 <dark9737@gmail.com>
2024-04-13 18:14:21 +0900

    Feat: password-field CoreInputField 적용

--- components/signup/stepBasicInfo/password-field.tsx
+++ components/signup/stepBasicInfo/password-field.tsx
@@ -1,46 +1,23 @@
-import { PASSWORD_REGEX, PASSWORD_REGEX_ERRORMSG } from '@/constants/constant';
-import { useFormContext } from "react-hook-form";
+import CoreInputField from "@/components/commons/coreInputField/core-input-field";
+import { PASSWORD_REGEX, PASSWORD_REGEX_ERRORMSG } from "@/constants/constant";
 
 export default function PasswordField() {
-  const { register, formState, setError, clearErrors } = useFormContext();
-  const error = formState.errors["password"];
-
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!PASSWORD_REGEX.test(e.target.value)) {
-      setError("password", {
-        type: "onChange",
-        message: "",
-      });
-    } else {
-      clearErrors("password");
-    }
-  };
-
   return (
     <div>
-      <label className="sr-only" htmlFor="password">
-        비밀번호
-      </label>
-      <input
-        className="root_input"
-        type="password"
-        placeholder="비밀번호를 입력하세요."
-        minLength={8}
-        maxLength={16}
-        {...register("password", {
-          required: "비밀번호를 입력하세요.",
-          onChange: handleChangePassword,
-          pattern: {
-            value: PASSWORD_REGEX,
-            message: PASSWORD_REGEX_ERRORMSG,
-          },
-        })}
+      <CoreInputField
+        label="비밀번호"
+        inputId="password"
+        inputName="password"
+        inputType="password"
+        inputPlaceholder="비밀번호를 입력하세요."
+        inputMinLength={8}
+        inputMaxLength={16}
+        inputRequired={"비밀번호를 입력하세요."}
+        inputPattern={{
+          value: PASSWORD_REGEX,
+          message: PASSWORD_REGEX_ERRORMSG,
+        }}
       />
-      {error?.message && (
-        <p className="text-xs ml-1 mt-1 text-red-400">
-          {typeof error.message === "string" ? error.message : ""}
-        </p>
-      )}
     </div>
   );
 }
