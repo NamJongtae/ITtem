677e93144e82b14c4ff8281ec27a8313cc964bd4
MAIN6419 <dark9737@gmail.com>
2024-04-13 17:37:54 +0900

    Chore: 이메일 인증 시간, 이메일 인증 초과 시간, 이메일 인증 완료 시간 상수로 선언

--- constants/constant.ts
+++ constants/constant.ts
@@ -24,12 +24,16 @@ export const EMAIL_REGEX_ERRORMSG = "이메일 형식을 확인해주세요.";
 export const PASSWORD_REGEX =
   /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
 export const PASSWORD_REGEX_ERRORMSG =
-  "8-16자 특수문자, 숫자, 영문을 포함해야합니다.";
+  "8-16자 특수문자, 숫자, 영문을 포함해야해요.";
 
 export const NICKNAME_REGEX = /^(?![0-9]+$)([a-zA-Z0-9가-힣]{4,8})$/;
 export const NICKNAME_REGEX_ERRORMSG =
-  "4-8자 닉네임(영문, 한글, 영문+숫자)을 입력해주세요.";
+  "4-8자 닉네임(영문, 한글, 숫자조합)을 입력해주세요.";
 
 export const VERIFYCODE_REGEX = /^[A-Za-z0-9]{6}$/;
 export const VERIFYCODE_REGEX_ERRORMSG =
-  "인증코드는 영문, 숫자, 영문+숫자 6자리입니다.";
+  "인증코드는 영문, 숫자, 영문+숫자 6자리이에요.";
+
+export const VERIFY_EMAIL_BLOCK_EXP = 60 * 60 * 24;
+export const VERIFY_EMAIL_EXP = 60 * 5;
+export const VERIFIED_EMAIL_EXP = 60 * 30;
