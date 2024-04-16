c080fa89cf1f23e83ae58422b139386c64aab490
MAIN6419 <dark9737@gmail.com>
2024-04-16 16:32:26 +0900

    Feat: react-query 및 redux SSR를 위한 hydrete 적용, Head 추가 - react-query hydrate를 위해 HydrationBoundary 추가 - redux hydrate를 위해 wrapper.withRedux HOC 적용 - SEO 최적화 및 페이지별 Head 설정을 위해 _document 컴포넌트에 있는 Head meta 태그 _app 컴포넌트로 이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,50 +4,72 @@ import "react-toastify/dist/ReactToastify.css";
 import "@/styles/toast.css";
 import type { AppProps } from "next/app";
 import { Noto_Sans_KR } from "next/font/google";
-import StoreProvider from "@/store/StoreProvider";
 import ReactQueryProvider from "@/store/ReactQueryProvider";
 import Layout from "@/components/commons/layout/layout";
 import { ToastContainer } from "react-toastify";
 import { usePathname } from "next/navigation";
+import Head from "next/head";
+import {
+  HydrationBoundary,
+} from "@tanstack/react-query";
+import wrapper from "@/store/store";
+
 
 const inter = Noto_Sans_KR({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   const pathanme = usePathname();
 
   return (
-    <StoreProvider>
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요."
+          key="desc"
+        />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1"
+        />
+        <link rel="icon" href="/icons/logo.svg" />
+      </Head>
       <ReactQueryProvider>
-        <style jsx global>{`
-          html {
-            font-family: ${inter.style.fontFamily};
-          }
-          #__next {
-            @apply ${!pathanme.includes("/signup") || !pathanme.includes
-            ("/signin") && "pb-16 md:pb-0"}
-          }
-        `}</style>
-        <Layout>
-          <main className={"flex-grow mt-[113px] md:mt-[127px]"}>
-            <Component {...pageProps} />
-          </main>
-          <ToastContainer
-            position="top-center"
-            limit={1}
-            closeOnClick={true}
-            closeButton={true}
-            pauseOnHover={false}
-            draggable={true}
-            autoClose={2000}
-            pauseOnFocusLoss={false}
-            theme="light"
-            hideProgressBar={true}
-          />
-        </Layout>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <style jsx global>{`
+            html {
+              font-family: ${inter.style.fontFamily};
+            }
+            #__next {
+              @apply ${!pathanme.includes("/signup") ||
+              (!pathanme.includes("/signin") && "pb-16 md:pb-0")};
+            }
+          `}</style>
+          <Layout>
+            <main className={"flex-grow mt-[113px] md:mt-[127px]"}>
+              <Component {...pageProps} />
+            </main>
+            <ToastContainer
+              position="top-center"
+              limit={1}
+              closeOnClick={true}
+              closeButton={true}
+              pauseOnHover={false}
+              draggable={true}
+              autoClose={2000}
+              pauseOnFocusLoss={false}
+              theme="light"
+              hideProgressBar={true}
+            />
+          </Layout>
+        </HydrationBoundary>
       </ReactQueryProvider>
-    </StoreProvider>
+    </>
   );
 }
+
+export default wrapper.withRedux(App);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,19 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head title="아이템을-잇다 잇템.">
-        <meta
-          name="description"
-          content="중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요."
-          key="desc"
-        />
-
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1"
-        />
-        <link rel="icon" href="/icons/logo.svg" />
-      </Head>
+      <Head title="아이템을-잇다 잇템."/>
       <body>
         <Main />
         <div id="portal-root"></div>
