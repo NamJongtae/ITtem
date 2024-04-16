2ccba8e3de22aec5bc658f75150e42003da3f0bf
MAIN6419 <dark9737@gmail.com>
2024-04-16 21:08:59 +0900

    Feat: layout 컴포넌트에 useAuth customhook 적용 - 페이지 마다 로그인 관리를 위한 layout 컴포넌트에 useAuth customhook 적용

--- components/commons/layout/layout.tsx
+++ components/commons/layout/layout.tsx
@@ -1,12 +1,14 @@
-import React from "react";
 import Header from "./layout-header";
 import Footer from "./layout-footer";
 import MobileNav from "./nav/layout-mobile-nav";
+import useAuth from '@/hooks/useAuth';
 
 interface IProps {
   children: React.ReactNode;
 }
 export default function Layout({ children }: IProps) {
+  useAuth();
+  
   return (
     <>
       <Header />
