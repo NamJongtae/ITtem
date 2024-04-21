0eae6c62a6f4f4a2178fe6c5119d69b9652f008b
MAIN6419 <dark9737@gmail.com>
2024-04-21 17:55:04 +0900

    Fix: signupMutate socialType 추가

--- hooks/useSignup.ts
+++ hooks/useSignup.ts
@@ -1,10 +1,12 @@
 import { FieldValues } from "react-hook-form";
 import useSignupMutate from "./querys/useSignupMutate";
+import { SocialType } from '@/types/apiTypes';
 
 export default function useSignup() {
   const { signupMutate, signupLoading } = useSignupMutate();
   const onSubmit = async (data: FieldValues) => {
     signupMutate({
+      socialType: SocialType.EMAIL,
       email: data.email,
       password: data.password,
       nickname: data.nickname,
