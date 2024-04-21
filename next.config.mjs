101f53605b3cdd6e8e763cee0350b8b9dd7335bc
MAIN6419 <dark9737@gmail.com>
2024-04-21 17:59:35 +0900

    Chore: images 설정 수정 - svg 이미지를 받아 올 수 있도록 설정 추가 firestore 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: config => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack"],
     });
     return config;
   },
   images: {
-    domains: ["images.unsplash.com", "lh3.googleusercontent.com"],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: [
+      "images.unsplash.com",
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+    ],
   },
 };
 
