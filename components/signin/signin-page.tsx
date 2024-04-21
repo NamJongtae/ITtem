4ded30f3b933714a1753bd37654ddb06f32e1cd6
MAIN6419 <dark9737@gmail.com>
2024-04-21 15:07:10 +0900

    Feat: google 로그인 구현을 위해 GoogleOAuthProvider 적용

--- components/signin/signin-page.tsx
+++ components/signin/signin-page.tsx
@@ -1,10 +1,15 @@
 import SigninForm from "./signin-form";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function SigninPage() {
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string;
+
   return (
-    <div className="absolute inset-0 border p-5  pt-[112px]  bg-gray-100">
-      <h2 className="sr-only">로그인</h2>
-      <SigninForm />
-    </div>
+    <GoogleOAuthProvider clientId={clientId}>
+      <div className="absolute inset-0 border p-5  pt-[112px]  bg-gray-100">
+        <h2 className="sr-only">로그인</h2>
+        <SigninForm />
+      </div>
+    </GoogleOAuthProvider>
   );
 }
