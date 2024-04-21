c589ab6dccc8176f275008ce0b55fbd582f9b144
MAIN6419 <dark9737@gmail.com>
2024-04-21 17:22:42 +0900

    Feat: 구글 소셜 로그인 로직 추가

--- components/signin/signin-social-loginBtns.tsx
+++ components/signin/signin-social-loginBtns.tsx
@@ -1,9 +1,36 @@
-import Image from 'next/image';
+import { useGoogleLogin } from "@react-oauth/google";
+import Image from "next/image";
+import Loading from "../commons/loading";
+import useSocialLoginMutate from '@/hooks/querys/useSocialLoginMutate';
+import { SocialType } from '@/types/apiTypes';
+import { toast } from 'react-toastify';
 
 export default function SigninSocialLoginBtns() {
+  const { socialLoginMutate, socialLoginLoading } = useSocialLoginMutate();
+  const handleClickGoogleLogin = useGoogleLogin({
+    onSuccess: async (credentialResponse) => {
+      const accessToken = credentialResponse.access_token;
+
+      socialLoginMutate({
+        socialType: SocialType.GOOGLE,
+        accessToken,
+      });
+    },
+    onError: () => {
+      toast.warn("로그인 중 에러가 발생하였습니다.\n잠시 후 다시 시도해주세요.")
+    },
+  });
+
+  if (socialLoginLoading) {
+    return <Loading />;
+  }
+
   return (
     <div className="relative flex flex-col gap-3">
-      <button className="relative mt-5 text-sm bg-[#FEE500] py-3 rounded-md">
+      <button
+        type="button"
+        className="relative mt-5 text-sm bg-[#FEE500] py-3 rounded-md"
+      >
         <Image
           className="absolute left-5 top-1/2 -translate-y-1/2"
           src="/icons/kakao_icon.svg"
@@ -18,7 +45,11 @@ export default function SigninSocialLoginBtns() {
         role="line"
         data-before="3초만에 시작하기"
       />
-      <button className="relative mb-5 text-sm bg-white py-3 rounded-md">
+      <button
+        type="button"
+        onClick={() => handleClickGoogleLogin()}
+        className="relative mb-5 text-sm bg-white py-3 rounded-md"
+      >
         <Image
           className="absolute left-5 top-1/2 -translate-y-1/2"
           src="/icons/google_icon.svg"
