725d35e53473a9de3411420677855dd27756fa84
MAIN6419 <dark9737@gmail.com>
2024-04-22 18:07:37 +0900

    Chore: 카카오 로그인 관련 타입 생성

--- types/apiTypes.ts
+++ types/apiTypes.ts
@@ -108,3 +108,23 @@ export const enum SocialType {
   GOOGLE = "GOOGLE",
   KAKAO = "KAKAO",
 }
+
+export interface KaKaoAuthAccessTokenResponseData {
+  token_type: string;
+  access_token: string;
+  refresh_token: string;
+  id_token: string;
+  expires_in: number;
+  refresh_token_expires_in: string;
+  scope: string;
+}
+
+export interface KakaoAuthInfoResponseData {
+  id: number;
+  connected_at: string;
+  properties: {
+    nickname: string;
+    profile_image?: string; // 640x640
+    thumbnail_image?: string; // 110x110
+  };
+}
