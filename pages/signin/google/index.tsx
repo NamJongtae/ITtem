99708e035d1665381072959d3140778471bd4ed1
MAIN6419 <dark9737@gmail.com>
2024-04-22 17:51:38 +0900

    Feat: 구글 로그인 로직 적용

--- pages/signin/google/index.tsx
+++ pages/signin/google/index.tsx
@@ -1,14 +1,22 @@
 import Loading from "@/components/commons/loading";
+import useGoogleSigninMutate from "@/hooks/querys/useGoogleSigninMutate";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from 'react';
+import { useCallback, useEffect } from "react";
 
 export default function GoogleAuth() {
   const params = useSearchParams();
   const code = params.get("code");
+  const { googleSigninMutate } = useGoogleSigninMutate();
 
-  useEffect(()=>{
-    console.log(code);
-  },[code])
+  const fetchLogin = useCallback(async () => {
+    if (code) {
+      googleSigninMutate(code);
+    }
+  }, [code, googleSigninMutate]);
+
+  useEffect(() => {
+    fetchLogin();
+  }, [code, fetchLogin]);
 
   return <Loading />;
 }
