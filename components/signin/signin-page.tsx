a12b6bc313054526dbe5d54e9495437e583f3efc
MAIN6419 <dark9737@gmail.com>
2024-04-22 17:46:50 +0900

    Fix: react-oauth/google 제거 - react-oauth/google 라이브러리를 사용하지 않고 리다이렉트 방식으로 구현

--- components/signin/signin-page.tsx
+++ components/signin/signin-page.tsx
@@ -1,15 +1,10 @@
 import SigninForm from "./signin-form";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function SigninPage() {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string;
-
   return (
-    <GoogleOAuthProvider clientId={clientId}>
       <div className="absolute inset-0 border p-5  pt-[112px]  bg-gray-100">
         <h2 className="sr-only">로그인</h2>
         <SigninForm />
       </div>
-    </GoogleOAuthProvider>
   );
 }
