dcd3fbcf56ccf7a9d2740cc86193e09aa7f4d99b
MAIN6419 <dark9737@gmail.com>
2024-04-23 17:22:05 +0900

    Feat: 중복로그인 로직 추가 - 중복로그인 에러 발생 시 사용자에게 confirm 창을 통해 중복로그인 여부 확인 - 중복 로그인시 기존 로그인된 계정 로그아웃을 위해 기존 토큰 삭제 - 이후 새로운 코드 발급을 위해 리다이렉트 시킴

--- hooks/querys/useGoogleSigninMutate.ts
+++ hooks/querys/useGoogleSigninMutate.ts
@@ -1,4 +1,4 @@
-import { googleSignin } from "@/lib/api/auth";
+import { deleteAllToken, googleSignin } from "@/lib/api/auth";
 import { authSlice } from "@/store/authSlice";
 import { AppDispatch } from "@/store/store";
 import {
@@ -25,10 +25,27 @@ export default function useGoogleSigninMutate() {
       dispatch(authSlice.actions.saveAuth(response.data.user));
       router.push("/");
     },
-    onError: (error) => {
-      if (isAxiosError<{ message: string }>(error)) {
-        toast.warn("로그인에 실패했어요.");
-        router.push("/signin");
+    onError: (error, variables) => {
+      if (isAxiosError<{ message: string; email: string }>(error)) {
+        if (error.response?.status == 409) {
+          const isLogin = confirm(
+            "제대로 로그아웃 하지 않았거나\n이미 로그인중인 아이디입니다.\n현재 사용중인 브라우저에서 로그아웃 하겠습니까?"
+          );
+          if (isLogin) {
+            const user = variables;
+            // 토큰 삭제 로직
+            (async () => await deleteAllToken(user.email))();
+
+            router.push(
+              `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email profile`
+            );
+          } else {
+            router.push("/signin");
+          }
+        } else {
+          toast.warn("로그인에 실패했어요.");
+          router.push("/signin");
+        }
       }
     },
   });
