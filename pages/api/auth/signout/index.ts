968a4c661d7ad66b08793972492c4a0e5e18d7fc
MAIN6419 <dark9737@gmail.com>
2024-04-23 22:13:53 +0900

    Feat: 카카오 계정일 시 추가적인 로그아웃이 필요하여 다른 메세지를 출력 - 카카오 계정을 완전히 로그아웃 하기 위해서는 쿠키 삭제 필요 - 쿠키 삭제를 위해서는 별도의 카카오 로그아웃 페이지로 이동이 필요함 - 이를 에러 메세지를 통해 구분하여 클라이언트에서 처리

--- pages/api/auth/signout/index.ts
+++ pages/api/auth/signout/index.ts
@@ -1,8 +1,10 @@
 import { ACCESS_TOKEN_KEY } from "@/constants/constant";
 import { deleteToken } from "@/lib/api/redis";
+import { DBClient } from "@/lib/database";
 import { sessionOptions } from "@/lib/server";
 import { verifyToken } from "@/lib/token";
-import { IronSessionType } from '@/types/apiTypes';
+import { IronSessionType, UserData } from "@/types/apiTypes";
+import axios from "axios";
 import { getIronSession } from "iron-session";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -12,7 +14,11 @@ export default async function handler(
 ) {
   if (req.method === "GET") {
     try {
-      const session = await getIronSession<IronSessionType>(req, res, sessionOptions);
+      const session = await getIronSession<IronSessionType>(
+        req,
+        res,
+        sessionOptions
+      );
       const accessToken = session.accessToken;
       const decodeAccessToken = verifyToken(accessToken, ACCESS_TOKEN_KEY);
 
@@ -22,10 +28,26 @@ export default async function handler(
         "refreshToken"
       );
       session.destroy();
+
+      await DBClient.connect();
+      const db = DBClient.db("auth");
+      const user = (await db.collection("user").findOne({
+        uid: decodeAccessToken?.data?.user.uid || "",
+      })) as UserData | null;
+
+      if (user?.socialType === "KAKAO") {
+        res
+          .status(200)
+          .json({ message: "카카오 계정은 별도의 로그아웃이 필요해요."});
+        return;
+      }
+
       res.status(200).json({ message: "로그아웃에 성공했어요." });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "로그아웃에 실패했어요." });
+    } finally {
+      DBClient.close();
     }
   }
 }
