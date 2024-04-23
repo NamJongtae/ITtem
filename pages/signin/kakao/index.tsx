37c009e5c4bef96abff0a8b860bddbff85335152
MAIN6419 <dark9737@gmail.com>
2024-04-23 18:08:29 +0900

    Fix: 분리된 카카오 로그인 로직을 적용 - code를 이용해 유저 정보를 받아오고, 유저 정보를 이용해 로그인하도록 수정

--- pages/signin/kakao/index.tsx
+++ pages/signin/kakao/index.tsx
@@ -1,22 +1,26 @@
 import Loading from "@/components/commons/loading";
 import useKakaoSigninMutate from "@/hooks/querys/useKakaoSigninMutate";
+import useKakaoUserInfoMutate from "@/hooks/querys/useKakaoUserInfoMutate";
 import { useSearchParams } from "next/navigation";
-import React, { useCallback, useEffect } from "react";
+import React, { useEffect } from "react";
 
 export default function KakaoAuth() {
   const params = useSearchParams();
   const code = params.get("code");
   const { kakaoSigninMutate } = useKakaoSigninMutate();
-
-  const fetchLogin = useCallback(async () => {
-    if (code) {
-      kakaoSigninMutate(code);
-    }
-  }, [code, kakaoSigninMutate]);
+  const { kakaoUserInfoMutate, user } = useKakaoUserInfoMutate();
 
   useEffect(() => {
-    fetchLogin();
-  }, [code, fetchLogin]);
+    if (code) {
+      kakaoUserInfoMutate(code);
+    }
+  }, [code, kakaoUserInfoMutate]);
+
+  useEffect(() => {
+    if (user) {
+      kakaoSigninMutate(user);
+    }
+  }, [user, kakaoSigninMutate]);
 
   return <Loading />;
 }
