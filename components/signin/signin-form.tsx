8ee1a15352739f49f9757b3fdac76ff20d776e45
MAIN6419 <dark9737@gmail.com>
2024-04-25 21:03:13 +0900

    HTML&CSS: 로그인 페이지 스타일 수정 - 모바일 여부 상관없이 가운데 정렬 되도록 수정

--- components/signin/signin-form.tsx
+++ components/signin/signin-form.tsx
@@ -1,19 +1,11 @@
 import { MyForm } from "../commons/myForm/MyForm";
 import { FieldValues } from "react-hook-form";
 import SigninFormContent from "./signin-form-content";
-import { isMobile } from "react-device-detect";
-import { useEffect, useState } from "react";
-import useSigninMutate from '@/hooks/querys/useSigninMutate';
+import useSigninMutate from "@/hooks/querys/useSigninMutate";
 import Loading from "../commons/loading";
 
-
 export default function SigninForm() {
-  const [mobile, setMobile] = useState(false);
   const { signinMutate, signinLoading } = useSigninMutate();
-
-  useEffect(() => {
-    setMobile(isMobile);
-  }, []);
 
   const onSubmit = async (data: FieldValues) => {
     const email = data.email;
@@ -35,9 +27,9 @@ export default function SigninForm() {
           password: "",
         },
       }}
-      className={`${
-        mobile ? "pt-8 left-1/2 -translate-x-1/2" : "center"
-      } w-full px-4 max-w-96 fixed flex flex-col`}
+      className={
+        "center w-full px-4 max-w-96 fixed flex flex-col pt-[113px] md:pt-[127px]"
+      }
     >
       <SigninFormContent />
     </MyForm>
--- components/signin/signin-page.tsx
+++ components/signin/signin-page.tsx
@@ -2,9 +2,9 @@ import SigninForm from "./signin-form";
 
 export default function SigninPage() {
   return (
-      <div className="absolute inset-0 border p-5  pt-[112px]  bg-gray-100">
-        <h2 className="sr-only">로그인</h2>
-        <SigninForm />
-      </div>
+    <div className="w-full h-[calc(100vh-113px)] bg-gray-100 overflow-hidden fixed">
+      <h2 className="sr-only">로그인</h2>
+      <SigninForm />
+    </div>
   );
 }
