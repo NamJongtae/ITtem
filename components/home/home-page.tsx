c1f64f2a926e7154dbf7f4a7ea22444b4ffb42d1
MAIN6419 <dark9737@gmail.com>
2024-04-26 16:51:05 +0900

    Feat: 홈 페이지 배너 구현 - swiper 라이브러리를 통해 홈페이지 배너 구현

---
+++ components/home/home-page.tsx
@@ -0,0 +1,10 @@
+import HomeBanner from "./home-banner";
+
+export default function HomePage() {
+  return (
+    <>
+      <h2 className='sr-only'>홈 페이지</h2>
+      <HomeBanner />
+    </>
+  );
+}
