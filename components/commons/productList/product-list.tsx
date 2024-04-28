e03cc55f11c625489286ee416c05de68a7ef9682
MAIN6419 <dark9737@gmail.com>
2024-04-28 18:38:52 +0900

    Feat: ProductListType를 통하여 다른 데이터를 받아올 수 있도록 추가 구현 - ProductListType이 TODAY일 때 오늘의 상품 목록을 받아오도록 함 - ProdcutListType이 CATEGORY일 때 카테고리 상품 목록을 받아오도록 함

--- components/commons/productList/product-list.tsx
+++ components/commons/productList/product-list.tsx
@@ -4,31 +4,87 @@ import ProductListPlaceholder from "./product-list-placeholder";
 import useProductTodayListInfiniteQuery from "@/hooks/querys/useProductTodayListInfiniteQuery";
 import InfiniteScroll from "react-infinite-scroller";
 import ProductListEmpty from "./product-list-empty";
+import useCategoryProductListInfiniteQuery from "@/hooks/querys/useCategoryProductListInfiniteQuery";
+import { useSearchParams } from "next/navigation";
+import { ProductCategory, ProductListType } from "@/types/productTypes";
 
-export default function ProductList() {
+interface IProps {
+  productListType: ProductListType;
+}
+
+export default function ProductList({ productListType }: IProps) {
+  const search = useSearchParams();
+  const category = (search.get("category") as ProductCategory) || null;
   const {
-    data,
-    hasNextPage,
-    fetchNextPage,
-    isFetchingNextPage,
-    isLoading,
-    isError,
-  } = useProductTodayListInfiniteQuery();
+    todayProductListData,
+    hasNextPageTodayProductList,
+    fetchNextPageTodayProductList,
+    isFetchingNextPageTodayProductList,
+    isLoadingTodayProductList,
+    isErrorTodayProductList,
+  } = useProductTodayListInfiniteQuery({ productListType });
+
+  const {
+    categoryProductListData,
+    hasNextPageCategoryProductList,
+    fetchNextPageCategoryProductList,
+    isFetchingNextPageCategoryProductList,
+    isLoadingCategoryProductList,
+    isErrorCategoryProductList,
+  } = useCategoryProductListInfiniteQuery({
+    category: category || undefined,
+    productListType,
+  });
+
+  const isLoading =
+    productListType === "TODAY"
+      ? isLoadingTodayProductList
+      : isLoadingCategoryProductList;
+  const data =
+    productListType === "TODAY"
+      ? todayProductListData
+      : categoryProductListData;
+  const fetchNextPage =
+    productListType === "TODAY"
+      ? fetchNextPageTodayProductList
+      : fetchNextPageCategoryProductList;
+  const isFetchingNextPage =
+    productListType === "TODAY"
+      ? isFetchingNextPageTodayProductList
+      : isFetchingNextPageCategoryProductList;
+  const hasNextPage =
+    productListType === "TODAY"
+      ? hasNextPageTodayProductList
+      : hasNextPageCategoryProductList;
+  const isError =
+    productListType === "TODAY"
+      ? isErrorTodayProductList
+      : isErrorCategoryProductList;
 
   return (
     <>
       {data?.length === 0 && !isLoading && (
-        <ProductListEmpty message={"오늘의 상품이 존재하지 않아요."} />
+        <ProductListEmpty
+          message={`${
+            productListType === "TODAY" ? "오늘의" : ""
+          } 상품이 존재하지 않아요.`}
+        />
       )}
 
-      {isError && <ProductListEmpty message={"오늘의 상품을 불러올 수 없어요.\n잠시 후 다시 시도해주세요."} />}
+      {isError && (
+        <ProductListEmpty
+          message={`${
+            productListType === "TODAY" ? "오늘의" : ""
+          } 상품을 불러올 수 없어요.\n잠시 후 다시 시도해주세요.`}
+        />
+      )}
+
       <InfiniteScroll
         hasMore={hasNextPage}
         loadMore={() => {
           if (!isFetchingNextPage) fetchNextPage();
         }}
       >
-        
         <div className="max-w-[1024px] mx-auto pb-12">
           <ul className="grid gap-5 grid-cols-autoFill mt-6 px-8 ">
             {isLoading ? (
