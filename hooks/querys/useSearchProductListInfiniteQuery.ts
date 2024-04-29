02bd79ae753fb76bf3fcfc55ec06f3ddc27c92b2
MAIN6419 <dark9737@gmail.com>
2024-04-29 19:40:32 +0900

    Feat: 검색 상품 목록 무한스크롤 infiniteQuery customhook 생성

---
+++ hooks/querys/useSearchProductListInfiniteQuery.ts
@@ -0,0 +1,59 @@
+import { getSearchProductListQueryKey } from "@/constants/constant";
+import { getSearchProductList } from "@/lib/api/product";
+import {
+  ProductCategory,
+  ProductData,
+  ProductListType,
+} from "@/types/productTypes";
+import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useSearchParams } from "next/navigation";
+
+export default function useSearchProductListInfiniteQuery({
+  limit = 10,
+  category = ProductCategory.전체,
+  productListType,
+}: {
+  limit?: number;
+  category?: ProductCategory;
+  productListType: ProductListType;
+}) {
+  const search = useSearchParams();
+  const keyword = search.get("keyword");
+
+  const {
+    data: searchProductListData,
+    hasNextPage: hasNextPageSearchProductList,
+    fetchNextPage: fetchNextPageSearchProductList,
+    isFetchingNextPage: isFetchingNextPageSearchProductList,
+    isLoading: isLoadingSearchProductList,
+    isError: isErrorSearchProductList,
+  } = useInfiniteQuery<ProductData[], AxiosError, InfiniteData<ProductData>>({
+    queryKey: getSearchProductListQueryKey(category, keyword),
+    queryFn: async ({ pageParam = 1 }) => {
+      const response = await getSearchProductList({
+        category,
+        page: pageParam,
+        limit,
+        keyword: keyword || "",
+      });
+      return response.data.product;
+    },
+    enabled: productListType === "SEARCH" && !!keyword,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allpages) => {
+      return lastPage.length === limit
+        ? allpages.flat().length / limit + 1
+        : undefined;
+    },
+  });
+
+  return {
+    searchProductListData: searchProductListData?.pages.flat(),
+    hasNextPageSearchProductList,
+    fetchNextPageSearchProductList,
+    isFetchingNextPageSearchProductList,
+    isLoadingSearchProductList,
+    isErrorSearchProductList,
+  };
+}
