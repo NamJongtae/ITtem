f1cedfeb8e3129f1ddc150cf04d5e93261e6631c
MAIN6419 <dark9737@gmail.com>
2024-04-29 18:02:22 +0900

    Feat: 현재 위치 검색 체크박스 컴포넌트 생성 및 상품 페이지 헤더에 추가

--- components/product/product-page-header.tsx
+++ components/product/product-page-header.tsx
@@ -1,13 +1,15 @@
 import CategoryNav from "../commons/category/category-nav";
 import CategoryMenu from "../commons/category/category-menu";
+import LocationCheckbox from './search-location-checkbox';
 
 export default function ProductPageHeader() {
   return (
     <div className="container flex flex-col gap-5 items-center mx-auto px-8 mt-8 max-w-[1024px]">
-      <h2 className="font-bold text-xl sm:text-2xl text-center">중고거래</h2>
+      <h2 className="sr-only">중고거래</h2>
 
       <div className="flex gap-5 justify-between w-full items-center">
         <CategoryNav className="ml-2" />
+        <LocationCheckbox />
       </div>
       <CategoryMenu />
     </div>
