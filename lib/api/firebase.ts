76cf1890d24b481319055c0d6d8c0ad7c510e0a7
MAIN6419 <dark9737@gmail.com>
2024-05-01 16:21:46 +0900

    Feat: fireStore 이미지 다중 업로드 api 함수 생성 및 수정된 UploadImgResponseData 타입 적용

--- lib/api/firebase.ts
+++ lib/api/firebase.ts
@@ -1,7 +1,7 @@
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../firebaseSetting";
 import { v4 as uuid } from "uuid";
-import { UploadImgResponseData } from '@/types/apiTypes';
+import { UploadImgResponseData } from "@/types/apiTypes";
 
 export const uploadImgToFireStore = async (
   file: File | ""
@@ -12,7 +12,29 @@ export const uploadImgToFireStore = async (
       ref(storage, `images/profile/${fileName}`),
       file
     );
-    const imgUrl = uploadImgResponse && (await getDownloadURL(uploadImgResponse.ref));
-    if (imgUrl) return { imgUrl, fileName };
+    const url =
+      uploadImgResponse && (await getDownloadURL(uploadImgResponse.ref));
+    if (url) return { url, name: fileName };
   }
+};
+
+export const uploadMultiImgToFirestore = async (
+  files: File[]
+): Promise<UploadImgResponseData[] | undefined> => {
+  if (!files) return;
+  console.log(files);
+  const uploadPromises = [...files].map(async (file) => {
+    // 각 파일에 대해 비동기 업로드 처리
+    const fileName = `${uuid()}_${file.name}`;
+    const uploadImgResponse = await uploadBytes(
+      ref(storage, `images/${fileName}`),
+      file
+    );
+    const url = await getDownloadURL(uploadImgResponse.ref);
+    return { url, name: fileName } as UploadImgResponseData;
+  });
+
+  // 모든 파일이 업로드되길 기다립니다.
+  const uploadResults = await Promise.all(uploadPromises);
+  return uploadResults;
 };
