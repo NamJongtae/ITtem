d972552211752840b5dc6bfcdcca076adb336898
MAIN6419 <dark9737@gmail.com>
2024-05-01 17:54:28 +0900

    Feat: ProdcutList의 Error UI 분리를 위해 ProductListError 컴포넌트 생성

---
+++ components/commons/productList/product-list-error.tsx
@@ -0,0 +1,36 @@
+import { ProductListType } from "@/types/productTypes";
+import ProductListEmpty from "./product-list-empty";
+import { AxiosError } from "axios";
+import { useSearchParams } from 'next/navigation';
+
+interface IProps {
+  productListType: ProductListType;
+  error: AxiosError<unknown, any>;
+}
+
+export default function ProductListError({ productListType, error }: IProps) {
+  const search = useSearchParams();
+  const keyword = search.get("keyword");
+
+  return error.response?.status === 404 ? (
+    <ProductListEmpty
+      message={`${
+        productListType === "TODAY"
+          ? "오늘의 상품이 존재하지 않아요."
+          : productListType === "CATEGORY"
+          ? "상품이 존재하지 않아요."
+          : `${'"' + keyword + '"'}에 대한 검색결과가 없어요.`
+      }`}
+    />
+  ) : (
+    <ProductListEmpty
+      message={`${
+        productListType === "TODAY"
+          ? "오늘의 상품을 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
+          : productListType === "CATEGORY"
+          ? "상품을 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
+          : "검색 결과를 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
+      }`}
+    />
+  );
+}
