00c9e8b23cd8b362abe8faf6e2d0e94353136692
MAIN6419 <dark9737@gmail.com>
2024-05-02 20:57:54 +0900

    BugFix: nav-sub-menu가 user가 존재하여도 렌더링되지 않는 오류 수정 - 컴포넌트가 마운트된 이후 렌더링 되도록 하여 오류 수정

--- components/commons/layout/nav/nav-sub-menu.tsx
+++ components/commons/layout/nav/nav-sub-menu.tsx
@@ -4,18 +4,26 @@ import NavNotification from "./nav-notification";
 import NavSell from "./nav-sell";
 import { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function NavSubMenu() {
   const user = useSelector((state: RootState) => state.auth.user);
+  const [mouted, setMouted] = useState(false);
+
+  useEffect(() => {
+    setMouted(true);
+  }, []);
 
   return (
-    <div className={`${!user && "invisible"} relative flex gap-3`}>
-      <NavNotification />
-      <div className="hidden md:flex gap-3">
-        <NavChat />
-        <NavSell />
-        <NavProduct />
+    mouted && (
+      <div className={`${!user && "invisible"} relative flex gap-3`}>
+        <NavNotification />
+        <div className="hidden md:flex gap-3">
+          <NavChat />
+          <NavSell />
+          <NavProduct />
+        </div>
       </div>
-    </div>
+    )
   );
 }
