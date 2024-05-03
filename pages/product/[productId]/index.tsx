c807330ffe429d3b0908cf20889f31040ce51eec
MAIN6419 <dark9737@gmail.com>
2024-05-03 21:17:38 +0900

    Feat: 상품 정보 및 유저 프로필 조회 SSR 처리 추가

--- pages/product/[productId]/index.tsx
+++ pages/product/[productId]/index.tsx
@@ -1,5 +1,48 @@
 import ProductDetailPage from "@/components/productDetail/product-detail";
+import { getProductQueryKey, getProfileQueryKey } from '@/constants/constant';
+import { getUserProfile } from '@/lib/api/auth';
+import { getProduct } from "@/lib/api/product";
+import { ProductData } from "@/types/productTypes";
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { GetServerSideProps } from "next";
 
 export default function ProductDetail() {
   return <ProductDetailPage />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const queryClient = new QueryClient();
+  const productId = context.params?.productId;
+
+  if(productId) {
+    await queryClient.prefetchQuery({
+      queryKey: getProductQueryKey(productId as string),
+      queryFn: async () => {
+        const reponse = await getProduct(productId as string);
+        return reponse.data.product;
+      },
+    });
+  
+    const productData = queryClient.getQueryData([
+      "product",
+      productId,
+    ]) as ProductData;
+  
+    const uid = productData?.uid;
+  
+    if(uid) {
+      await queryClient.prefetchQuery({
+        queryKey: getProfileQueryKey(uid),
+        queryFn: async () => {
+          const profile = await getUserProfile(uid);
+          return profile.data.profile;
+        },
+      });
+    }
+  }
+  
+
+  return {
+    props: { dehydratedState: dehydrate(queryClient) },
+  };
+};
