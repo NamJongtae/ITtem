1bec786db181c97897504130716fbe2efdcb1fd6
MAIN6419 <dark9737@gmail.com>
2024-05-03 22:03:15 +0900

    Chore: ProductListEmpty 컴포넌트명을 Empty로 수정 및 경로 수정 - 공통컴포넌트로  빈 데이터 ui로 사용하기 위해 수정

--- components/commons/Empty.tsx
+++ components/commons/Empty.tsx
@@ -4,12 +4,12 @@ interface IProps {
   message: string;
 }
 
-export default function ProductListEmpty({ message }: IProps) {
+export default function Empty({ message }: IProps) {
   return (
-    <div className="w-full mx-auto flex flex-col gap-3 justify-center items-center min-h-[300px]">
+    <div className="w-full mx-auto  flex flex-col gap-3 justify-center items-center min-h-[300px]">
       <Image
         src={"/icons/empty_icon.svg"}
-        alt="상품이 존재하지 않습니다."
+        alt="상품이 존재하지 않아요."
         width={60}
         height={60}
       />
