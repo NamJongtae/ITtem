d5da55c87ad301d3f1482af886777c58d3e05eb7
MAIN6419 <dark9737@gmail.com>
2024-05-04 19:19:34 +0900

    Fix: 파이어스토어 이미지 업로드 함수 에러 예외 처리 추가

--- lib/api/firebase.ts
+++ lib/api/firebase.ts
@@ -11,37 +11,44 @@ import { UploadImgResponseData } from "@/types/apiTypes";
 export const uploadImgToFireStore = async (
   file: File | ""
 ): Promise<UploadImgResponseData | undefined> => {
-  if (file) {
-    const fileName = `${uuid()}_${file.name}`;
-    const uploadImgResponse = await uploadBytes(
-      ref(storage, `images/profile/${fileName}`),
-      file
-    );
-    const url =
-      uploadImgResponse && (await getDownloadURL(uploadImgResponse.ref));
-    if (url) return { url, name: fileName };
+  try {
+    if (file) {
+      const fileName = `${uuid()}_${file.name}`;
+      const uploadImgResponse = await uploadBytes(
+        ref(storage, `images/profile/${fileName}`),
+        file
+      );
+      const url =
+        uploadImgResponse && (await getDownloadURL(uploadImgResponse.ref));
+      if (url) return { url, name: fileName };
+    }
+  } catch (error) {
+    throw new Error("이미지 업로드에 실패했어요\n잠시 후 다시 시도해주세요.");
   }
 };
 
 export const uploadMultiImgToFirestore = async (
   files: File[]
 ): Promise<UploadImgResponseData[] | undefined> => {
-  if (!files) return;
-  console.log(files);
-  const uploadPromises = [...files].map(async (file) => {
-    // 각 파일에 대해 비동기 업로드 처리
-    const fileName = `${uuid()}_${file.name}`;
-    const uploadImgResponse = await uploadBytes(
-      ref(storage, `images/${fileName}`),
-      file
-    );
-    const url = await getDownloadURL(uploadImgResponse.ref);
-    return { url, name: fileName } as UploadImgResponseData;
-  });
+  try {
+    if (!files) return;
+    const uploadPromises = [...files].map(async (file) => {
+      // 각 파일에 대해 비동기 업로드 처리
+      const fileName = `${uuid()}_${file.name}`;
+      const uploadImgResponse = await uploadBytes(
+        ref(storage, `images/${fileName}`),
+        file
+      );
+      const url = await getDownloadURL(uploadImgResponse.ref);
+      return { url, name: fileName } as UploadImgResponseData;
+    });
 
-  // 모든 파일이 업로드되길 기다립니다.
-  const uploadResults = await Promise.all(uploadPromises);
-  return uploadResults;
+    // 모든 파일이 업로드되길 기다립니다.
+    const uploadResults = await Promise.all(uploadPromises);
+    return uploadResults;
+  } catch (error) {
+    throw new Error("이미지 업로드에 실패했어요\n잠시 후 다시 시도해주세요.");
+  }
 };
 
 export const deleteImgToFirestore = async (
