c4c4b1990b3ca5ec3cfb91d1231defc8c31da74f
MAIN6419 <dark9737@gmail.com>
2024-05-04 19:07:24 +0900

    Fix: 상품 업로드시 createdAt를 클라이언트 대신 서버 시간을 기준으로 설정 하도록 수정

--- pages/api/product/upload/index.ts
+++ pages/api/product/upload/index.ts
@@ -1,5 +1,5 @@
 import { DBClient } from "@/lib/database";
-import { checkAuthorization } from '@/lib/server';
+import { checkAuthorization } from "@/lib/server";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -23,12 +23,17 @@ export default async function handler(
         res.status(422).json({ message: "상품 데이터가 없어요." });
         return;
       }
-
+      const uploadProductData = { ...productData, createdAt: new Date() };
       await DBClient.connect();
       const db = DBClient.db("ITtem");
-      await db.collection("product").insertOne(productData);
+      await db.collection("product").insertOne(uploadProductData);
 
-      res.status(201).json({ message: "상품 등록에 성공했어요.", productData });
+      res
+        .status(201)
+        .json({
+          message: "상품 등록에 성공했어요.",
+          product: uploadProductData,
+        });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "상품 등록에 실패하였어요." });
