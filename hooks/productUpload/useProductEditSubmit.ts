b295f1dd6201674968c4dcfbcbae5086fc0bf680
MAIN6419 <dark9737@gmail.com>
2024-05-06 18:29:41 +0900

    Fix: 상품 수정 submit customhook 수정 - 상품 수정 데이터 설정 함수에서 다른 속성들도 수정 데이터에 포함되도록 로직 추가 - 에러 처리 로직 수정

--- hooks/productUpload/useProductEditSubmit.ts
+++ hooks/productUpload/useProductEditSubmit.ts
@@ -10,6 +10,7 @@ import {
 import { UploadImgResponseData } from "@/types/apiTypes";
 import { useState } from "react";
 import { toast } from "react-toastify";
+import { isAxiosError } from "axios";
 
 export default function useProductEditSubmit() {
   const [productEditLoading, setProductEditLoading] = useState(false);
@@ -20,9 +21,14 @@ export default function useProductEditSubmit() {
   const productId = params?.productId;
   let productEditData = {} as Partial<ProductData>;
 
+  const productData =
+    (queryClient.getQueryData(["product", productId]) as ProductData) ||
+    undefined;
+
   /**
    * 상품 수정시 수정된 데이터만 설정하는 함수
    */
+
   const setProductEditData = async (values: FieldValues) => {
     for (const key of Object.keys(values)) {
       if (key === "price") {
@@ -37,21 +43,35 @@ export default function useProductEditSubmit() {
           productEditData.imgData = values.prevImgData;
         }
         if (values.imgData) {
-          const imgData = await uploadMultiImgToFirestore(
-            values.imgData.filter((data: object) => data instanceof File)
+          const imgFiles = values.imgData.filter(
+            (data: object) => data instanceof File
           );
+          const imgData = await uploadMultiImgToFirestore(imgFiles);
           productEditData.imgData = [
             ...values.prevImgData,
             ...(imgData as UploadImgResponseData[]),
           ];
         }
+      } else if (
+        key === "description" ||
+        key === "transaction" ||
+        key === "deliveryFee" ||
+        key === "returnPolicy" ||
+        key === "condition" ||
+        key === "location" ||
+        key === "category" ||
+        key === "name"
+      ) {
+        if (productData[key] !== values[key]) {
+          productEditData[key] = values[key];
+        }
       }
     }
   };
 
-  /**
-   * 상품 수정시 삭제된 이미지 삭제 함수
-   */
+  // /**
+  //  * 상품 수정시 삭제된 이미지 삭제 함수
+  //  */
   const deleteImages = async (values: FieldValues) => {
     if (productEditData.imgData && values.prevImgData) {
       const productDataImgName = productData.imgData.map((data) => data.name);
@@ -62,10 +82,6 @@ export default function useProductEditSubmit() {
     }
   };
 
-  const productData =
-    (queryClient.getQueryData(["product", productId]) as ProductData) ||
-    undefined;
-
   const handleClickProductEditSubmit = async (values: FieldValues) => {
     try {
       setProductEditLoading(true);
@@ -73,13 +89,18 @@ export default function useProductEditSubmit() {
       await deleteImages(values);
       await productEditMutate(productEditData);
     } catch (error) {
-      toast.warn(
-        "상품 수정 중 에러가 발생했어요.\n잠시 후 다시 시도해 주세요."
-      );
+      if (isAxiosError<{ message: string }>(error)) {
+        toast.warn(error.response?.data.message);
+      } else if (error instanceof Error) {
+        toast.warn(error.message);
+      }
     } finally {
       setProductEditLoading(false);
     }
   };
 
-  return { handleClickProductEditSubmit, productEditLoading };
+  return {
+    handleClickProductEditSubmit,
+    productEditLoading
+  };
 }
