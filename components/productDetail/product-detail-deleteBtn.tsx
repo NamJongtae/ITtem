1ab057e93a9891ab0dcdc592b5538af2b48d2c95
MAIN6419 <dark9737@gmail.com>
2024-05-07 22:32:23 +0900

    Feat: 상품 삭제 mutation customhook 생성 및 상세 상세 페이지 상품 삭제 로직 적용

--- components/productDetail/product-detail-deleteBtn.tsx
+++ components/productDetail/product-detail-deleteBtn.tsx
@@ -1,7 +1,18 @@
+import useProductDeleteMutate from "@/hooks/querys/useProducDeletetMutate";
+import Loading from "../commons/loading";
+
 export default function ProductDetailDeleteBtn() {
+  const { productDeleteMutate, productDeleteLoading } =
+    useProductDeleteMutate();
+
+  if (productDeleteLoading) {
+    return <Loading />;
+  }
+
   return (
     <button
       type="button"
+      onClick={() => productDeleteMutate()}
       className="px-8 py-2 bg-red-600 text-white text-sm font-medium rounded hover:betterhover:bg-red-500 focus:outline-none focus:bg-red-500 md:px-6"
     >
       삭제하기
---
+++ hooks/querys/useProducDeletetMutate.ts
@@ -0,0 +1,31 @@
+import { PRODUCT_TODAY_LIST_QUERY_KEY } from "@/constants/constant";
+import { deleteProduct } from "@/lib/api/product";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { isAxiosError } from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+
+export default function useProductDeleteMutate() {
+  const router = useRouter();
+  const productId = router.query?.productId;
+  const queryClient = useQueryClient();
+
+  const { mutate: productDeleteMutate, isPending: productDeleteLoading } =
+    useMutation({
+      mutationFn: () => deleteProduct(productId as string),
+      onSuccess: async (response) => {
+        queryClient.invalidateQueries({
+          queryKey: PRODUCT_TODAY_LIST_QUERY_KEY,
+        });
+        await router.push("/");
+        toast.success(response.data.message);
+      },
+      onError: (error) => {
+        if (isAxiosError<{ message: string }>(error)) {
+          toast.warn(error.response?.data.message);
+        }
+      },
+    });
+
+  return { productDeleteMutate, productDeleteLoading };
+}
