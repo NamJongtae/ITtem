e4edfba96e662818fc3a51ed0448778a8e080eb2
MAIN6419 <dark9737@gmail.com>
2024-05-08 23:42:07 +0900

    Feat: 신고 mutateion에 낙관적 업데이트 처리 추가

--- hooks/querys/useProductReportMutate.ts
+++ hooks/querys/useProductReportMutate.ts
@@ -1,21 +1,46 @@
+import { getProductQueryKey } from "@/constants/constant";
 import { reportProduct } from "@/lib/api/product";
-import { useMutation } from "@tanstack/react-query";
-import { isAxiosError } from "axios";
+import { ProductDetailData } from "@/types/productTypes";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse, isAxiosError } from "axios";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 
 export default function useProductReportMutate() {
   const router = useRouter();
   const productId = router.query?.productId || "";
+  const queryClient = useQueryClient();
+  const PRODUCT_QUERYKEY = getProductQueryKey(productId as string);
 
-  const { mutate: productReportMutate } = useMutation({
+  const { mutate: productReportMutate } = useMutation<
+    AxiosResponse<{ message: string }>,
+    AxiosError,
+    undefined,
+    { previousProduct: ProductDetailData }
+  >({
     mutationFn: () => reportProduct(productId as string),
-    onSuccess: (response) => {
-      toast.success(response.data.message);
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: PRODUCT_QUERYKEY });
+
+      const previousProduct = queryClient.getQueryData(
+        PRODUCT_QUERYKEY
+      ) as ProductDetailData;
+
+      const newProduct = { ...previousProduct, isReport: true };
+
+      queryClient.setQueryData(PRODUCT_QUERYKEY, newProduct);
+
+      toast.success("해당 상품을 신고했어요.");
+
+      return { previousProduct };
     },
-    onError: (error) => {
+    onError: (error, data, ctx) => {
       if (isAxiosError<{ message: string }>(error))
         toast.warn(error.response?.data.message);
+      queryClient.setQueryData(PRODUCT_QUERYKEY, ctx?.previousProduct);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: PRODUCT_QUERYKEY });
     },
   });
 
