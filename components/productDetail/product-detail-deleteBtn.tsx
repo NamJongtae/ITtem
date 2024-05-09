6d054806ecc470b21e585262ac6c6927ed4a96d5
MAIN6419 <dark9737@gmail.com>
2024-05-09 14:25:57 +0900

    Fix: 삭제 버튼에 삭제 아이콘 추가 및 삭제 여부를 물어보는 confirm창 추가

--- components/productDetail/product-detail-deleteBtn.tsx
+++ components/productDetail/product-detail-deleteBtn.tsx
@@ -1,9 +1,17 @@
 import useProductDeleteMutate from "@/hooks/querys/useProducDeletetMutate";
 import Loading from "../commons/loading";
+import Image from "next/image";
 
 export default function ProductDetailDeleteBtn() {
   const { productDeleteMutate, productDeleteLoading } =
     useProductDeleteMutate();
+
+  const handleClickDelete = () => {
+    const isDelete = confirm("정말 삭제하겠습니까?");
+    if (isDelete) {
+      productDeleteMutate();
+    }
+  };
 
   if (productDeleteLoading) {
     return <Loading />;
@@ -12,9 +20,15 @@ export default function ProductDetailDeleteBtn() {
   return (
     <button
       type="button"
-      onClick={() => productDeleteMutate()}
-      className="px-8 py-2 bg-red-600 text-white text-sm font-medium rounded hover:betterhover:bg-red-500 focus:outline-none focus:bg-red-500 md:px-6"
+      onClick={handleClickDelete}
+      className="px-4 py-2 flex gap-2 items-center bg-red-600 text-white text-sm font-medium rounded hover:betterhover:bg-red-500 focus:outline-none focus:bg-red-500 md:px-6"
     >
+      <Image
+        src={"/icons/delete_icon.svg"}
+        width={20}
+        height={20}
+        alt="삭제하기"
+      />
       삭제하기
     </button>
   );
