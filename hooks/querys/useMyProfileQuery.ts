33dc47f767d744473624574617ee3a7b35a28225
MAIN6419 <dark9737@gmail.com>
2024-05-09 21:01:51 +0900

    Feat: 자신의 프로필 조회 customhook useQuery 생성

---
+++ hooks/querys/useMyProfileQuery.ts
@@ -0,0 +1,30 @@
+import { MY_PROFILE_QUERY_KEY } from "@/constants/constant";
+import { getMyProfile } from "@/lib/api/auth";
+import { RootState } from "@/store/store";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+export default function useMyProfileQuery() {
+  const user = useSelector((state: RootState) => state.auth.user);
+
+  const { data, isLoading, isError } = useQuery({
+    queryFn: async () => {
+      const response = await getMyProfile();
+      return response.data.profile;
+    },
+    queryKey: MY_PROFILE_QUERY_KEY,
+    enabled: !!user,
+  });
+
+  useEffect(() => {
+    if (isError) {
+      toast.warn(
+        "나의 프로필 정보를 가져오는데 실패했어요.\n로그인 정보를 확인해주세요."
+      );
+    }
+  }, [isError]);
+  
+  return { myProfileData: data, loadMyProfileLoading: isLoading };
+}
