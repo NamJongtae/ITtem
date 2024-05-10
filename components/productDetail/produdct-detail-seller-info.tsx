ce01cdbb510595e6197e022193402132572bdb3e
MAIN6419 <dark9737@gmail.com>
2024-05-10 15:17:49 +0900

    Fix: FollowerBtn 컴포넌트가 csr되도록 dynamic import ssr:false 설정

--- components/productDetail/produdct-detail-seller-info.tsx
+++ components/productDetail/produdct-detail-seller-info.tsx
@@ -1,6 +1,11 @@
-import { ProductDetailAuthData } from '@/types/productTypes';
+import { ProductDetailAuthData } from "@/types/productTypes";
 import Image from "next/image";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+const ProductDetailSerllerFollowBtn = dynamic(
+  () => import("./product-detail-serller-followBtn"),
+  { ssr: false }
+);
 
 interface IProps {
   auth: ProductDetailAuthData;
@@ -8,28 +13,31 @@ interface IProps {
 
 export default function ProductDetailSellerInfo({ auth }: IProps) {
   return (
-    <Link
-      href={`/profile/${auth.uid}`}
-      className="inline-flex items-center gap-4 rounded-md p-3 "
-    >
-      <Image
-        className="inline-block h-14 w-14 cursor-pointer rounded-full object-cover object-center mr-1 border-2"
-        src={auth?.profileImg || "/icons/user_icon.svg"}
-        alt="판매자"
-        width={56}
-        height={56}
+    <div className="flex items-center gap-3">
+      <Link
+        href={`/profile/${auth.uid}`}
+        className="inline-flex items-center gap-4 rounded-md p-3 "
+      >
+        <Image
+          className="inline-block h-14 w-14 cursor-pointer rounded-full object-cover object-center mr-1 border-2"
+          src={auth?.profileImg || "/icons/user_icon.svg"}
+          alt="판매자"
+          width={56}
+          height={56}
+        />
+        <div>
+          <span>{auth.nickname}</span>
+          <span className="block text-gray-500 text-sm">
+            {auth.reviewPercentage === 0
+              ? "리뷰없음"
+              : `평가 : ${auth.reviewPercentage}%`}
+          </span>
+        </div>
+      </Link>
+      <ProductDetailSerllerFollowBtn
+        uid={auth.uid}
+        authFollowers={auth.followers}
       />
-      <div>
-        <span>{auth.nickname}</span>
-        <span className="block text-gray-500 text-sm">
-          {auth.reviewPercentage === 0
-            ? "리뷰없음"
-            : `평가 : ${auth.reviewPercentage}%`}
-        </span>
-      </div>
-      <button className="border py-2 px-4 text-md betterhover:hover:bg-gray-100">
-        + 팔로우
-      </button>
-    </Link>
+    </div>
   );
 }
