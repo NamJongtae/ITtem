ec7260b86f700a2661f93cc27af6c782689b3730
MAIN6419 <dark9737@gmail.com>
2024-05-10 15:39:18 +0900

    Fix: 찜 목록 추가 및 해제 낙관적 업데이트 수정 - isWish는 더 이상 사용하지 않기 때문에 제거 - 상품 데이터에 wishUserIds 배열에 자신의 uid 추가/제거 - myProfile 데이터 낙관적 업데이트에 추가/제거 - myProfile 데이터에 wishProductIds에 상품의 uid 추가/제거

--- hooks/querys/useAddWishMutate.ts
+++ hooks/querys/useAddWishMutate.ts
@@ -1,9 +1,12 @@
-import { getProductQueryKey } from "@/constants/constant";
+import { MY_PROFILE_QUERY_KEY, getProductQueryKey } from "@/constants/constant";
 import { addWish } from "@/lib/api/product";
+import { RootState } from "@/store/store";
+import { ProfileData } from "@/types/authTypes";
 import { ProductDetailData } from "@/types/productTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse, isAxiosError } from "axios";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
 export default function useAddWishMutate() {
@@ -11,12 +14,13 @@ export default function useAddWishMutate() {
   const productId = router.query?.productId || "";
   const queryClient = useQueryClient();
   const PRODUCT_QUERYKEY = getProductQueryKey(productId as string);
+  const myUid = useSelector((state: RootState) => state.auth.user);
 
   const { mutate: addWishMutate } = useMutation<
     AxiosResponse<{ message: string }>,
     AxiosError,
     undefined,
-    { previousProduct: ProductDetailData }
+    { previousProduct: ProductDetailData; previousMyProfile: ProfileData }
   >({
     mutationFn: () => addWish(productId as string),
     onMutate: async () => {
@@ -24,24 +28,37 @@ export default function useAddWishMutate() {
         queryKey: PRODUCT_QUERYKEY,
       });
 
+      await queryClient.cancelQueries({ queryKey: MY_PROFILE_QUERY_KEY });
+
       const previousProduct = queryClient.getQueryData(
         PRODUCT_QUERYKEY
       ) as ProductDetailData;
 
+      const previousMyProfile = queryClient.getQueryData(
+        MY_PROFILE_QUERY_KEY
+      ) as ProfileData;
+
       const newProduct = {
         ...previousProduct,
-        isWish: true,
+        wishUserIds: [...previousProduct.wishUserIds, myUid],
         wishCount: previousProduct.wishCount + 1,
       };
 
+      const newMyProfile = {
+        ...previousMyProfile,
+        wishProductIds: [...previousMyProfile.wishProductIds, previousProduct.uid],
+      };
+
       queryClient.setQueryData(PRODUCT_QUERYKEY, newProduct);
+      queryClient.setQueryData(MY_PROFILE_QUERY_KEY, newMyProfile);
+
       toast.success("찜 목록에 상품을 추가했어요.");
-      return { previousProduct };
+      return { previousProduct, previousMyProfile };
     },
     onError: (error, data, ctx) => {
+
       if (isAxiosError<{ message: string }>(error)) {
         toast.warn(error.response?.data.message);
-        queryClient.setQueryData(PRODUCT_QUERYKEY, ctx?.previousProduct);
       }
     },
     onSettled: () => {
--- hooks/querys/useDeleteWishMutate.ts
+++ hooks/querys/useDeleteWishMutate.ts
@@ -1,9 +1,12 @@
-import { getProductQueryKey } from "@/constants/constant";
+import { MY_PROFILE_QUERY_KEY, getProductQueryKey } from "@/constants/constant";
 import { deleteWish } from "@/lib/api/product";
+import { RootState } from "@/store/store";
+import { ProfileData } from "@/types/authTypes";
 import { ProductDetailData } from "@/types/productTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse, isAxiosError } from "axios";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
 export default function useDeleteWishMutate() {
@@ -11,12 +14,13 @@ export default function useDeleteWishMutate() {
   const productId = router.query?.productId || "";
   const queryClient = useQueryClient();
   const PRODUCT_QUERYKEY = getProductQueryKey(productId as string);
+  const myUid = useSelector((state: RootState) => state.auth.user);
 
   const { mutate: deleteWishMutate } = useMutation<
     AxiosResponse<{ message: string }>,
     AxiosError,
     undefined,
-    { previousProduct: ProductDetailData }
+    { previousProduct: ProductDetailData; previousMyProfile: ProfileData }
   >({
     mutationFn: () => deleteWish(productId as string),
     onMutate: async () => {
@@ -28,23 +32,40 @@ export default function useDeleteWishMutate() {
         PRODUCT_QUERYKEY
       ) as ProductDetailData;
 
+      const previousMyProfile = queryClient.getQueryData(
+        MY_PROFILE_QUERY_KEY
+      ) as ProfileData;
+
       const newProduct = {
         ...previousProduct,
-        isWish: false,
+        wishUserIds: [
+          ...previousProduct.wishUserIds.filter((id) => id !== (myUid || "")),
+        ],
         wishCount: previousProduct.wishCount - 1,
       };
 
+      const newMyProfile = {
+        ...previousMyProfile,
+        followings: [
+          ...previousMyProfile.followings.filter(
+            (id) => id !== previousProduct.uid
+          ),
+        ],
+      };
+
       queryClient.setQueryData(PRODUCT_QUERYKEY, newProduct);
+      queryClient.setQueryData(MY_PROFILE_QUERY_KEY, newMyProfile);
 
       toast.success("찜 목록에서 상품을 삭제했어요.");
 
-      return { previousProduct };
+      return { previousProduct, previousMyProfile };
     },
 
     onError: (error, data, ctx) => {
+      queryClient.setQueryData(PRODUCT_QUERYKEY, ctx?.previousProduct);
+      queryClient.setQueryData(MY_PROFILE_QUERY_KEY, ctx?.previousMyProfile);
       if (isAxiosError<{ message: string }>(error)) {
         toast.warn(error.response?.data.message);
-        queryClient.setQueryData(PRODUCT_QUERYKEY, ctx?.previousProduct);
       }
     },
     onSettled: () => {
