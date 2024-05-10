d9a61db1d6a01b8e3ec7a1c418898e4eca3f570e
MAIN6419 <dark9737@gmail.com>
2024-05-10 15:36:25 +0900

    Fix: 신고 낙관적 업데이트 로직 수정 - isReort 데이터는 더 이상 하지 않기 때문에 제거 - reportUserIds에 자신의 uid 추가, reportCount를 1 증가, block reportCount가 4이상 일 시 block 처리

--- hooks/querys/useProductReportMutate.ts
+++ hooks/querys/useProductReportMutate.ts
@@ -1,9 +1,11 @@
 import { getProductQueryKey } from "@/constants/constant";
 import { reportProduct } from "@/lib/api/product";
+import { RootState } from '@/store/store';
 import { ProductDetailData } from "@/types/productTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse, isAxiosError } from "axios";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
 export default function useProductReportMutate() {
@@ -11,6 +13,7 @@ export default function useProductReportMutate() {
   const productId = router.query?.productId || "";
   const queryClient = useQueryClient();
   const PRODUCT_QUERYKEY = getProductQueryKey(productId as string);
+  const myUid = useSelector((state: RootState) => state.auth.user);
 
   const { mutate: productReportMutate } = useMutation<
     AxiosResponse<{ message: string }>,
@@ -26,7 +29,12 @@ export default function useProductReportMutate() {
         PRODUCT_QUERYKEY
       ) as ProductDetailData;
 
-      const newProduct = { ...previousProduct, isReport: true };
+      const newProduct = {
+        ...previousProduct,
+        reportUserIds: [...previousProduct.reportUserIds, myUid],
+        reportCount: previousProduct.reportCount + 1,
+        block: previousProduct.reportCount >= 4,
+      };
 
       queryClient.setQueryData(PRODUCT_QUERYKEY, newProduct);
 
