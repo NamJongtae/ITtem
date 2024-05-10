432c59a6f234f78af16acc7e56547f95367d8190
MAIN6419 <dark9737@gmail.com>
2024-05-10 15:19:18 +0900

    Feat: 자신의 프로필을 불러오도록 useMyProfileQuery 추가 - wishBtn에 찜확인을 위헤 myProfileData Props로 전달

--- components/productDetail/product-detail-content-btns.tsx
+++ components/productDetail/product-detail-content-btns.tsx
@@ -1,38 +1,39 @@
-import { ProfileData } from "@/types/authTypes";
 import ProductDetailBuyBtn from "./product-detail-buyBtn";
 import ProductDetailChattingBtn from "./product-detail-chattingBtn";
 import ProductDetailWishBtn from "./product-detail-wishBtn";
 import { ProductDetailData } from "@/types/productTypes";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
 import ProductDetailDeleteBtn from "./product-detail-deleteBtn";
 import ProductDetailEditBtn from "./product-detail-EditBtn";
+import useMyProfileQuery from "@/hooks/querys/useMyProfileQuery";
 
 interface IProps {
   productDetailData: ProductDetailData | undefined;
-  isWish: boolean | undefined;
 }
 
 export default function ProductDetailContentBtns({
   productDetailData,
-  isWish,
 }: IProps) {
-  const user = useSelector((state: RootState) => state.auth.user);
+  const { myProfileData, loadMyProfileLoading } = useMyProfileQuery();
 
   return (
-    <div className="flex items-center mt-6 gap-3 flex-wrap">
-      {productDetailData?.uid === user?.uid ? (
-        <>
-          <ProductDetailEditBtn />
-          <ProductDetailDeleteBtn />
-        </>
-      ) : (
-        <>
-          <ProductDetailWishBtn isWish={isWish} />
-          <ProductDetailChattingBtn />
-          <ProductDetailBuyBtn />
-        </>
-      )}
-    </div>
+    !loadMyProfileLoading && (
+      <div className="flex items-center mt-6 gap-3 flex-wrap">
+        {productDetailData?.uid === myProfileData?.uid ? (
+          <>
+            <ProductDetailEditBtn />
+            <ProductDetailDeleteBtn />
+          </>
+        ) : (
+          <>
+            <ProductDetailWishBtn
+              productDetailData={productDetailData}
+              myProfileData={myProfileData}
+            />
+            <ProductDetailChattingBtn />
+            <ProductDetailBuyBtn />
+          </>
+        )}
+      </div>
+    )
   );
 }
