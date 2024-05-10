3a4556e410100e09b76f99b416ba04a6b93e9d41
MAIN6419 <dark9737@gmail.com>
2024-05-10 15:27:27 +0900

    Fix: ProductDetailContentBtns isWish props 제거

--- components/productDetail/prduct-dateil-content.tsx
+++ components/productDetail/prduct-dateil-content.tsx
@@ -1,4 +1,4 @@
-import { ProductData, ProductDetailData } from "@/types/productTypes";
+import { ProductDetailData } from "@/types/productTypes";
 import dynamic from "next/dynamic";
 import ProductDetailContentInfo from "./product-detail-content-info";
 import ProductDetailContentImg from "./product-detail-content-img";
@@ -19,7 +19,7 @@ export default function PrductDateilContent({ productDetailData }: IProps) {
       <ProductDetailContentImg productDetailData={productDetailData} />
       <div className="w-full mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
         <ProductDetailContentInfo productDetailData={productDetailData} />
-        <ProductDetailContentBtns productDetailData={productDetailData} isWish={productDetailData?.isWish} />
+        <ProductDetailContentBtns productDetailData={productDetailData}/>
       </div>
     </div>
   );
