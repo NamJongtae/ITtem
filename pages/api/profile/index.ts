e1e840ca5387c426a7724378aa4a8d636ef87631
MAIN6419 <dark9737@gmail.com>
2024-05-11 15:18:32 +0900

    Fix: 자신의 프로필 조회 api router에 method가 GET일 때 응답 되도록 수정

--- pages/api/profile/index.ts
+++ pages/api/profile/index.ts
@@ -8,45 +8,40 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    await dbConnect();
+  if (req.method === "GET") {
+    try {
+      await dbConnect();
 
-    const isValidAuth = await checkAuthorization(req, res);
+      const isValidAuth = await checkAuthorization(req, res);
 
-    if (!isValidAuth.isValid) {
-      res.status(401).json({
-        message: isValidAuth.message,
+      const myUid = isValidAuth?.auth?.uid;
+
+      const profile = await User.findOne({
+        _id: new mongoose.Types.ObjectId(myUid),
       });
-      return;
-    }
-    
-    const myUid = isValidAuth?.auth?.uid;
 
-    const profile = await User.findOne({
-      _id: new mongoose.Types.ObjectId(myUid),
-    });
+      if (!profile) {
+        res.status(404).json({
+          message: "유저가 존재하지 않습니다.\n로그인 정보를 확인해주세요.",
+        });
+        return;
+      }
 
-    if (!profile) {
-      res.status(404).json({
-        message: "유저가 존재하지 않습니다.\n로그인 정보를 확인해주세요.",
+      let profileData = { ...profile._doc, uid: profile._id };
+      delete profileData.password;
+      delete profileData.__v;
+      delete profileData.loginType;
+      delete profileData._id;
+
+      res.status(200).json({
+        message: "프로필 조회에 성공했어요.",
+        profile: profileData,
       });
-      return;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "프로필 조회에 실패했어요.\n잠시 후 다시 시도해주세요.",
+      });
     }
-
-    let profileData = { ...profile._doc, uid: profile._id };
-    delete profileData.password;
-    delete profileData.__v;
-    delete profileData.loginType;
-    delete profileData._id;
-
-    res.status(200).json({
-      message: "프로필 조회에 성공했어요.",
-      profile: profileData,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "프로필 조회에 실패했어요.\n잠시 후 다시 시도해주세요.",
-    });
   }
 }
