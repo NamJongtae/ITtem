774cc2541563b01c76fede69e55cce8d9edd6693
MAIN6419 <dark9737@gmail.com>
2024-05-11 20:00:26 +0900

    Feat: SSR를 통해 유저 프로필 데이터 prefetch 로직 추가

--- pages/profile/[uid]/index.tsx
+++ pages/profile/[uid]/index.tsx
@@ -1,7 +1,27 @@
-import ProfilePage from '@/components/profile/profile-page';
+import ProfilePage from "@/components/profile/profile-page";
+import { getProfileQueryKey } from "@/constants/constant";
+import { getUserProfile } from "@/lib/api/auth";
+
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { GetServerSideProps } from "next";
 
 export default function UserProfile() {
-  return (
-    <ProfilePage />
-  )
+  return <ProfilePage />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const queryClient = new QueryClient();
+  const uid = context.query.uid;
+
+  await queryClient.prefetchQuery({
+    queryKey: getProfileQueryKey(uid as string),
+    queryFn: async () => {
+      const response = await getUserProfile(uid as string);
+      return response.data.profile;
+    },
+  });
+
+  return {
+    props: { dehydratedState: dehydrate(queryClient) },
+  };
+};
