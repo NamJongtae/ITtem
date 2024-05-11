5676031b036c506cd3181c72692eb0ee4cb3e28d
MAIN6419 <dark9737@gmail.com>
2024-05-11 19:58:59 +0900

    Fix: useProfileQuery 수정 - uid가 존재할 때만 실행되도록 enabled 속성에 조건추가 - prefetch된 캐시 데이터를 사용하기 위해 staleTime 추가 - uid를 파라미터로 받지 않고, url query를 통해 가져오도록 수정

--- hooks/querys/useProfileQuery.ts
+++ hooks/querys/useProfileQuery.ts
@@ -1,20 +1,26 @@
 import { getProfileQueryKey } from "@/constants/constant";
 import { getUserProfile } from "@/lib/api/auth";
-import { ProfileData } from '@/types/authTypes';
+import { ProfileData } from "@/types/authTypes";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useRouter } from "next/router";
 
-export default function useProfileQuery(uid: string) {
+export default function useProfileQuery() {
+  const router = useRouter();
+  const uid = router.query?.uid || "";
+
   const {
     data: profileData,
     isLoading: loadProfileDataLoading,
     error: loadProfileDataError,
   } = useQuery<ProfileData, AxiosError>({
-    queryKey: getProfileQueryKey(uid),
+    queryKey: getProfileQueryKey(uid as string),
     queryFn: async () => {
-      const response = await getUserProfile(uid);
+      const response = await getUserProfile(uid as string);
       return response.data.profile;
     },
+    enabled: !!uid,
+    staleTime: 30 * 1000,
   });
 
   return { profileData, loadProfileDataLoading, loadProfileDataError };
