f0301d4da1daac5fa893cf7c50f956a3848e4ca9
MAIN6419 <dark9737@gmail.com>
2024-05-12 17:42:45 +0900

    Fix: useProfileImg customhook 수정 - profileImg setValue 함수에 shouldDirtry, shouldValidate 속성 true로 추가 - resetProfileImg 함수에서 프로필 수정시 기존 이미지가 없어지도록 resetField 대신 setValue로 직접 기본값으로 수정 - 프로필 수정 시 기존 프로필 이미지 preview 설정을 위해 useEffect hook 추가

--- hooks/signup/useProfileImg.ts
+++ hooks/signup/useProfileImg.ts
@@ -1,12 +1,13 @@
 import { imgValidation } from "@/lib/imgValidation";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useFormContext } from "react-hook-form";
 
 export default function useProfileImg() {
   const [preview, setPreview] = useState("/icons/user_icon.svg");
   const imgInputRef = useRef<HTMLInputElement | null>(null);
 
-  const { setValue, resetField } = useFormContext();
+  const { setValue, resetField, getValues } = useFormContext();
+  const img = getValues("profileImg");
 
   const handleClickImgInput = () => {
     if (!imgInputRef.current) return;
@@ -21,14 +22,19 @@ export default function useProfileImg() {
     if (!isValid) return;
 
     const imgPreview = URL.createObjectURL(file);
-    setValue("profileImg", file);
+    setValue("profileImg", file, { shouldDirty: true, shouldValidate: true });
     setPreview(imgPreview);
   };
 
   const resetProfileImg = () => {
-    resetField("profileImg");
+    setValue("profileImg", "", { shouldDirty: true, shouldValidate: true });
     setPreview("/icons/user_icon.svg");
   };
+
+  // 프로필 수정시 초기 이미지 설정
+  useEffect(() => {
+    setPreview(img);
+  }, []);
 
   return {
     handleClickImgInput,
