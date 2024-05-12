d98a0fcfc038c6df5c031d19b1a67d59fd7e919f
MAIN6419 <dark9737@gmail.com>
2024-05-12 15:13:49 +0900

    Feat: MyProfilePage 생성 및 SSR myProfile 데이터 prefetch

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,5 +1,44 @@
+import ProfilePage from "@/components/profile/profile-page";
+import { MY_PROFILE_QUERY_KEY } from "@/constants/constant";
+import customAxios from "@/lib/customAxios";
+import { sessionOptions } from "@/lib/server";
+import { IronSessionData } from "@/types/apiTypes";
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { getIronSession } from "iron-session";
+import { GetServerSideProps } from "next";
+
 export default function MyProfile() {
-  return (
-    <div>MyProfile</div>
-  )
+  return <ProfilePage my={true} />;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const queryClient = new QueryClient();
+
+  const session = await getIronSession<IronSessionData>(
+    context.req,
+    context.res,
+    sessionOptions
+  );
+  const cookie = context.req.headers.cookie;
+
+  if (session) {
+    await queryClient.prefetchQuery({
+      queryKey: MY_PROFILE_QUERY_KEY,
+      queryFn: async () => {
+        try {
+          const response = await customAxios("/api/profile", {
+            headers: {
+              Cookie: cookie,
+            },
+          });
+          return response.data.profile;
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    });
+  }
+  return {
+    props: { dehydratedState: dehydrate(queryClient) },
+  };
+};
