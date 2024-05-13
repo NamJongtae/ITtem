6b12fb3944bb398d413171b5cc7b5c27afc2be1e
MAIN6419 <dark9737@gmail.com>
2024-05-13 23:37:00 +0900

    Feat: 팔로워/팔로잉 목록 조회 api router 생성

---
+++ pages/api/profile/followings/index.ts
@@ -0,0 +1,130 @@
+import User from "@/lib/db/models/User";
+import mongoose from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const { userIds } = req.body;
+      const { cursor, limit } = req.query;
+
+      if (!userIds) {
+        res
+          .status(422)
+          .json({ message: "팔로잉 유저 아이디 목록이 존재하지 않아요." });
+        return;
+      }
+
+      const pageLimit = parseInt(limit as string, 10) || 10;
+      const objectIdArray = userIds.map(
+        (id: string) => new mongoose.Types.ObjectId(id)
+      );
+
+      let matchStage;
+      if (cursor) {
+        matchStage = {
+          $match: {
+            _id: {
+              $in: objectIdArray,
+              $lt: new mongoose.Types.ObjectId(cursor as string),
+            },
+          },
+        };
+      } else {
+        matchStage = {
+          $match: {
+            _id: {
+              $in: objectIdArray,
+            },
+          },
+        };
+      }
+
+      const aggregation = [
+        matchStage,
+        {
+          $lookup: {
+            from: "reviewScore",
+            localField: "_id",
+            foreignField: "_id",
+            as: "reviewInfo",
+          },
+        },
+        {
+          $limit: pageLimit,
+        },
+        {
+          $unwind: {
+            path: "$reviewInfo",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $addFields: {
+            reviewPercentage: {
+              $cond: {
+                if: {
+                  $eq: [{ $ifNull: ["$reviewInfo.totalScore", null] }, null],
+                },
+                then: 0,
+                else: {
+                  $round: [
+                    {
+                      $multiply: [
+                        {
+                          $divide: [
+                            {
+                              $divide: [
+                                "$reviewInfo.totalScore",
+                                "$reviewInfo.totalReviewCount",
+                              ],
+                            },
+                            5,
+                          ],
+                        },
+                        100,
+                      ],
+                    },
+                    1,
+                  ],
+                },
+              },
+            },
+            uid: "$_id",
+          },
+        },
+        {
+          $project: {
+            password: 0,
+            __v: 0,
+            loginType: 0,
+          },
+        },
+      ];
+
+      const followings = await User.aggregate(aggregation)
+        .limit(pageLimit)
+        .sort({ _id: 1 });
+
+      if (!followings.length) {
+        res.status(404).json({ message: "팔로잉 목록이 존재하지 않아요."});
+        return;
+      }
+
+      const newFollowings = followings.map(({ _id, ...rest }) => rest);
+
+      res.status(200).json({
+        message: "팔로잉 목록 조회에 성공했어요.",
+        followings: newFollowings,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "팔로잉 목록 조회에 실패했어요.\n잠시 후 다시 시도해주세요.",
+      });
+    }
+  }
+}
