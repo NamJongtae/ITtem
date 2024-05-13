c3dc682e216fc25ea6f9cac0751fdc8c33ba0dec
MAIN6419 <dark9737@gmail.com>
2024-05-13 18:09:41 +0900

    Fix: 비밀번호 변경 Mutation에서 비밀번호 변경과 찾기를 모두 수행하도록 수정 - 기존 비밀번호 찾기에 사용되던 useChangePassword hook 제거 - useChangePasswordMutate에서 onSucess와 onError를 통해 에러와 성공후 로직 처리 추가 - isFindPw와 closeModal를 옵셔널 파라미터로 받아 onSuccess에서 비밀번호 찾기와 비밀번호 변경시 로직을 다르게 처리

--- hooks/querys/useChangePasswordMutate.ts
+++ hooks/querys/useChangePasswordMutate.ts
@@ -1,25 +1,45 @@
 import { changePassword } from "@/lib/api/auth";
 import { useMutation } from "@tanstack/react-query";
-import { AxiosError, AxiosResponse } from "axios";
+import { AxiosError, AxiosResponse, isAxiosError } from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
-export default function useChangePasswordMutate() {
-  const {
-    mutate: changePasswordMutate,
-    isPending: changePasswordLoading,
-    isSuccess: changePasswordSuccess,
-    error: changePasswordError,
-  } = useMutation<
-    AxiosResponse<{ message: string }>,
-    AxiosError,
-    { email: string; password: string; isFindPw?: boolean }
-  >({
-    mutationFn: async ({ email, password, isFindPw }) =>
-      await changePassword({ email, password, isFindPw }),
-  });
+export default function useChangePasswordMutate({
+  isFindPw,
+  closeModal,
+}: {
+  isFindPw?: boolean;
+  closeModal?: () => void;
+}) {
+  const router = useRouter();
+  const { mutate: changePasswordMutate, isPending: changePasswordLoading } =
+    useMutation<
+      AxiosResponse<{ message: string }>,
+      AxiosError,
+      {
+        email?: string;
+        password: string;
+        currentPassword?: string;
+        isFindPw?: boolean;
+      }
+    >({
+      mutationFn: async ({ email, password, currentPassword }) =>
+        await changePassword({ email, password, currentPassword, isFindPw }),
+      onSuccess: (response) => {
+        if (isFindPw) {
+          router.push("/signin");
+        }
+        if (closeModal) closeModal();
+        toast.success(response.data.message);
+      },
+      onError: (error) => {
+        if (isAxiosError<{ message: string }>(error)) {
+          toast.warn(error.response?.data.message);
+        }
+      },
+    });
   return {
     changePasswordMutate,
     changePasswordLoading,
-    changePasswordSuccess,
-    changePasswordError,
   };
 }
