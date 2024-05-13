12a95914b0ee9c5f52773f4c74ea2e2376fbfbaa
MAIN6419 <dark9737@gmail.com>
2024-05-13 15:11:09 +0900

    Fix 유저 인증 api router 수정 - accessToken에서 유저 데이터를 가져오지 않고 데이터에서 직접 유저 데이터를 가져오도록 수정

--- pages/api/auth/user/index.ts
+++ pages/api/auth/user/index.ts
@@ -1,4 +1,6 @@
 import { ERROR_MESSAGE } from "@/constants/constant";
+import User from "@/lib/db/models/User";
+import mongoose from "mongoose";
 import { checkAuthorization } from "@/lib/server";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -17,11 +19,21 @@ export default async function handler(
         return;
       }
 
+      const myUid = isValidAuth?.auth?.uid;
+
+      const user = await User.findOne({
+        _id: new mongoose.Types.ObjectId(myUid),
+      });
+
       res.status(200).json({
         message: "유저정보를 성공적으로 불러왔어요.",
-        user: isValidAuth.auth,
+        user: {
+          uid: user._id,
+          nickname: user.nickname,
+          profileImg: user.profileImg,
+          email: user.email,
+        },
       });
-      
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: ERROR_MESSAGE });
