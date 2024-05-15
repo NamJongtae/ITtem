dda579f3511286cc0d72dff2c44a1f5b2ac5477f
MAIN6419 <dark9737@gmail.com>
2024-05-15 22:20:34 +0900

    Feat: 프로필 찜 목록 스켈레톤 ui 생성

---
+++ components/profile/profileDetailWish/profile-detail-wish-skeletonUI.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface IProps {
+  listCount?: number;
+}
+
+export default function ProfileDetailWishSkeletonUI({
+  listCount = 10,
+}: IProps) {
+  return Array(listCount)
+    .fill("")
+    .map((_, index) => (
+      <li key={index} className="animate-pulse border">
+        <div className="flex gap-3 w-full">
+          <Image
+            className="w-32 h-32 border-r bg-gray-200"
+            src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+            width={56}
+            height={56}
+            alt="loading..."
+          />
+          <div className="flex flex-col gap-3 px-4 py-2">
+            <div className="w-32 h-[14px] bg-gray-300 mb-2"></div>
+            <div className="h-[14px] bg-gray-300 mb-1"></div>
+            <div className="h-[14px] bg-gray-300 mb-1"></div>
+            <div className="w-20 h-[14px] bg-gray-300"></div>
+          </div>
+        </div>
+      </li>
+    ));
+}
