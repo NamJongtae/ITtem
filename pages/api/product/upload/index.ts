1adc96630622f7a3bf607ace7f3dbcb17a71cf4b
MAIN6419 <dark9737@gmail.com>
2024-05-18 20:43:24 +0900

    Feat: 상품 업로드시 상품 판매 거래 정보 데이터를 생성하도록 코드 추가

--- pages/api/product/upload/index.ts
+++ pages/api/product/upload/index.ts
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/db";
 import Product from "@/lib/db/models/Product";
+import SalesTrading from '@/lib/db/models/SalesTrading';
 import User from "@/lib/db/models/User";
 import { checkAuthorization } from "@/lib/server";
 import mongoose from "mongoose";
@@ -9,63 +10,72 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await mongoose.startSession();
-  session.startTransaction();
-  try {
-    const isValidAuth = await checkAuthorization(req, res);
+  if (req.method === "POST") {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const isValidAuth = await checkAuthorization(req, res);
 
-    if (!isValidAuth.isValid) {
+      if (!isValidAuth.isValid) {
+        await session.abortTransaction();
+        session.endSession();
+        res.status(401).json({
+          message: isValidAuth.message,
+        });
+        return;
+      }
+
+      const { productData } = req.body;
+
+      const myUid = isValidAuth?.auth?.uid;
+
+      await dbConnect();
+
+      const newProduct = new Product(productData);
+
+      await newProduct.save({ session });
+
+      const updateResult = await User.updateOne(
+        { _id: new mongoose.Types.ObjectId(myUid) },
+        { $push: { productIds: newProduct._id } },
+        { session }
+      );
+
+      if (!updateResult.acknowledged || updateResult.modifiedCount === 0) {
+        throw new Error("유저에 상품 아이디 등록에 실패했어요.");
+      }
+
+      const salesTrading= new SalesTrading({
+        seller: newProduct.uid,
+        productId: newProduct._id,
+      });
+
+      await salesTrading.save({ session });
+
+      await session.commitTransaction();
+      session.endSession();
+
+      res.status(201).json({
+        message: "상품 등록에 성공했어요.",
+        product: newProduct,
+      });
+    } catch (error) {
       await session.abortTransaction();
       session.endSession();
-      res.status(401).json({
-        message: isValidAuth.message,
+      console.error(error);
+      if (error instanceof mongoose.Error.ValidationError) {
+        const errorMessages = Object.values(error.errors).map(
+          (err) => err.message
+        );
+        res.status(422).json({
+          message: "유효하지 않은 값이 있어요.",
+          error: errorMessages,
+        });
+        return;
+      }
+      res.status(500).json({
+        message: "상품 등록에 실패하였어요.\n잠시 후 다시 시도해주세요.",
       });
-      return;
     }
-
-    const { productData } = req.body;
-
-    const myUid = isValidAuth?.auth?.uid;
-
-    await dbConnect();
-
-    const newProduct = new Product(productData);
-
-    const updateResult = await User.updateOne(
-      { _id: new mongoose.Types.ObjectId(myUid) },
-      { $push: { productIds: newProduct._id } },
-      { session }
-    );
-
-    if (!updateResult.acknowledged || updateResult.modifiedCount === 0) {
-      throw new Error("유저에 상품 아이디 등록에 실패했어요.");
-    }
-
-    await newProduct.save({ session });
-
-    await session.commitTransaction();
-    session.endSession();
-
-    res.status(201).json({
-      message: "상품 등록에 성공했어요.",
-      product: newProduct,
-    });
-  } catch (error) {
-    await session.abortTransaction();
-    session.endSession();
-    console.error(error);
-    if (error instanceof mongoose.Error.ValidationError) {
-      const errorMessages = Object.values(error.errors).map(
-        (err) => err.message
-      );
-      res.status(422).json({
-        message: "유효하지 않은 값이 있어요.",
-        error: errorMessages,
-      });
-      return;
-    }
-    res.status(500).json({
-      message: "상품 등록에 실패하였어요.\n잠시 후 다시 시도해주세요.",
-    });
   }
 }
