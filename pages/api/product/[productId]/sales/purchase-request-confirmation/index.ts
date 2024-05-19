266b608aa486631e4081255ef65b667e08a02b13
MAIN6419 <dark9737@gmail.com>
2024-05-19 14:30:09 +0900

    Feat: 판매자 구매확인 api router 구현 - 구매요청이온 상품을 판매자가 확인하는 과정입니다. - 판매자가 구매요청을 확인하면, 거래 판매 process는 다음단계인 상품전달확인으로 변경됩니다. - 거래 구매 process는 다음단계인 상품전달중으로 변경됩니다. - 현재 구매요청 확인 단계인지를 확인하고, 아니라면 예외처리합니다. - 거래가 취소, 환불, 완료된 상태인지 확인하고 예외처리합니다. - 판매자의 uid와 나의 uid를 비교하여 다르다면 요청이 거부되도록 처리합니다.

---
+++ pages/api/product/[productId]/sales/purchase-request-confirmation/index.ts
@@ -0,0 +1,160 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import mongoose from "mongoose";
+import dbConnect from "@/lib/db";
+import SalesTrading from "@/lib/db/models/SalesTrading";
+import {
+  PurchaseTradingProcess,
+  SalesTradingProcess,
+  TradingStatus,
+} from "@/types/productTypes";
+import PurchaseTrading from "@/lib/db/models/PurchaseTrading";
+import { checkAuthorization } from "@/lib/server";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PATCH") {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const isValidAuth = await checkAuthorization(req, res);
+
+      if (!isValidAuth.isValid) {
+        await session.abortTransaction();
+        session.endSession();
+        res.status(401).json({
+          message: isValidAuth.message,
+        });
+        return;
+      }
+
+      const myUid = isValidAuth?.auth?.uid;
+      
+      const { productId } = req.query;
+
+      if (!productId) {
+        res.status(422).json({ message: "상품 ID가 존재하지 않아요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      await dbConnect();
+
+      const salesTrading = await SalesTrading.findOne(
+        {
+          productId,
+        },
+        null,
+        { session }
+      );
+
+      if (!salesTrading) {
+        res.status(404).json({ message: "거래중인 판매 상품 정보가 없어요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (myUid !== salesTrading.seller) {
+        res.status(401).json({ message: "잘못된 요청입니다." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      const purchaseTrading = await PurchaseTrading.findOne(
+        {
+          productId,
+        },
+        null,
+        { session }
+      );
+
+      if (!purchaseTrading) {
+        res.status(404).json({ message: "거래중인 구매 상품 정보가 없어요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (salesTrading.status === TradingStatus.CANCEL) {
+        res.status(409).json({ message: "구매자가 취소요청한 상품입니다." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (
+        salesTrading.process === SalesTradingProcess.상품전달확인 &&
+        purchaseTrading.process === PurchaseTradingProcess.상품전달중
+      ) {
+        res.status(409).json({ message: "이미 구매요청을 확인한 상품입니다." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (
+        salesTrading.process !== SalesTradingProcess.구매요청확인 &&
+        purchaseTrading.process !== PurchaseTradingProcess.판매자확인중
+      ) {
+        res
+          .status(409)
+          .json({ message: "상품 구매요청확인 단계가 아닌 상품입니다." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (salesTrading.process === SalesTradingProcess.구매요청확인) {
+        const saleTradingUpdateResult = await SalesTrading.updateOne(
+          {
+            productId,
+          },
+          { process: SalesTradingProcess.상품전달확인 },
+          { session }
+        );
+
+        if (
+          !saleTradingUpdateResult.acknowledged ||
+          saleTradingUpdateResult.modifiedCount === 0
+        ) {
+          throw new Error("거래 상품 판매 status 업데이트에 실패했어요.");
+        }
+      }
+
+      if (purchaseTrading.process === PurchaseTradingProcess.판매자확인중) {
+        const purchaseTradingUpdateResult = await PurchaseTrading.updateOne(
+          {
+            productId,
+          },
+          { process: PurchaseTradingProcess.상품전달중 },
+          { session }
+        );
+
+        if (
+          !purchaseTradingUpdateResult.acknowledged ||
+          purchaseTradingUpdateResult.modifiedCount === 0
+        ) {
+          throw new Error("거래 상품 구매 status 업데이트에 실패했어요.");
+        }
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      res.status(200).json({
+        message: "구매요청 확인에 성공했어요.",
+      });
+    } catch (error) {
+      console.error(error);
+      await session.abortTransaction();
+      session.endSession();
+      res.status(500).json({
+        message: "구매요청 확인에 실패했어요.\n잠시 후 다시 시도해주세요.",
+      });
+    }
+  }
+}
