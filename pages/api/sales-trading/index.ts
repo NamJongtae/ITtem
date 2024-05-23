322bec1dc73534c7a782ef435e2d053f4199a8cd
MAIN6419 <dark9737@gmail.com>
2024-05-23 17:21:22 +0900

    Feat: 상품 판매/구매 정보 조회 api router 구현

---
+++ pages/api/sales-trading/index.ts
@@ -0,0 +1,116 @@
+import SalesTrading from "@/lib/db/models/SalesTrading";
+import { checkAuthorization } from '@/lib/server';
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const { search, status, cursor, limit } = req.query;
+
+    const isValidAuth = await checkAuthorization(req, res);
+
+    if (!isValidAuth.isValid) {
+      res.status(401).json({
+        message: isValidAuth.message,
+      });
+      return;
+    }
+
+    const myUid = isValidAuth?.auth?.uid;
+
+    if (
+      status !== "TRADING" &&
+      status !== "TRADING_END" &&
+      status !== "CANCEL_END/RETURN_END"
+    ) {
+      res.status(422).json({ message: "올바르지 않은 status이에요." });
+      return;
+    }
+
+    const message =
+      status === "CANCEL_END/RETURN_END"
+        ? "취소/반품"
+        : status === "TRADING_END"
+        ? "거래완료"
+        : "거래중";
+
+    try {
+      const currentCursor = cursor ? new Date(cursor as string) : new Date();
+      const currentLimit = limit ? parseInt(limit as string, 10) : 10;
+      const currentStatus =
+        status === "CANCEL_END/RETURN_END"
+          ? ["CANCEL_END", "RETURN_END"]
+          : [status];
+
+      let matchStage: any = {
+        saleStartDate: { $lt: currentCursor },
+        seller: myUid,
+        status: { $in: currentStatus },
+      };
+
+      if (search) {
+        matchStage.productName = { $regex: search, $options: "i" };
+      }
+
+      const aggregate: any[] = [
+        {
+          $match: matchStage,
+        },
+        {
+          $sort: { saleStartDate: -1 },
+        },
+        {
+          $limit: currentLimit,
+        },
+        {
+          $addFields: {
+            convertedProductId: { $toObjectId: "$productId" },
+          },
+        },
+        {
+          $lookup: {
+            from: "products",
+            localField: "convertedProductId",
+            foreignField: "_id",
+            as: "productData",
+          },
+        },
+        {
+          $unwind: "$productData",
+        },
+        {
+          $project: {
+            "productData.name": 1,
+            "productData.imgData.url": 1,
+            "productData.price": 1,
+            _id: 1,
+            seller: 1,
+            productId: 1,
+            saleStartDate: 1,
+            saleEndDate: 1,
+            returnReason: 1,
+            cancelReason: 1,
+            cancelStartDate: 1,
+            cacelEndDate: 1,
+            returnStartDate: 1,
+            returnEndDate: 1,
+            process: 1,
+            status: 1,
+          },
+        },
+      ];
+      const salesTrading = await SalesTrading.aggregate(aggregate);
+
+      res
+        .status(200)
+        .json({ message: `${message} 목록 조회에 성공했어요.`, salesTrading });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: `${message} 목록 조회에 실패하였습니다.\n잠시 후 다시 시도해주세요.`,
+      });
+    }
+  }
+}
