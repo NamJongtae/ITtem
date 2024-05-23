78d0fc96f2c54349b55287de92d00aa64398862e
MAIN6419 <dark9737@gmail.com>
2024-05-23 14:04:23 +0900

    Fix: ReturnPuchaseRejectSchema 필드명 refund를 return으로 수정

--- lib/db/models/ReturnPurchaseReject.ts
+++ lib/db/models/ReturnPurchaseReject.ts
@@ -6,9 +6,9 @@ export const returnPurchaseRejectSchema = new mongoose.Schema(
     buyer: { type: String, required: [true, "구매자 ID가 없어요."] },
     seller: { type: String, required: [true, "판매자 ID가 없어요."] },
     productId: { type: String, required: [true, "상품 ID가 없어요."] },
-    refundStartDate: { type: Date, required: [true, "반품 시작일이 없어요."] },
+    returnStartDate: { type: Date, required: [true, "반품 시작일이 없어요."] },
     rejectReason: { type: String, require: [true, "반품 거부 사유가 없어요."] },
-    refundEndDate: { type: Date, required: false, default: Date.now },
+    returnEndDate: { type: Date, required: false, default: Date.now },
   },
   { collection: "returnPurchaseReject" }
 );
