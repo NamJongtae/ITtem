a3c55c315fce5f7de0e777e22fb3f8c882ee8c34
MAIN6419 <dark9737@gmail.com>
2024-05-24 15:27:09 +0900

    Feat: 상품 판매/구매 거래 정보 날짜 형식 설정 함수 생성

--- lib/getDateFormate.ts
+++ lib/getDateFormate.ts
@@ -25,3 +25,14 @@ export const getDateFormat = (time: string) => {
     ).slice(-2)}. ${("0" + dataTime.getDate()).slice(-2)}.`;
   }
 };
+
+export const getTradingDateFormat = (dateString: string) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${year}.${month}.${day} ${hours}:${minutes}`;
+};
