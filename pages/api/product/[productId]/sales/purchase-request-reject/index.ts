d2303cabd082b04dfa2ac80c30dd63dd148073a8
MAIN6419 <dark9737@gmail.com>
2024-05-25 16:35:27 +0900

    Feat: 구매요청 거절 api router 구현

---
+++ pages/api/product/[productId]/sales/purchase-request-reject/index.ts
@@ -0,0 +1,242 @@
+import dbConnect from "@/lib/db";
+import Product from "@/lib/db/models/Product";
+import PurchaseTrading from "@/lib/db/models/PurchaseTrading";
+import SalesTrading from "@/lib/db/models/SalesTrading";
+import { checkAuthorization } from "@/lib/server";
+import {
+  ProductStatus,
+  PurchaseCancelProcess,
+  PurchaseReturnProcess,
+  PurchaseTradingProcess,
+  SalesCancelProcess,
+  SalesReturnProcess,
+  SalesTradingProcess,
+  TradingStatus,
+} from "@/types/productTypes";
+import mongoose from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PATCH") {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const isValidAuth = await checkAuthorization(req, res);
+
+      if (!isValidAuth.isValid) {
+        await session.abortTransaction();
+        session.endSession();
+        res.status(401).json({
+          message: isValidAuth.message,
+        });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      const myUid = isValidAuth?.auth?.uid;
+
+      await dbConnect();
+
+      const { productId } = req.query;
+      const { cancelReason } = req.body;
+
+      if (!productId) {
+        res.status(422).json({ message: "상품 ID가 존재하지 않아요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (!cancelReason || !cancelReason.trim()) {
+        res
+          .status(422)
+          .json({ message: "구매요청 거절 사유가 존재하지 않아요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      const salesTrading = await SalesTrading.findOne(
+        {
+          $and: [
+            { process: { $ne: SalesCancelProcess.취소완료 } },
+            { process: { $ne: SalesReturnProcess.반품완료 } },
+          ],
+          productId,
+        },
+        null,
+        { session }
+      );
+
+      if (!salesTrading) {
+        res.status(404).json({ message: "거래중인 판매 상품 정보가 없어요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (myUid !== salesTrading.seller) {
+        res.status(401).json({ message: "잘못된 요청이에요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (salesTrading.status === TradingStatus.CANCEL) {
+        res.status(409).json({ message: "취소 요청한 상품이 이에요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (salesTrading.status === TradingStatus.RETURN) {
+        res.status(409).json({ message: "반품 요청한 상품이에요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (salesTrading.staus === TradingStatus.TRADING_END) {
+        res.status(409).json({ message: "거래가 완료된 상품이에요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (salesTrading.staus === TradingStatus.CANCEL_END) {
+        res.status(409).json({ message: "취소된 상품이에요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (salesTrading.staus === TradingStatus.RETURN_END) {
+        res.status(409).json({ message: "반품된 상품이에요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      const purchaseTrading = await PurchaseTrading.findOne(
+        {
+          $and: [
+            { process: { $ne: PurchaseCancelProcess.취소완료 } },
+            { process: { $ne: PurchaseReturnProcess.반품완료 } },
+          ],
+          productId,
+        },
+        null,
+        { session }
+      );
+
+      if (!purchaseTrading) {
+        res.status(404).json({ message: "거래중인 구매 상품 정보가 없어요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      if (
+        salesTrading.process !== SalesTradingProcess.구매요청확인 &&
+        purchaseTrading.process !== PurchaseTradingProcess.구매요청
+      ) {
+        res.status(409).json({ message: "구매요청 취소 단계가 아니에요." });
+        await session.abortTransaction();
+        session.endSession();
+        return;
+      }
+
+      const productUpdateResult = await Product.updateOne(
+        {
+          _id: new mongoose.Types.ObjectId(productId as string),
+        },
+        {
+          status: ProductStatus.sold,
+        },
+        { session }
+      );
+
+      if (
+        !productUpdateResult.acknowledged ||
+        productUpdateResult.modifiedCount === 0
+      ) {
+        throw new Error("상품 status 업데이트에 실패했어요.");
+      }
+
+      const currentDate = new Date();
+
+      const salesTradingUpdateResult = await SalesTrading.updateOne(
+        {
+          $and: [
+            { process: { $ne: SalesCancelProcess.취소완료 } },
+            { process: { $ne: SalesReturnProcess.반품완료 } },
+          ],
+          productId,
+        },
+        {
+          status: TradingStatus.CANCEL_END,
+          process: SalesCancelProcess.취소완료,
+          cancelReason,
+          cancelEndDate: currentDate,
+        },
+        { session }
+      );
+
+      if (
+        !salesTradingUpdateResult.acknowledged ||
+        salesTradingUpdateResult.modifiedCount === 0
+      ) {
+        throw new Error("상품 판매 정보 업데이트에 실패했어요.");
+      }
+
+      const purchaseTradingUpdateResult = await PurchaseTrading.updateOne(
+        {
+          $and: [
+            { process: { $ne: PurchaseCancelProcess.취소완료 } },
+            { process: { $ne: PurchaseReturnProcess.반품완료 } },
+          ],
+          productId,
+        },
+        {
+          status: TradingStatus.CANCEL_END,
+          process: PurchaseCancelProcess.취소완료,
+          cancelReason,
+          cancelEndDate: currentDate,
+        },
+        { session }
+      );
+
+      if (
+        !purchaseTradingUpdateResult.acknowledged ||
+        purchaseTradingUpdateResult.modifiedCount === 0
+      ) {
+        throw new Error("거래 상품 구매 status 업데이트에 실패했어요.");
+      }
+      const newSalesTrading = new SalesTrading({
+        productId,
+        seller: myUid,
+        saleStartDate: salesTrading.saleStartDate,
+        productName: salesTrading.productName,
+      });
+
+      await newSalesTrading.save();
+
+      await session.commitTransaction();
+      session.endSession();
+      res.status(200).json({
+        message: "구매요청 취소 성공했어요. 거래가 취소 되었어요.",
+      });
+    } catch (error) {
+      console.error(error);
+      await session.abortTransaction();
+      session.endSession();
+      res.status(500).json({
+        message: "구매요청 취소 실패했어요.\n잠시 후 다시 시도해주세요.",
+      });
+    }
+  }
+}
