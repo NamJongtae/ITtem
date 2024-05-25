77d6c87048418834b0eeeb85a33ce5ef31ec8858
MAIN6419 <dark9737@gmail.com>
2024-05-25 17:06:01 +0900

    Fix: 상품 관리 페이지에서 재사용을 위해 수정 - productId를 파라미터로 받음 - router.query에 productId가 존재하지 않을 경우 파라미터 productId로 설정 - router.query에 productId가 존재하는 경우에만 메인페이지로 이동되도록 수정

--- hooks/querys/useProducDeletetMutate.ts
+++ hooks/querys/useProducDeletetMutate.ts
@@ -1,23 +1,25 @@
-import { PRODUCT_TODAY_LIST_QUERY_KEY } from "@/constants/constant";
 import { deleteProduct } from "@/lib/api/product";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { isAxiosError } from "axios";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 
-export default function useProductDeleteMutate() {
+export default function useProductDeleteMutate(productId?: string) {
   const router = useRouter();
-  const productId = router.query?.productId;
+  const currentProductId = router.query?.productId || productId;
   const queryClient = useQueryClient();
 
   const { mutate: productDeleteMutate, isPending: productDeleteLoading } =
     useMutation({
-      mutationFn: () => deleteProduct(productId as string),
+      mutationFn: () => deleteProduct(currentProductId as string),
       onSuccess: async (response) => {
         queryClient.invalidateQueries({
           queryKey: ["product"],
         });
-        await router.replace("/");
+        if (router.query?.productId) {
+          await router.replace("/");
+        }
+
         toast.success(response.data.message);
       },
       onError: (error) => {
