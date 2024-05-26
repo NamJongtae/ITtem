942c73ba8b014265121cc6a584c0d0f74a44c0f3
MAIN6419 <dark9737@gmail.com>
2024-05-26 16:16:16 +0900

    Fix: ProductManageItemTradingBtns 컴포넌트 수정 - 현재 거래 status를 비교하는 조건문을 추가 필요한 버튼들을 상세 구분하도록 처리

--- components/product-manage/product-manage-item-purchase-trading-btn.tsx
+++ components/product-manage/product-manage-item-purchase-trading-btn.tsx
@@ -3,7 +3,15 @@ import {
   PurchaseReturnProcess,
   PurchaseTradingData,
   PurchaseTradingProcess,
+  TradingStatus,
 } from "@/types/productTypes";
+import PurchaseTradingChattingBtn from "./purchaseTradingBtn/purchase-trading-chatting-btn";
+import PurchaseTradingCancelBtn from "./purchaseTradingBtn/purchase-trading-cancel-btn";
+import PurchaseTradingReceiptConfirmationBtn from "./purchaseTradingBtn/purchase-trading-receipt-confirmation-btn";
+import PurchaseTradingReturnBtn from "./purchaseTradingBtn/purchase-trading-return-btn";
+import PurchaseTradingReturnwithdrawalBtn from "./purchaseTradingBtn/purchase-trading-reutrn-withdrawal-btn";
+import PurchaseTradingCancelWithdrawalBtn from "./purchaseTradingBtn/purchase-trading-cancel-withdrawal-btn";
+import PurchaseTradingReturnDeliveryConfirmationBtn from "./purchaseTradingBtn/purchase-trading-return-delivery-confirmation-btn";
 
 interface IProps {
   tradingData: PurchaseTradingData;
@@ -12,74 +20,72 @@ interface IProps {
 export default function ProductManageItemPurchaseTradingBtn({
   tradingData,
 }: IProps) {
-  if (tradingData.process === PurchaseTradingProcess.판매자확인중) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <button className="text-sm sm:text-base px-4 py-2 bg-red-500 text-white font-semibold betterhover:hover:bg-red-600">
-          채팅하기
-        </button>
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          구매취소
-        </button>
-      </div>
-    );
-  } else if (tradingData.process === PurchaseTradingProcess.판매자상품전달중) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <button className="text-sm sm:text-base px-4 py-2 bg-red-500 text-white font-semibold betterhover:hover:bg-red-600">
-          채팅하기
-        </button>
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          구매취소
-        </button>
-      </div>
-    );
-  } else if (tradingData.process === PurchaseTradingProcess.상품인수확인) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <button className="text-sm sm:text-base px-4 py-2 bg-red-500 text-white font-semibold betterhover:hover:bg-red-600">
-          상품인수
-        </button>
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          환불요청
-        </button>
-      </div>
-    );
-  } else if (tradingData.process === PurchaseCancelProcess.취소요청) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          취소철회
-        </button>
-      </div>
-    );
-  } else if (tradingData.process === PurchaseReturnProcess.판매자확인중) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          환불철회
-        </button>
-      </div>
-    );
-  } else if (tradingData.process === PurchaseReturnProcess.반품상품전달확인) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          상품전달 확인
-        </button>
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          환불철회
-        </button>
-      </div>
-    );
-  } else if (tradingData.process === PurchaseReturnProcess.판매자반품상품인수확인중) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-          채팅하기
-        </button>
-      </div>
-    );
+  if (tradingData.status === TradingStatus.TRADING) {
+    if (tradingData.process === PurchaseTradingProcess.판매자확인중) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <PurchaseTradingChattingBtn />
+          <PurchaseTradingCancelBtn productId={tradingData.productId} />
+        </div>
+      );
+    } else if (
+      tradingData.process === PurchaseTradingProcess.판매자상품전달중
+    ) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <PurchaseTradingChattingBtn />
+          <PurchaseTradingCancelBtn productId={tradingData.productId} />
+        </div>
+      );
+    } else if (tradingData.process === PurchaseTradingProcess.상품인수확인) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <PurchaseTradingReceiptConfirmationBtn
+            productId={tradingData.productId}
+          />
+          <PurchaseTradingReturnBtn productId={tradingData.productId} />
+        </div>
+      );
+    }
+  } else if (tradingData.status === TradingStatus.CANCEL) {
+    if (tradingData.process === PurchaseCancelProcess.판매자확인중) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <PurchaseTradingCancelWithdrawalBtn
+            productId={tradingData.productId}
+          />
+        </div>
+      );
+    }
+  } else if (tradingData.status === TradingStatus.RETURN) {
+    if (tradingData.process === PurchaseReturnProcess.판매자확인중) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <PurchaseTradingReturnwithdrawalBtn
+            productId={tradingData.productId}
+          />
+        </div>
+      );
+    } else if (tradingData.process === PurchaseReturnProcess.반품상품전달확인) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <PurchaseTradingReturnDeliveryConfirmationBtn
+            productId={tradingData.productId}
+          />
+          <PurchaseTradingReturnwithdrawalBtn
+            productId={tradingData.productId}
+          />
+        </div>
+      );
+    } else if (
+      tradingData.process === PurchaseReturnProcess.판매자반품상품인수확인중
+    ) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <PurchaseTradingChattingBtn />
+        </div>
+      );
+    }
   }
   return null;
 }
--- components/product-manage/product-manage-item-sale-trading-btn.tsx
+++ components/product-manage/product-manage-item-sale-trading-btn.tsx
@@ -3,6 +3,7 @@ import {
   SalesCancelProcess,
   SalesReturnProcess,
   SalesTradingProcess,
+  TradingStatus,
 } from "@/types/productTypes";
 import SaleTradingDeleteBtn from "./saleTradingBtn/sale-trading-delete-btn";
 import SaleTradingEditBtn from "./saleTradingBtn/sale-trading-edit-btn";
@@ -23,64 +24,76 @@ interface IProps {
 export default function ProductManageItemSaleTradingBtn({
   tradingData,
 }: IProps) {
-  if (tradingData.process === SalesTradingProcess.판매중) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingEditBtn productId={tradingData.productId} />
-        <SaleTradingDeleteBtn productId={tradingData.productId} />
-      </div>
-    );
-  } else if (tradingData.process === SalesTradingProcess.구매요청확인) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingPurchaseConfirmationBtn productId={tradingData.productId} />
-        <SaleTradingPurchaseRequestRejectBtn
-          productId={tradingData.productId}
-        />
-      </div>
-    );
-  } else if (tradingData.process === SalesTradingProcess.상품전달확인) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingDeliveryConfirmationBtn productId={tradingData.productId} />
-      </div>
-    );
-  } else if (tradingData.process === SalesTradingProcess.구매자상품인수중) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingChattingBtn />
-      </div>
-    );
-  } else if (tradingData.process === SalesCancelProcess.취소요청확인) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingCancelConfirmationBtn productId={tradingData.productId} />
-        <SaleTradingPurchaseCancelRejectBtn productId={tradingData.productId} />
-      </div>
-    );
-  } else if (tradingData.process === SalesReturnProcess.반품요청확인) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingReturnConfirmationBtn productId={tradingData.productId} />
-      </div>
-    );
-  } else if (tradingData.process === SalesReturnProcess.구매자반품상품전달중) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingChattingBtn />
-      </div>
-    );
-  } else if (
-    tradingData.process === SalesReturnProcess.판매자반품상품인수확인
-  ) {
-    return (
-      <div className="flex flex-row justify-end sm:flex-col gap-3">
-        <SaleTradingReturnReceiptComfirmationBtn
-          productId={tradingData.productId}
-        />
-        <SaleTradingRetrunRejectBtn productId={tradingData.productId} />
-      </div>
-    );
+  if (tradingData.status === TradingStatus.TRADING) {
+    if (tradingData.process === SalesTradingProcess.판매중) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingEditBtn productId={tradingData.productId} />
+          <SaleTradingDeleteBtn productId={tradingData.productId} />
+        </div>
+      );
+    } else if (tradingData.process === SalesTradingProcess.구매요청확인) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingPurchaseConfirmationBtn
+            productId={tradingData.productId}
+          />
+          <SaleTradingPurchaseRequestRejectBtn
+            productId={tradingData.productId}
+          />
+        </div>
+      );
+    } else if (tradingData.process === SalesTradingProcess.상품전달확인) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingDeliveryConfirmationBtn
+            productId={tradingData.productId}
+          />
+        </div>
+      );
+    } else if (tradingData.process === SalesTradingProcess.구매자상품인수중) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingChattingBtn />
+        </div>
+      );
+    }
+  } else if (tradingData.status === TradingStatus.CANCEL) {
+    if (tradingData.process === SalesCancelProcess.취소요청확인) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingCancelConfirmationBtn productId={tradingData.productId} />
+          <SaleTradingPurchaseCancelRejectBtn
+            productId={tradingData.productId}
+          />
+        </div>
+      );
+    }
+  } else if (tradingData.status === TradingStatus.RETURN) {
+    if (tradingData.process === SalesReturnProcess.반품요청확인) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingReturnConfirmationBtn productId={tradingData.productId} />
+        </div>
+      );
+    } else if (
+      tradingData.process === SalesReturnProcess.구매자반품상품전달중
+    ) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingChattingBtn />
+        </div>
+      );
+    } else if (tradingData.process === SalesReturnProcess.반품상품인수확인) {
+      return (
+        <div className="flex flex-row justify-end sm:flex-col gap-3">
+          <SaleTradingReturnReceiptComfirmationBtn
+            productId={tradingData.productId}
+          />
+          <SaleTradingRetrunRejectBtn productId={tradingData.productId} />
+        </div>
+      );
+    }
   }
   return null;
 }
