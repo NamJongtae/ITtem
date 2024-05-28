3fc525f196ff57f1b0bca7b1b3170febeeb59795
MAIN6419 <dark9737@gmail.com>
2024-05-28 16:18:55 +0900

    Fix: ReviewSchema 필드 수정 및 추가

--- lib/db/models/Review.ts
+++ lib/db/models/Review.ts
@@ -1,13 +1,35 @@
 import mongoose from "mongoose";
 
-export const reviewSchema = new mongoose.Schema(
+interface ReviewDB {
+  seller: string;
+  buyer: string;
+  reviewScore: number;
+  saleTradingId: string;
+  purchaseTradingId: string;
+  productId: string;
+  productName: string;
+  reviewContent: string;
+  reviewTags: number[];
+  createdAt: Date;
+}
+
+export const reviewSchema = new mongoose.Schema<ReviewDB>(
   {
-    reviewScore: { type: Number, required: [true, "리뷰 점수가 없어요."] },
-    productId: { type: String, required: [true, "상품 ID가 없어요."] },
-    content: { type: String, required: [true, "리뷰 내용이 없어요."] },
     seller: { type: String, required: [true, "판매자 ID가 없어요."] },
     buyer: { type: String, required: [true, "구매자 ID가 없어요."] },
-    tags: { type: [Number], required: false, default: [0, 0, 0, 0, 0] },
+    reviewScore: { type: Number, required: [true, "리뷰 점수가 없어요."] },
+    saleTradingId: {
+      type: String,
+      required: [true, "상품 판매 거래 ID가 없어요."],
+    },
+    purchaseTradingId: {
+      type: String,
+      required: [true, "상품 구매 거래 ID가 없어요."],
+    },
+    productId: { type: String, required: [true, "상품 ID가 없어요."] },
+    productName: { type: String, required: [true, "상품명이 없어요."] },
+    reviewContent: { type: String, required: [true, "리뷰 내용이 없어요."] },
+    reviewTags: { type: [Number], required: [true, "리뷰 태그가 없어요."] },
     createdAt: { type: Date, default: Date.now },
   },
   { collection: "reviews" }
