5b07d13fe06b1dfc9b9d19f762bafba4e76c4e21
MAIN6419 <dark9737@gmail.com>
2024-05-28 17:57:43 +0900

    Feat: 구매 거래완료 내역 반품요청 버튼 적용

--- components/product-manage/product-manage-item-trading-end-btn.tsx
+++ components/product-manage/product-manage-item-trading-end-btn.tsx
@@ -7,6 +7,7 @@ import { ProductManageMenu } from "./product-manage-page";
 import PurchaseTradingEndReviewUploadBtn from "./purchase-trading-end-btn/purchase-trading-end-review-upload-btn";
 import PurchaseTradingEndReviewBtn from "./purchase-trading-end-btn/purchase-trading-end-review-btn";
 import SaleTradingEndReviewBtn from "./modal/sale-trading-end-btn/sale-trading-end-review-btn";
+import PurchaseTradingReturnBtn from "./purchaseTradingBtn/purchase-trading-return-btn";
 
 interface IProps {
   tradingData: SaleTradingData | PurchaseTradingData;
@@ -29,9 +30,7 @@ export default function ProductManageItemTradingEndBtn({
       return (
         <div className="flex flex-row justify-end sm:flex-col gap-3">
           <PurchaseTradingEndReviewBtn productId={tradingData.productId} />
-          <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-            환불요청
-          </button>
+          <PurchaseTradingReturnBtn productId={tradingData.productId} />
         </div>
       );
     } else {
@@ -40,9 +39,7 @@ export default function ProductManageItemTradingEndBtn({
           <PurchaseTradingEndReviewUploadBtn
             productId={tradingData.productId}
           />
-          <button className="text-sm sm:text-base px-4 py-2 bg-gray-500 text-white font-semibold betterhover:hover:bg-gray-600">
-            환불요청
-          </button>
+          <PurchaseTradingReturnBtn productId={tradingData.productId} />
         </div>
       );
     }
