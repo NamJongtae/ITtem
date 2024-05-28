cf82dddf494d1ff60415790ea169ea76b64b9185
MAIN6419 <dark9737@gmail.com>
2024-05-28 16:21:20 +0900

    Chore: ReviewScoreSchema 및 model 생성

---
+++ lib/db/models/ReviewScore.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+interface ReviewScoreDB {
+  uid: string;
+  totalReviewCount: number;
+  totalReviewScore: number;
+  reviewTags: number[];
+}
+
+export const reviewScoreSchema = new mongoose.Schema<ReviewScoreDB>(
+  {
+    uid: { type: String, required: [true, "유저 ID가 없어요."] },
+    totalReviewCount: { type: Number, require: false, default: 0 },
+    totalReviewScore: { type: Number, require: false, default: 0 },
+    reviewTags: { type: [Number], required: false, default: [0, 0, 0, 0, 0] },
+  },
+  { collection: "reviewScores" }
+);
+
+const ReviewScore =
+  mongoose.models?.ReviewScore ||
+  mongoose.model("ReviewScore", reviewScoreSchema);
+
+export default ReviewScore;
