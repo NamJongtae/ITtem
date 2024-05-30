f1b995a505dfd4be6422a89feabc19b0fb85c77c
MAIN6419 <dark9737@gmail.com>
2024-05-30 17:47:39 +0900

    Fix: 사용하지 않는 import 제거

--- components/product-manage/product-manage-item-btns/product-manage-item-trading-end-btns.tsx
+++ components/product-manage/product-manage-item-btns/product-manage-item-trading-end-btns.tsx
@@ -1,15 +1,8 @@
 import {
   PurchaseTradingData,
   SaleTradingData,
-  TradingStatus,
 } from "@/types/productTypes";
 import { ProductManageMenu } from "../product-manage-page";
-import PurchaseTradingEndReviewUploadBtn from "./purchase-trading-end-btn/purchase-trading-end-review-upload-btn";
-import PurchaseTradingEndReviewBtn from "./purchase-trading-end-btn/purchase-trading-end-review-btn";
-import SaleTradingEndReviewBtn from "./sale-trading-end-btn/sale-trading-end-review-btn";
-import PurchaseTradingReturnBtn from "./purchase-trading-btns/purchase-trading-return-btn";
-import PurchaseTradingEndTradingDetailBtn from "./purchase-trading-end-btn/purchase-trading-end-trading-detail-btn";
-import SeleTradingEndTradingDetailBtn from "./sale-trading-end-btn/sale-trading-end-trading-detail-btn";
 import SaleTradingEndBtns from "./sale-trading-end-btn/sale-trading-end-btns";
 import PurchaseTradingEndBtns from "./purchase-trading-end-btn/purchase-trading-end-btns";
 
