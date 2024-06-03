2ba8d5c84160afbb90f4cb35e7d4a004fc367435
MAIN6419 <dark9737@gmail.com>
2024-06-03 16:26:52 +0900

    Fix: 알림 메세지 조회 firebase api 함수 대신 api router 함수 적용

--- hooks/querys/useNotificationInfiniteQuery.ts
+++ hooks/querys/useNotificationInfiniteQuery.ts
@@ -1,4 +1,4 @@
-import { getNotificationMessage } from "@/lib/api/firebase";
+import { getNotificationMessage } from "@/lib/api/notification";
 import { RootState } from "@/store/store";
 import { NotificationMessageData } from "@/types/notification";
 import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
@@ -15,19 +15,21 @@ export default function useNotificationInfiniteQuery(limit: number = 10) {
     isLoading,
     error,
   } = useInfiniteQuery<
-    { items: NotificationMessageData[]; nextKey: string | null },
+    { messages: NotificationMessageData[]; nextKey: string },
     Error,
-    InfiniteData<{ items: NotificationMessageData; nextKey: string | null }>
+    InfiniteData<{ messages: NotificationMessageData; nextKey: string }>
   >({
     queryKey: ["notification"],
     queryFn: async ({ pageParam }) => {
       const response = await getNotificationMessage({
-        userId: user?.uid || "",
         lastKey: pageParam,
         limit,
       });
 
-      return response;
+      return {
+        messages: response.data.messages,
+        nextKey: response.data.nextKey,
+      };
     },
     enabled: !!user,
     initialPageParam: null,
@@ -35,7 +37,7 @@ export default function useNotificationInfiniteQuery(limit: number = 10) {
   });
 
   return {
-    data: data?.pages.map((page) => page?.items).flat(),
+    data: data?.pages.map((page) => page?.messages).flat(),
     hasNextPage,
     fetchNextPage,
     isFetchingNextPage,
