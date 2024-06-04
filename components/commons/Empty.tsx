17792f74928417ca2d4f0f075862aa4fa6c516be
MAIN6419 <dark9737@gmail.com>
2024-06-04 17:11:23 +0900

    Fix: messageSize, iconSize props 추가 및 messageSize 및 iconSize가 적용 되도록 수정

--- components/commons/Empty.tsx
+++ components/commons/Empty.tsx
@@ -2,18 +2,26 @@ import Image from "next/image";
 
 interface IProps {
   message: string;
+  iconSize?: number;
+  messageSize?: "xs" | "sm" | "base" | "lg" | "xl";
 }
 
-export default function Empty({ message }: IProps) {
+export default function Empty({ message,iconSize, messageSize }: IProps) {
   return (
-    <div className="w-full mx-auto  flex flex-col gap-3 justify-center items-center min-h-[300px]">
+    <div className="w-full mx-auto flex flex-col gap-3 justify-center items-center min-h-[300px]">
       <Image
         src={"/icons/empty_icon.svg"}
         alt="상품이 존재하지 않아요."
-        width={60}
-        height={60}
+        width={iconSize ? iconSize : 60}
+        height={iconSize ? iconSize : 60}
       />
-      <p className='whitespace-pre text-center'>{message}</p>
+      <p
+        className={`whitespace-pre text-center text-${
+          messageSize ? messageSize : "base"
+        }`}
+      >
+        {message}
+      </p>
     </div>
   );
 }
