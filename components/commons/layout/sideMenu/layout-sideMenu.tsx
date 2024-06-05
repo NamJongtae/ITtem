0fffb669c59e0d186c5664ec35d0f7db6c62ad06
MAIN6419 <dark9737@gmail.com>
2024-06-05 17:58:45 +0900

    Fix: SideMenu가 chat, signup, signin 페이지에서 렌더링 되지 않도록 수정

--- components/commons/layout/sideMenu/layout-sideMenu.tsx
+++ components/commons/layout/sideMenu/layout-sideMenu.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import SideMenuTopBtn from "./top-btn/sideMenu-top-btn";
+import { useRouter } from "next/router";
 
 const SideMenuRecentProduct = dynamic(
   import("./recent-product/sideMenu-recentProduct"),
@@ -9,6 +10,16 @@ const SideMenuRecentProduct = dynamic(
 );
 
 export default function SideMenu() {
+  const router = useRouter();
+  const pathname = router.pathname;
+  if (
+    pathname.includes("chat") ||
+    pathname.includes("signup") ||
+    pathname.includes("signin")
+  ) {
+    return null;
+  }
+
   return (
     <aside className="fixed bottom-32 right-3 z-10">
       <SideMenuRecentProduct />
--- components/commons/layout/sideMenu/top-btn/sideMenu-top-btn.tsx
+++ components/commons/layout/sideMenu/top-btn/sideMenu-top-btn.tsx
@@ -1,15 +1,8 @@
-import useScrollToTop from '@/hooks/layout/useScrollToTop';
-import { usePathname } from "next/navigation";
-import React from "react";
+import useScrollToTop from "@/hooks/layout/useScrollToTop";
 
 export default function SideMenuTopBtn() {
   const { handleClickTop } = useScrollToTop();
-  const pathname = usePathname();
 
-  if (pathname.includes("signin") || pathname.includes("signup")) {
-    return null;
-  }
-  
   return (
     <button
       onClick={handleClickTop}
