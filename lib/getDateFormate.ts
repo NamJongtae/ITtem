fd5ea08f234a6897979ad799576c60b23dea6e89
MAIN6419 <dark9737@gmail.com>
2024-06-07 17:01:15 +0900

    Feat: 채팅 날짜 formatting 함수 생성

--- lib/getDateFormate.ts
+++ lib/getDateFormate.ts
@@ -36,3 +36,49 @@ export const getTradingDateFormat = (dateString: string) => {
 
   return `${year}.${month}.${day} ${hours}:${minutes}`;
 };
+
+export const getChatDateFormat = (inputDate: Date): string => {
+  const now = new Date();
+  const dataTime = new Date(inputDate);
+
+  const diff = Math.floor((now.getTime() - dataTime.getTime()) / 1000);
+  const diffInDays = diff / (60 * 60 * 24); // 차이를 일 단위로 계산
+
+  // "hour12" 옵션을 false로 설정하여 24시간제를 사용합니다.
+  const options: Intl.DateTimeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+
+  // 하루 이내 차이인 경우
+  if (diffInDays < 1) {
+    const hours = dataTime.getHours();
+    const period = hours < 12 ? "오전" : "오후";
+    const hourFor12 = hours % 12 === 0 ? 12 : hours % 12;
+    const formattedTime = dataTime
+      .toLocaleTimeString("ko-KR", options)
+      .replace(/^(\d{2})/, hourFor12.toString().padStart(2, "0"));
+    return `${period} ${formattedTime}`;
+  }
+
+  // 하루 차이인 경우
+  if (diffInDays < 2) {
+    return "어제";
+  }
+
+  // 1년 이내 차이인 경우
+  if (diffInDays < 365) {
+    return dataTime.toLocaleDateString("ko-KR", {
+      month: "2-digit",
+      day: "2-digit",
+    });
+  }
+
+  // 1년 이상 차이인 경우
+  return dataTime.toLocaleDateString("ko-KR", {
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+  });
+};
