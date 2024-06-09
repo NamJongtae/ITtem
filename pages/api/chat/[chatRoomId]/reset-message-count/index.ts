86c94f847e0de47e8394ebc34369a38a631f9398
MAIN6419 <dark9737@gmail.com>
2024-06-09 15:51:39 +0900

    Fix: 채팅 메세지 전송 api router 경로 변경 - chatRoomId를 query로 가져오기 위해 [chatRoomId] 경로 추가

---
+++ pages/api/chat/[chatRoomId]/reset-message-count/index.ts
@@ -0,0 +1,61 @@
+import { resetChatMessageCount } from "@/lib/api/firebase";
+import { checkAuthorization } from "@/lib/server";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PATCH") {
+    try {
+      const { chatRoomId } = req.query;
+
+      if (!chatRoomId) {
+        res.status(422).json({ message: "채팅방 ID가 없어요." });
+        return;
+      }
+      
+      const isValidAuth = await checkAuthorization(req, res);
+
+      if (!isValidAuth.isValid) {
+        res.status(401).json({
+          message: isValidAuth.message,
+        });
+        return;
+      }
+
+      const myUid = isValidAuth?.auth?.uid;
+
+      if (!myUid) {
+        res.status(400).json({
+          message: "유저 ID가 없어요.",
+        });
+        return;
+      }
+
+      await resetChatMessageCount({
+        myUid,
+        chatRoomId: chatRoomId as string,
+      });
+      res
+        .status(200)
+        .json({ message: "새 메세지 카운터 업데이트에 성공했어요." });
+    } catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        if (error.message === "존재하지 않는 채팅방이에요.") {
+          res.status(404).json({ message: error.message });
+          return;
+        } else if (error.message === "잘못된 접근이에요.") {
+          res.status(400).json({ message: error.message });
+          return;
+        }
+      }
+      res
+        .status(500)
+        .json(
+          "새 메세지 카운터 업데이트에 실패했어요.\n잠시 후 다시 시도해주세요."
+        );
+    }
+  }
+}
