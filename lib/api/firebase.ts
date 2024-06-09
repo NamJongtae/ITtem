433a9b02e4741bca40067564d8a34aea8de3b624
MAIN6419 <dark9737@gmail.com>
2024-06-09 16:31:23 +0900

    Feat: firebase 채팅방 나가기 api 함수 생성

--- lib/api/firebase.ts
+++ lib/api/firebase.ts
@@ -497,3 +497,36 @@ export const sendToChatMessage = async ({
     throw error;
   }
 };
+
+export const exitChatRoom = async ({
+  myUid,
+  chatRoomId,
+}: {
+  myUid: string;
+  chatRoomId: string;
+}) => {
+  try {
+    const chatRoomRef = doc(firestoreDB, `chatRooms/${chatRoomId}`);
+    const userChatRoomIdsRef = doc(firestoreDB, `chatRoomIds/${myUid}`);
+
+    const chatRoomDoc = await getDoc(chatRoomRef);
+    if (!chatRoomDoc.exists()) {
+      throw new Error("존재하지 않는 채팅방이에요.");
+    }
+    const chatRoomData = chatRoomDoc.data();
+    if (!chatRoomData.participantIDs.includes(myUid)) {
+      throw new Error("잘못된 접근이에요.");
+    }
+    const chatRoomIdsDoc = await getDoc(userChatRoomIdsRef);
+
+    if (!chatRoomIdsDoc.exists()) {
+      if (!chatRoomDoc.exists()) {
+        throw new Error("유저 채팅방 목록이 존재하지 않아요.");
+      }
+    }
+    await updateDoc(chatRoomRef, { participantIDs: arrayRemove(myUid) });
+    await updateDoc(userChatRoomIdsRef, { chatRoomIds: arrayRemove(myUid) });
+  } catch (error) {
+    throw error;
+  }
+};
