1e3abafdea23f56f27311f011d570ca25ef7d5c6
MAIN6419 <dark9737@gmail.com>
2024-06-10 17:52:19 +0900

    Fix: 실시간 알림 메세지를 사용하지 않도록 수정 - 실시간 알림 메세지를 사용하지 않고 totalMessageCount를 이용하여 새로운 메세지를 표시하도록 구현

--- hooks/chat/useNotificationChat.ts
+++ hooks/chat/useNotificationChat.ts
@@ -1,102 +1,29 @@
-import { useEffect, useCallback } from "react";
-import {
-  collection,
-  doc,
-  getDoc,
-  onSnapshot,
-  query,
-  updateDoc,
-  where,
-} from "firebase/firestore";
+import { useEffect } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
 import { firestoreDB } from "@/lib/firebaseSetting";
-import { toast } from "react-toastify";
+
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store/store";
 import { chatSlice } from "@/store/chatSlice";
 
 export default function useNotificationChat() {
-  const chatRoomIds = useSelector((state: RootState) => state.chat.chatRoomIds);
   const user = useSelector((state: RootState) => state.auth.user);
   const myUid = user?.uid;
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     if (!myUid) return;
-    const chatRoomIdsRef = doc(firestoreDB, `chatRoomIds/${myUid}`);
+    const chatRoomIdsRef = doc(firestoreDB, `userChatInfo/${myUid}`);
     const unsubscribe = onSnapshot(chatRoomIdsRef, (snapshot) => {
       if (snapshot.exists()) {
-        const ids = (snapshot.data() as { chatRoomIds: string[] }).chatRoomIds;
+        const data = snapshot.data();
+        const ids = data.chatRoomIds;
+        const totalMessageCount = data.totalMessageCount;
         dispatch(chatSlice.actions.saveChatRoomIds(ids));
+        dispatch(chatSlice.actions.saveTotalMessageCount(totalMessageCount));
       }
     });
 
     return () => unsubscribe();
   }, [myUid]);
-
-  const subscribeToChatMessages = useCallback(() => {
-    if (!myUid || chatRoomIds.length === 0) return;
-
-    chatRoomIds.forEach(async (chatRoomId) => {
-      const chatRoomRef = doc(firestoreDB, `chatRooms/${chatRoomId}`);
-      const chatRoomSnapshot = await getDoc(chatRoomRef);
-
-      if (chatRoomSnapshot.exists() && chatRoomSnapshot.data().isAlarm[myUid]) {
-        const entered = chatRoomSnapshot.data().entered[myUid];
-        if (entered === false) {
-          const messagesRef = collection(
-            firestoreDB,
-            `chatRooms/${chatRoomId}/messages`
-          );
-          const messagesQuery = query(
-            messagesRef,
-            where("isNotification", "==", false)
-          );
-
-          onSnapshot(messagesQuery, (snapshot) => {
-            snapshot.docChanges().forEach(async (change) => {
-              if (change.type === "added") {
-                const messageDoc = change.doc;
-                const message = messageDoc.data();
-                const messageRef = doc(
-                  firestoreDB,
-                  `chatRooms/${chatRoomId}/messages/${messageDoc.id}`
-                );
-
-                if (message.senderId !== myUid) {
-                  toast.info(message.content);
-                  await updateDoc(messageRef, { isNotification: true });
-                }
-              }
-            });
-          });
-        }
-      }
-    });
-  }, [chatRoomIds, myUid]);
-
-  // 리스너 해제 함수
-  const unsubscribeFromChatMessages = useCallback(() => {
-    chatRoomIds.forEach((chatRoomId) => {
-      const messagesRef = collection(
-        firestoreDB,
-        `chatRooms/${chatRoomId}/messages`
-      );
-      const unsubscribe = onSnapshot(messagesRef, () => {});
-      unsubscribe();
-    });
-  }, [chatRoomIds]);
-
-  useEffect(() => {
-    if (myUid) {
-      subscribeToChatMessages();
-    }
-    return () => {
-      unsubscribeFromChatMessages();
-    };
-  }, [
-    myUid,
-    chatRoomIds,
-    subscribeToChatMessages,
-    unsubscribeFromChatMessages,
-  ]);
 }
