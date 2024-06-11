c644a2c789ff6e86ee172d232517ccd444db1e7f
MAIN6419 <dark9737@gmail.com>
2024-06-11 15:26:40 +0900

    Feat: 채팅방 삭제 api router 생성

---
+++ pages/api/chat/[chatRoomId]/index.ts
@@ -0,0 +1,57 @@
+import { deleteChatRoom } from "@/lib/api/firebase";
+import { checkAuthorization } from "@/lib/server";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "DELETE") {
+    try {
+      const { chatRoomId } = req.query;
+
+      if (!chatRoomId) {
+        res.status(422).json({ message: "채팅방 ID가 없어요." });
+        return;
+      }
+
+      const isValidAuth = await checkAuthorization(req, res);
+
+      if (!isValidAuth.isValid) {
+        res.status(401).json({
+          message: isValidAuth.message,
+        });
+        return;
+      }
+
+      const myUid = isValidAuth?.auth?.uid;
+
+      if (!myUid) {
+        res.status(400).json({
+          message: "유저 ID가 없어요.",
+        });
+        return;
+      }
+
+      await deleteChatRoom(chatRoomId as string);
+
+      res.status(201).json({ message: "채팅방 삭제에 성공했어요." });
+    } catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        if (error.message === "존재하지 않는 채팅방이에요.") {
+          res.status(404).json({ message: error.message });
+          return;
+        } else if (
+          error.message === "채팅방 참여 인원이 있어 채팅방을 삭제할 수 없어요."
+        ) {
+          res.status(400).json({ message: error.message });
+          return;
+        }
+      }
+      res.status(500).json({
+        message: "채팅방 삭제에 실패했어요.\n잠시 후 다시 시도해주세요.",
+      });
+    }
+  }
+}
