819aa5032462b93e869860ad078c440f768dd39a
MAIN6419 <dark9737@gmail.com>
2024-06-20 14:51:26 +0900

    Fix: 로그아웃 mutation 낙관적 업데이트 제거 - 로그아웃이 안전하게 이루어지도록 로그아웃이 성공하였을때 로그아웃이 이루어지도록 수정

--- hooks/reactQuery/mutations/auth/useSignoutMutate.ts
+++ hooks/reactQuery/mutations/auth/useSignoutMutate.ts
@@ -3,7 +3,7 @@ import { queryKeys } from "@/queryKeys";
 import { authSlice } from "@/store/authSlice";
 import { AppDispatch } from "@/store/store";
 import { SignoutResposeData } from "@/types/apiTypes";
-import { AuthData, ProfileData } from "@/types/authTypes";
+import { AuthData } from "@/types/authTypes";
 import { ProductData } from "@/types/productTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse, isAxiosError } from "axios";
@@ -15,7 +15,7 @@ export default function useSignoutMutate() {
   const router = useRouter();
   const dispatch = useDispatch<AppDispatch>();
   const queryClient = useQueryClient();
-  const authQueryKey = queryKeys.auth.info._def;
+  const authQueryKey = queryKeys.auth.info().queryKey;
   const myProfileQueryKey = queryKeys.profile.my.queryKey;
   const sessionQueryKey = queryKeys.session.isExist.queryKey;
 
@@ -29,26 +29,14 @@ export default function useSignoutMutate() {
     }
   >({
     mutationFn: signout,
-    onMutate: () => {
-      const previousAuth: AuthData | unknown = queryClient.getQueriesData({
-        queryKey: authQueryKey,
-      });
-
-      const previousMyProfile: ProfileData | unknown =
-        queryClient.getQueriesData({
-          queryKey: myProfileQueryKey,
-        });
-
-      dispatch(authSlice.actions.resetAuth());
-
+    onSuccess: (response) => {
       queryClient.removeQueries({ queryKey: authQueryKey });
 
       queryClient.removeQueries({ queryKey: myProfileQueryKey });
 
-      return { previousAuth, previousMyProfile };
-    },
-    onSuccess: (response) => {
       queryClient.removeQueries({ queryKey: sessionQueryKey });
+
+      dispatch(authSlice.actions.resetAuth());
       if (
         response.data.message === "카카오 계정은 별도의 로그아웃이 필요해요."
       ) {
@@ -59,14 +47,7 @@ export default function useSignoutMutate() {
         router.replace("/signin");
       }
     },
-    onError: (error: unknown, _, context) => {
-      if (context?.previousAuth) {
-        queryClient.setQueryData(authQueryKey, context.previousAuth);
-        dispatch(authSlice.actions.saveAuth(context.previousAuth as AuthData));
-      }
-      if (context?.previousMyProfile) {
-        queryClient.setQueryData(myProfileQueryKey, context.previousMyProfile);
-      }
+    onError: (error: unknown) => {
       if (isAxiosError<{ message: string }>(error)) {
         toast.warn(error.response?.data.message);
       }
