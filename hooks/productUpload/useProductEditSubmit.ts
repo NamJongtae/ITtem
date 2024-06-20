a44779e23086c2e41b7a0be89ba79d89a5cf64d5
MAIN6419 <dark9737@gmail.com>
2024-06-20 17:52:58 +0900

    Fix: productDeatilQueryKey를 queryKeys factory에서 불러오도록 수정

--- hooks/productUpload/useProductEditSubmit.ts
+++ hooks/productUpload/useProductEditSubmit.ts
@@ -11,6 +11,7 @@ import { UploadImgResponseData } from "@/types/apiTypes";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { isAxiosError } from "axios";
+import { queryKeys } from "@/queryKeys";
 
 export default function useProductEditSubmit() {
   const [productEditLoading, setProductEditLoading] = useState(false);
@@ -20,15 +21,17 @@ export default function useProductEditSubmit() {
   const params = useParams();
   const productId = params?.productId;
   let productEditData = {} as Partial<ProductData>;
+  const productDetailQuerykKey = queryKeys.product.detail(
+    productId as string
+  ).queryKey;
 
   const productData =
-    (queryClient.getQueryData(["product", productId]) as ProductData) ||
+    queryClient.getQueryData(productDetailQuerykKey) as ProductData ||
     undefined;
 
   /**
    * 상품 수정시 수정된 데이터만 설정하는 함수
    */
-
   const setProductEditData = async (values: FieldValues) => {
     for (const key of Object.keys(values)) {
       if (key === "price") {
@@ -69,15 +72,17 @@ export default function useProductEditSubmit() {
     }
   };
 
-  // /**
-  //  * 상품 수정시 삭제된 이미지 삭제 함수
-  //  */
+  /**
+   * 상품 수정시 삭제된 이미지 삭제 함수
+   */
   const deleteImages = async (values: FieldValues) => {
     if (productEditData.imgData && values.prevImgData) {
       const productDataImgName = productData.imgData.map((data) => data.name);
+      console.log(productDataImgName);
       const prevImgDataImgName = values.prevImgData.map(
         (data: ProductImgData) => data.name
       );
+      console.log(prevImgDataImgName);
       await deleteImgToFirestore(productDataImgName, prevImgDataImgName);
     }
   };
