d93dc07697fdc873699536f7078b22cffff91230
MAIN6419 <dark9737@gmail.com>
2024-06-24 23:13:52 +0900

    Feat: _app 컴포넌트 MetaHead 태그 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,11 @@ import { Noto_Sans_KR } from "next/font/google";
 import ReactQueryProvider from "@/store/ReactQueryProvider";
 import Layout from "@/components/commons/layout/layout";
 import { ToastContainer } from "react-toastify";
-import Head from "next/head";
 import { HydrationBoundary } from "@tanstack/react-query";
 import wrapper from "@/store/store";
 import Script from "next/script";
 import { Provider } from "react-redux";
+import MetaHead from "@/components/metaHead/meta-head";
 
 const inter = Noto_Sans_KR({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -33,18 +33,7 @@ function App({ Component, ...rest }: AppProps) {
 
   return (
     <Provider store={store}>
-      <Head>
-        <meta
-          name="description"
-          content="중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요."
-          key="desc"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1"
-        />
-        <link rel="icon" href="/icons/logo.svg" />
-      </Head>
+      <MetaHead />
       <ReactQueryProvider>
         <HydrationBoundary state={props.pageProps.dehydratedState}>
           <style jsx global>{`
