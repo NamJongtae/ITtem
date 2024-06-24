7ceeb6a981d2963748f57733327913f04ff3a5d9
MAIN6419 <dark9737@gmail.com>
2024-06-24 23:13:16 +0900

    Fix: 공통적으로 사용하는 icon Head 태그에 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head title="아이템을-잇다 잇템."/>
+      <Head>
+        <link rel="icon" href="/icons/logo.svg" />
+      </Head>
       <body>
         <Main />
         <div id="portal-root"></div>
