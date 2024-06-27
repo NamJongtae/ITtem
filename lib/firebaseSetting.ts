600c47bced75f8129f483d552e1d9da06882ae4b
MAIN6419 <dark9737@gmail.com>
2024-06-27 21:10:08 +0900

    Refactoring: Firebase 초기화 최적화 - 중복 초기화를 방지하기 위해 Firebase 앱을 한 번만 초기화 - Firestore, Realtime Database, Storage 인스턴스를 정의하여 번들 크기 최적화

--- lib/firebaseSetting.ts
+++ lib/firebaseSetting.ts
@@ -1,19 +1,29 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from 'firebase/database';
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getApp, getApps, initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
-export const firestoreDB = getFirestore(app);
-export const database = getDatabase(app);
-export const storage = getStorage(app);
+// 파이어베이스 앱 초기화/설정 (이미 초기화되어있으면 기존 설정 사용)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const getFirestoreDB = async () => {
+  const { getFirestore } = await import("firebase/firestore");
+  return getFirestore(app);
+};
+
+export const getRealtimeDB = async () => {
+  const { getDatabase } = await import("firebase/database");
+  return getDatabase(app);
+};
+
+export const getStorageInstance = async () => {
+  const { getStorage } = await import("firebase/storage");
+  return getStorage(app);
+};
