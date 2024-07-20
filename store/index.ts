1bb1ca1709522d142208d88d9d80e458cb40ec02
MAIN6419 <dark9737@gmail.com>
2024-07-20 14:59:40 +0900

    Feat: store 수정 및 ReduxProvider 생성 - store wraaper 제거 makeStore export - App Router Redux 사용을 위해 ReduxProvider 컴포넌트 생성

--- store/index.ts
+++ store/index.ts
@@ -1,20 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { createWrapper } from "next-redux-wrapper";
 import logger from "redux-logger";
 import reducer from "./reducers";
 
-const makeStore = () =>
+export const makeStore = () =>
   configureStore({
     reducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
     devTools: process.env.NODE_ENV !== "production",
   });
-
-const wrapper = createWrapper(makeStore, {
-  debug: process.env.NODE_ENV !== "production",
-});
-
-export default wrapper;
 
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
