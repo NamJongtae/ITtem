ff913e923dffcec3833866496c9b6f7fd5b2b887
MAIN6419 <dark9737@gmail.com>
2024-07-21 21:10:07 +0900

    Fix: 비밀번호 변경 버튼 Link 태그로 수정 - 비밀번호 변경 페이지로 이동시켜 parallel route, interceptor route를 통해 모달창 열리도록 함

--- components/profile/profileUserInfo/profile-userInfo-card-changePw-btn.tsx
+++ components/profile/profileUserInfo/profile-userInfo-card-changePw-btn.tsx
@@ -1,24 +1,13 @@
-import useModal from "@/hooks/commons/useModal";
-import ChangePasswordModal from "../changePasswordModal/changePassword-modal";
+import Link from "next/link";
 
 export default function ProfileUserInfoCardChangePwBtn() {
-  const { isOpenModal, openModal, handleClickCloseBtn } = useModal();
-
   return (
-    <>
-      <button
-        type="button"
-        onClick={openModal}
-        className="border py-2 px-4 w-full betterhover:hover:bg-gray-100"
-      >
-        비밀번호 변경
-      </button>
-
-      {isOpenModal && (
-        <ChangePasswordModal
-          handleClickChangePwCloseBtn={handleClickCloseBtn}
-        />
-      )}
-    </>
+    <Link
+      href={"/profile/passwordChange"}
+      className="border py-2 px-4 w-full betterhover:hover:bg-gray-100 text-center"
+      scroll={false}
+    >
+      비밀번호 변경
+    </Link>
   );
 }
