99452121ffcbd2dc9f8460ad279b8c7620c295e1
MAIN6419 <dark9737@gmail.com>
2024-07-24 20:06:45 +0900

    Fix: 내지역검색 체크박스 클릭 시 checkLocation state가 변경되도록 적용 - 페이지가 바뀌어도 내지역검색이 유지되도록 redux checkLocation state 적용

--- hooks/product/useProductLocationChkBox.ts
+++ hooks/product/useProductLocationChkBox.ts
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 import useLocation from "../commons/useLocation";
-import { AppDispatch } from "@/store";
-import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "@/store";
+import { useDispatch, useSelector } from "react-redux";
 import { locationSlice } from "@/store/locationSlice";
 
 export default function useProductLocationChkBox() {
   const { fetchCurrentLocation } = useLocation();
 
-  const [checked, setChecked] = useState(false);
-
   const dispatch = useDispatch<AppDispatch>();
 
+  const checked = useSelector(
+    (state: RootState) => state.location.checkLoacation
+  );
+
   const handleClickCheck = () => {
-    setChecked((prev) => !prev);
+    dispatch(locationSlice.actions.setCheckLocation(!checked))
   };
 
   useEffect(() => {
