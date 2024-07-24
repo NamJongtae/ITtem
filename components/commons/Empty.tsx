01059e619f784e57cf0d47c8657beebb84922bf8
MAIN6419 <dark9737@gmail.com>
2024-07-24 20:08:07 +0900

    HTML&CSS: Empty 컴포넌트 이미지 비율 유지를 위해 스타일 수정

--- components/commons/Empty.tsx
+++ components/commons/Empty.tsx
@@ -10,7 +10,7 @@ export default function Empty({ message, iconSize, messageSize }: IProps) {
   return (
     <div className="w-full mx-auto flex flex-col gap-3 justify-center items-center min-h-[300px]">
       <Image
-        className={`${iconSize ? `w-[${iconSize}px]` : "w-[60px] h-[60px]"} `}
+        className={`${iconSize ? `w-[${iconSize}px] h-[${iconSize}px]` : "w-[60px] h-[60px]"} object-contain`}
         src={"/icons/empty_icon.svg"}
         alt="상품이 존재하지 않아요."
         width={iconSize ? iconSize : 60}
