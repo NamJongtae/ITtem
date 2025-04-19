c3f33c20d274acb94dfc696ed8e19d690574e312
MAIN6419 <dark9737@gmail.com>
2025-04-19 23:29:59 +0900

    Fix: API Routes Method GET => POST로 수정 - accessToken를 재발급 받는 API이므로 RestFul에 적합한 POST로 Method 수정

--- app/api/auth/refresh-token/route.ts
+++ app/api/auth/refresh-token/route.ts
@@ -1,7 +1,7 @@
 import {
   ACCESS_TOKEN_EXP,
   ACCESS_TOKEN_KEY,
-  REFRESH_TOKEN_KEY,
+  REFRESH_TOKEN_KEY
 } from "@/constants/constant";
 import { getToken, saveToken } from "@/lib/api/redis";
 import { sessionOptions } from "@/lib/server";
@@ -13,12 +13,26 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function POST() {
   try {
     const session = await getIronSession<IronSessionType>(
       cookies(),
       sessionOptions
     );
+
+    const accessToken = session.accessToken;
+
+    const decodeAccessToken = await verifyToken(
+      accessToken,
+      ACCESS_TOKEN_KEY as string
+    );
+
+    if (decodeAccessToken?.isValid) {
+      return NextResponse.json(
+        { message: "유효한 토큰입니다.", accessToken },
+        { status: 200 }
+      );
+    }
 
     const refreshToken = session.refreshToken;
 
@@ -31,18 +45,18 @@ export async function GET() {
       decodeRefreshToken?.data?.user.uid || "",
       "refreshToken"
     );
-
+    
     if (
       !redisRefreshToken ||
       redisRefreshToken !== refreshToken ||
       !decodeRefreshToken?.isValid
     ) {
-      session.destroy();
-
       const response = NextResponse.json(
-        { message: "세션이 만료됬어요." },
+        { message: "로그인이 만료됬어요." },
         { status: 401 }
       );
+
+      session.destroy();
 
       return response;
     }
@@ -50,16 +64,16 @@ export async function GET() {
     const newAccessToken = await generateToken({
       payload: {
         user: decodeRefreshToken.data?.user,
-        exp: Math.floor(Date.now() / 1000) + ACCESS_TOKEN_EXP,
+        exp: Math.floor(Date.now() / 1000) + ACCESS_TOKEN_EXP
       },
-      secret: ACCESS_TOKEN_KEY,
+      secret: ACCESS_TOKEN_KEY
     });
 
     await saveToken({
       uid: decodeRefreshToken.data?.user.uid || "",
       token: newAccessToken,
       type: "accessToken",
-      exp: Math.floor(Date.now() / 1000) + ACCESS_TOKEN_EXP,
+      exp: Math.floor(Date.now() / 1000) + ACCESS_TOKEN_EXP
     });
 
     session.accessToken = newAccessToken;
@@ -68,16 +82,17 @@ export async function GET() {
     const response = NextResponse.json(
       {
         message: "토큰이 발급 됬어요.",
-        accessToken: newAccessToken,
+        accessToken: newAccessToken
       },
       { status: 200 }
     );
+
     return response;
   } catch (error) {
     console.log(error);
     const response = NextResponse.json(
       {
-        message: "토큰 발급에 실패 했어요.",
+        message: "토큰 발급에 실패 했어요."
       },
       { status: 500 }
     );
