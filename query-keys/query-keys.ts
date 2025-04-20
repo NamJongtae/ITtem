95d98982813f60217bf62aef5daebef419f0d1e8
MAIN6419 <dark9737@gmail.com>
2025-04-20 17:03:27 +0900

    Refactor: queryKey 관리 리팩터링 - productList queryKey 삼항 연산자를 사용하지 않고, 독립적으로 분리하여 queryKey 생성 - 정적 queryKey를 사용중인 하위 쿼리키에 빈 배열이 아닌 배열 키 값을 넣도록 수정 - queryKey 값 타입 단언을 const 타입으로 선언 - 유저 queryKey키 정적 queryKey로 수정 => 동적 queryKey 불필요 - sesssionQuerykey queryFn 수정 => 세션 쿠키 존재 여부만 반환하도록 수정

--- query-keys/query-keys.ts
+++ query-keys/query-keys.ts
@@ -7,7 +7,7 @@ import {
   getProfileProductList,
   getReview,
   getSearchProductList,
-  getTodayProductList,
+  getTodayProductList
 } from "@/lib/api/product";
 import {
   getFollowers,
@@ -15,94 +15,95 @@ import {
   getMyProfile,
   getProfileReviews,
   getProfileWish,
-  getUserProfile,
+  getUserProfile
 } from "@/lib/api/profile";
 import { getPurchaseTrading, getSalesTrading } from "@/lib/api/trading";
-import { ProductCategory, ProductListType } from "@/types/product-types";
+import { ProductCategory } from "@/types/product-types";
 import {
   createQueryKeys,
-  mergeQueryKeys,
+  mergeQueryKeys
 } from "@lukemorales/query-key-factory";
 
 export const authQueryKey = createQueryKeys("auth", {
-  info: (userId?: string) => ({
-    queryKey: [userId],
-    queryFn: getUser,
-  }),
+  info: {
+    queryKey: null,
+    queryFn: getUser
+  }
 });
 
 export const productQueryKey = createQueryKeys("product", {
-  list: ({
-    productListType,
-    produdctCategory,
+  today: (limit: number = 10) => ({
+    queryKey: ["list"] as const,
+    queryFn: async ({ pageParam }) => {
+      const response = await getTodayProductList(pageParam, limit);
+      return response.data.products;
+    }
+  }),
+  category: ({
+    category,
     location,
-    limit = 10,
+    limit = 10
   }: {
-    productListType?: ProductListType;
-    produdctCategory?: ProductCategory;
+    category: ProductCategory;
     location?: string;
     limit?: number;
   }) => ({
-    queryKey: productListType
-      ? [productListType]
-      : produdctCategory && location
-      ? [produdctCategory, location]
-      : [produdctCategory],
-    queryFn: produdctCategory
-      ? async ({ pageParam }) => {
-          const response = await getCategoryProductList({
-            category: produdctCategory,
-            cursor: pageParam,
-            limit,
-            location,
-          });
-          return response.data.products;
-        }
-      : async ({ pageParam }) => {
-          const response = await getTodayProductList(pageParam, limit);
-          return response.data.products;
-        },
+    queryKey: (() => {
+      if (location) {
+        return [category, location, "list"] as const;
+      }
+      return [category, "list"] as const;
+    })(),
+    queryFn: async ({ pageParam }) => {
+      const response = await getCategoryProductList({
+        category,
+        location,
+        limit,
+        cursor: pageParam
+      });
+      return response.data.products;
+    }
   }),
   search: ({
     keyword,
     category = ProductCategory.전체,
-    limit = 10,
+    limit = 10
   }: {
     keyword?: string;
     category?: ProductCategory;
     limit?: number;
   }) => ({
-    queryKey: [keyword, category],
+    queryKey: [keyword, category] as const,
     queryFn: async ({ pageParam }) => {
       const response = await getSearchProductList({
         category,
         cursor: pageParam,
         limit,
-        keyword: (keyword as string) || "",
+        keyword: (keyword as string) || ""
       });
       return response.data.products;
-    },
+    }
   }),
   detail: (productId: string) => ({
-    queryKey: [productId],
+    queryKey: [productId] as const,
     queryFn: async () => {
       const response = await getProduct(productId);
       return response.data.product;
-    },
+    }
   }),
   review: (productId: string) => ({
-    queryKey: [productId],
+    queryKey: [productId] as const,
     queryFn: async () => {
       const response = await getReview(productId);
       return response.data.review;
-    },
+    }
   }),
   manage: ({
     currentMenu,
     status,
     search,
     menu,
-    limit = 10,
+    limit = 10
   }: {
     currentMenu: "sale" | "purchase";
     status: string;
@@ -110,14 +111,14 @@ export const productQueryKey = createQueryKeys("product", {
     menu: ProductManageMenu;
     limit?: number;
   }) => ({
-    queryKey: [currentMenu, status, search],
+    queryKey: [currentMenu, status, search] as const,
     queryFn: async ({ pageParam }) => {
       if (menu === "판매") {
         const response = await getSalesTrading({
           status,
           cursor: pageParam,
           search,
-          limit,
+          limit
         });
         return response.data.saleTrading;
       } else {
@@ -125,12 +126,12 @@ export const productQueryKey = createQueryKeys("product", {
           status,
           cursor: pageParam,
           search,
-          limit,
+          limit
         });
         return response.data.purchaseTrading;
       }
-    },
-  }),
+    }
+  })
 });
 
 export const profileQueryKey = createQueryKeys("profile", {
@@ -144,82 +145,82 @@ export const profileQueryKey = createQueryKeys("profile", {
       products: ({
         category,
         limit = 10,
-        productIds,
+        productIds
       }: {
         category: ProductCategory;
         productIds: string[];
         limit?: number;
       }) => ({
-        queryKey: [category],
+        queryKey: [category, "list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = await getProfileProductList({
             cursor: pageParam,
             category,
             limit,
-            productIds,
+            productIds
           });
           return response.data.products;
-        },
+        }
       }),
       followers: ({
         userIds,
-        limit = 10,
+        limit = 10
       }: {
         userIds: string[];
         limit?: number;
       }) => ({
-        queryKey: [] as any,
+        queryKey: ["list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = await getFollowers({
             cursor: pageParam,
             limit,
-            userIds,
+            userIds
           });
           return response.data.followers;
-        },
+        }
       }),
       followings: ({
         userIds,
-        limit = 10,
+        limit = 10
       }: {
         userIds: string[];
         limit?: number;
       }) => ({
-        queryKey: [] as any,
+        queryKey: ["list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = await getFollowings({
             cursor: pageParam,
             limit,
-            userIds,
+            userIds
           });
           return response.data.followings;
-        },
+        }
       }),
       wish: ({
         wishProductIds,
-        limit = 10,
+        limit = 10
       }: {
         wishProductIds: string[];
         limit?: number;
       }) => ({
-        queryKey: ["wish"],
+        queryKey: ["list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = await getProfileWish({
             wishProductIds,
             cursor: pageParam,
-            limit,
+            limit
           });
           return response.data.products;
-        },
+        }
       }),
       reviews: ({ uid, limit = 10 }: { uid: string; limit: number }) => ({
-        queryKey: [] as any,
+        queryKey: ["list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = getProfileReviews({ uid, cursor: pageParam, limit });
           return (await response).data.reviews;
-        },
-      }),
-    },
+        }
+      })
+    }
   },
   user: (userId: string) => ({
     queryKey: [userId],
@@ -231,94 +232,97 @@ export const profileQueryKey = createQueryKeys("profile", {
       products: ({
         category,
         limit = 10,
-        productIds,
+        productIds
       }: {
         category: ProductCategory;
         productIds: string[];
         limit?: number;
       }) => ({
-        queryKey: [category],
+        queryKey: [category, "list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = await getProfileProductList({
             cursor: pageParam,
             category,
             limit,
-            productIds,
+            productIds
           });
           return response.data.products;
-        },
+        }
       }),
       followers: ({
         userIds,
-        limit = 10,
+        limit = 10
       }: {
         userIds: string[];
         limit?: number;
       }) => ({
-        queryKey: [] as any,
+        queryKey: ["list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = await getFollowers({
             cursor: pageParam,
             limit,
-            userIds,
+            userIds
           });
           return response.data.followers;
-        },
+        }
       }),
       followings: ({
         userIds,
-        limit = 10,
+        limit = 10
       }: {
         userIds: string[];
         limit?: number;
       }) => ({
-        queryKey: [] as any,
+        queryKey: ["list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = await getFollowings({
             cursor: pageParam,
             limit,
-            userIds,
+            userIds
           });
           return response.data.followings;
-        },
+        }
       }),
       reviews: ({ limit = 10 }: { limit: number }) => ({
-        queryKey: [] as any,
+        queryKey: ["list"] as const,
         queryFn: async ({ pageParam }) => {
           const response = getProfileReviews({
             uid: userId,
             cursor: pageParam,
-            limit,
+            limit
           });
           return (await response).data.reviews;
-        },
-      }),
-    },
-  }),
+        }
+      })
+    }
+  })
 });
 
 export const notificationQueryKey = createQueryKeys("notification", {
   messages: (limit: number = 10) => ({
-    queryKey: [] as any,
+    queryKey: ["list"] as const,
     queryFn: async ({ pageParam }) => {
       const response = await getNotificationMessage({
         lastKey: pageParam,
-        limit,
+        limit
       });
 
       return {
         messages: response.data.messages,
-        nextKey: response.data.nextKey,
+        nextKey: response.data.nextKey
       };
-    },
-  }),
+    }
+  })
 });
 
 export const sessionQueryKey = createQueryKeys("session", {
   isExist: {
     queryKey: null,
-    queryFn: getSessionCookies,
-  },
+    queryFn: async () => {
+      const response = await getSessionCookies();
+      return response.data?.ok;
+    }
+  }
 });
 
 export const queryKeys = mergeQueryKeys(
