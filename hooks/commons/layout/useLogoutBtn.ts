ae148891a44cf1d8dee9c97a8fdcb7cb2691174d
MAIN6419 <dark9737@gmail.com>
2025-04-20 18:48:11 +0900

    Fix: 로그아웃 핸들러 함수 유저 로딩 설정 추가 - 로그아웃 시 빈 유저 정보가 나오지 않도록 유저 로딩 상태를 true로 설정

--- hooks/commons/layout/useLogoutBtn.ts
+++ hooks/commons/layout/useLogoutBtn.ts
@@ -1,10 +1,12 @@
 import useSignoutMutate from "@/hooks/react-query/mutations/auth/useSignoutMutate";
+import useAuthStore from "@/store/auth-store";
 
 export default function useLogoutBtn() {
   const { signoutMutate } = useSignoutMutate();
-
+  const { actions } = useAuthStore((state) => state);
   const handleClickLogout = () => {
-    signoutMutate(undefined);
+    actions.setIsLoading(true);
+    signoutMutate();
   };
 
   return { handleClickLogout };
