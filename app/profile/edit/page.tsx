6ce77cff8d9d96887afd7b5e0e1a511afc2aa010
MAIN6419 <dark9737@gmail.com>
2025-04-20 17:48:09 +0900

    Fix: 프로필 수정 페이지 proefile prefetch 로직 제거 - 일반적으로 프로필 수정 페이지는 프로필 페이지를 거쳐 오기 때문에 profile prefetch 불필요 - 만약, 직접 프로필 수정 페이지 접근시 CSR에서 프로필을 불러오도록 처리

--- app/profile/edit/page.tsx
+++ app/profile/edit/page.tsx
@@ -1,52 +1,5 @@
 import ProfileEditForm from "@/components/profile-edit/profile-edit-form";
-import customAxios from "@/lib/customAxios";
-import { sessionOptions } from "@/lib/server";
-import { queryKeys } from "@/query-keys/query-keys";
-import { IronSessionData } from "@/types/api-types";
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from "@tanstack/react-query";
-import { cookies } from "next/headers";
 
-async function prefetchMyProfile(queryClient: QueryClient) {
-  const { getIronSession } = await import("iron-session");
-  const session = await getIronSession<IronSessionData>(
-    cookies(),
-    sessionOptions
-  );
-  const sessionCookie = cookies().get("session");
-  const cookieHeader = sessionCookie
-    ? `${sessionCookie.name}=${sessionCookie.value}`
-    : "";
-  const myProfileQueryKeyConfig = queryKeys.profile.my;
-  if (session.refreshToken) {
-    await queryClient.prefetchQuery({
-      queryKey: myProfileQueryKeyConfig.queryKey,
-      queryFn: async () => {
-        try {
-          const response = await customAxios("/api/profile", {
-            headers: {
-              Cookie: cookieHeader,
-            },
-          });
-          return response.data.profile;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    });
-  }
-}
-
-export default async function ProfileEdit() {
-  const queryClient = new QueryClient();
-  await prefetchMyProfile(queryClient);
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProfileEditForm />
-    </HydrationBoundary>
-  );
+export default function ProfileEdit() {
+  return <ProfileEditForm />;
 }
