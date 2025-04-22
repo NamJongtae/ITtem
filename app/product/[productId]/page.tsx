fe85c053c954064328bde92ff2529ac755a2ec47
MAIN6419 <dark9737@gmail.com>
2025-04-22 19:33:10 +0900

    Refactor: UX 향상을 위한 상품 상세 데이터 비동기 처리 컴포넌트 분리 및 SkeletonUI 적용 - 상품 상세 데이터 비동기 처리 ProductDetailContainer 컴포넌트 생성 - 상품 상세 페이지 Suspense fallback에 적용할 SkeletonUI 컴포넌트 생성 - 상품 상세 페이지 상품 상세 데이터 비동기 처리 컴포넌트 Suspense fallback SkeletonUI 적용

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,20 +1,8 @@
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate
-} from "@tanstack/react-query";
-import { getIronSession } from "iron-session";
-import { cookies } from "next/headers";
-import { sessionOptions } from "@/lib/server";
-import { getProduct, incrementViewCount } from "@/lib/api/product";
-import customAxios from "@/lib/customAxios";
-import { queryKeys } from "@/query-keys/query-keys";
-import { IronSessionData } from "@/types/api-types";
-import ProductDetailPage from "@/components/product-detail/product-detail-page";
 import { BASE_URL } from "@/constants/constant";
 import { Suspense } from "react";
-import Loading from "@/app/loading";
-import { redirect } from "next/navigation";
+import ProductDetailContainer from "@/components/product-detail/product-detail-container";
+import { getProduct } from "@/lib/api/product";
+import ProductDetailSkeletionUI from "@/components/product-detail/product-detail-skeletionUI";
 
 export async function generateMetadata({
   params
@@ -44,76 +32,18 @@ export async function generateMetadata({
   };
 }
 
-async function fetchProductData({
-  productId,
-  queryClient
-}: {
-  productId: string;
-  queryClient: QueryClient;
-}) {
-  const productQueryKeyConfing = queryKeys.product.detail(productId);
-
-  await queryClient.prefetchQuery({
-    queryKey: productQueryKeyConfing.queryKey,
-    queryFn: productQueryKeyConfing.queryFn
-  });
-}
-
-async function fetchProfileData() {
-  const session = await getIronSession<IronSessionData>(
-    cookies(),
-    sessionOptions
-  );
-  const sessionCookie = cookies().get("session");
-  const cookieHeader = sessionCookie
-    ? `${sessionCookie.name}=${sessionCookie.value}`
-    : "";
-  if (session.refreshToken) {
-    try {
-      const response = await customAxios("/api/profile", {
-        headers: {
-          Cookie: cookieHeader
-        }
-      });
-      return response.data.profile;
-    } catch (error) {
-      if (error instanceof Error) {
-        if (error.message === "Expired AccessToken.") {
-          const { cookies } = await import("next/headers");
-          const cookie = cookies();
-          const currentURL = cookie.get("X-Requested-URL")?.value || "/";
-          redirect(`${BASE_URL}/refresh-token?next=${currentURL}`);
-        }
-      }
-    }
-  }
-}
-
 export default async function ProductDetail({
   params
 }: {
   params: { productId: string | undefined };
 }) {
-  const myProfileQueryKeyConfig = queryKeys.profile.my;
-  const queryClient = new QueryClient();
-  const productId = params?.productId;
-
-  if (productId) {
-    await incrementViewCount(productId);
-    await Promise.all([
-      fetchProductData({ productId, queryClient }),
-      queryClient.fetchQuery({
-        queryKey: myProfileQueryKeyConfig.queryKey,
-        queryFn: fetchProfileData
-      })
-    ]);
-  }
-
   return (
-    <Suspense fallback={<Loading />}>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <ProductDetailPage />
-      </HydrationBoundary>
-    </Suspense>
+    <>
+      <Suspense
+        fallback={<ProductDetailSkeletionUI userUid={params.productId} />}
+      >
+        <ProductDetailContainer params={params} />
+      </Suspense>
+    </>
   );
 }
