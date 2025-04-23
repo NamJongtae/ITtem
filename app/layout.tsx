743e0cf9620c984d3f7d1d1abe0f36bf77811c28
MAIN6419 <dark9737@gmail.com>
2025-04-23 19:50:47 +0900

    Fix: layout 컴포넌트에서 async 제거하여 ReactQueryProvider 훅 에러 해결 - layout.tsx가 async 함수일 경우 서버에서 해당 훅이 실행되면서 'Invalid hook call' 오류가 발생 - 이를 해결하기 위해 layout 컴포넌트를 동기 함수로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "@/styles/globals.css";
-import ReactQueryProvider from "@/store/ReactQueryProvider";
 import Layout from "@/components/commons/layout/layout";
 import { ToastContainer } from "react-toastify";
 import { Suspense } from "react";
 import Loading from "@/app/loading";
 import { BASE_URL } from "@/constants/constant";
+import ReactQueryProvider from '@/store/ReactQueryProvider';
 const inter = Noto_Sans_KR({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -20,23 +20,23 @@ export const metadata: Metadata = {
   verification: {
     google: "c6hLQ83ILZIlJb0fwdyrw71DGHnTFaI7hmmORy7fpk0",
     other: {
-      "naver-site-verification": "8a189164663805b9eeea137c43336f57fa7bdcf2",
-    },
+      "naver-site-verification": "8a189164663805b9eeea137c43336f57fa7bdcf2"
+    }
   },
   icons: {
-    icon: "/icons/logo.svg",
+    icon: "/icons/logo.svg"
   },
   openGraph: {
     url: BASE_URL,
     title: "ITtem",
     description:
-      "중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요.",
-  },
+      "중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요."
+  }
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
-  signin,
+  signin
 }: Readonly<{
   children: React.ReactNode;
   signin: React.ReactNode;
@@ -44,29 +44,29 @@ export default async function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
-          <ReactQueryProvider>
-            <Suspense fallback={<Loading />}>
-              <Layout>
-                {signin}
-                <main className={"flex-grow mt-[113px] md:mt-[127px]"}>
-                  {children}
-                </main>
+        <ReactQueryProvider>
+          <Suspense fallback={<Loading />}>
+            <Layout>
+              {signin}
+              <main className={"flex-grow mt-[113px] md:mt-[127px]"}>
+                {children}
+              </main>
 
-                <ToastContainer
-                  position="top-center"
-                  limit={1}
-                  closeOnClick={true}
-                  closeButton={true}
-                  pauseOnHover={false}
-                  draggable={true}
-                  autoClose={2000}
-                  pauseOnFocusLoss={false}
-                  theme="light"
-                  hideProgressBar={true}
-                />
-              </Layout>
-            </Suspense>
-          </ReactQueryProvider>
+              <ToastContainer
+                position="top-center"
+                limit={1}
+                closeOnClick={true}
+                closeButton={true}
+                pauseOnHover={false}
+                draggable={true}
+                autoClose={2000}
+                pauseOnFocusLoss={false}
+                theme="light"
+                hideProgressBar={true}
+              />
+            </Layout>
+          </Suspense>
+        </ReactQueryProvider>
         <div id="portal-root"></div>
       </body>
     </html>
