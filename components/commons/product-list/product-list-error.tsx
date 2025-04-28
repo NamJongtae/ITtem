38727e94c647adb150f3dd3ced4357da8238eef5
MAIN6419 <dark9737@gmail.com>
2025-04-28 15:38:57 +0900

    Fix: 상품 목록 에러 메세지 'TODAY' 에러 메세지 'RECOMMED' 에러 메세지로 교체

--- components/commons/product-list/product-list-error.tsx
+++ components/commons/product-list/product-list-error.tsx
@@ -9,8 +9,8 @@ export default function ProductListError({ productListType }: IProps) {
   return (
     <Empty
       message={`${
-        productListType === "TODAY"
-          ? "오늘의 상품을 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
+        productListType === "RECOMMEND"
+          ? "오늘의 추천 상품을 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
           : productListType === "CATEGORY" || productListType === "PROFILE"
           ? "상품을 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
           : "검색 결과를 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
