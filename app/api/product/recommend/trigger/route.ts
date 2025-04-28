f95ca556f28149b366330230157914857fc269e4
MAIN6419 <dark9737@gmail.com>
2025-04-28 15:07:26 +0900

    Feat: Vercel Cron을 통한 추천 상품 목록 생성 API 추가 - Vercel Cron은 GET 요청만 지원 - 내부적으로 POST 요청을 트리거하는 GET API 작성

---
+++ app/api/product/recommend/trigger/route.ts
@@ -0,0 +1,40 @@
+import customAxios from "@/lib/customAxios";
+import { isAxiosError } from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const CORN_KEY = process.env.CRON_SECRET;
+
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  try {
+    const response = await customAxios.post(
+      `/api/product/recommend`,
+      {},
+      {
+        headers: { Authorization: `Bearer ${CORN_KEY}` }
+      }
+    );
+    return NextResponse.json(
+      {
+        message: response.data.message,
+        products: response.data.products
+      },
+      { status: response.status }
+    );
+  } catch (error) {
+    if (isAxiosError(error)) {
+      const errorMessage =
+        error.response?.data?.message || "알 수 없는 에러가 발생했어요.";
+
+      return NextResponse.json(
+        { message: errorMessage },
+        { status: error.response?.status || 500 }
+      );
+    }
+  }
+}
