03e060218815289bed38af20d4b5f57bc45f565b
MAIN6419 <dark9737@gmail.com>
2025-04-28 15:37:03 +0900

    Feat: 추천 상품 컴포넌트 및 추천 상품 목록 컴포넌트 생성

---
+++ components/home/recommend-product.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import { Suspense } from 'react';
+import Spinner from '../commons/spinner';
+import RecommendProductList from './recommend-product-list';
+
+export default function RecommendProduct() {
+  return (
+    <section>
+        <h2 className="flex justify-center items-center font-semibold text-xl sm:text-2xl mt-20 mb-5 gap-2">
+        <Image src={"/icons/star.svg"} alt='' width={29} height={28}/> 오늘의 추천 상품
+        </h2>
+
+        <p className="text-center">
+          오늘의 <strong>추천 상품</strong>이에요.
+        </p>
+        <p className="text-center mb-5">원하는 상품이 있는지 확인해보세요.</p>
+
+        <Suspense
+          fallback={
+            <div className="flex max-w-[1024px] mx-auto pt-32 justify-center items-center">
+              <Spinner />
+            </div>
+          }
+        >
+          <RecommendProductList />
+        </Suspense>
+      </section>
+  )
+}
