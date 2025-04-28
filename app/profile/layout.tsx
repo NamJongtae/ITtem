2c155952407256eef8ee911a710a04d0b5300c68
MAIN6419 <dark9737@gmail.com>
2025-04-29 00:04:28 +0900

    Fix: 프로필 페이지 병렬 라우트 Suspense 직접 적용 및 Loading 중복 문제 해결 - 프로필 수정, 비밀번호 수정 병렬 라우트 처리용 Layout 파일 수정 - Link prefetch={false} 설정에도 불구하고 Loading 컴포넌트 중복 호출 문제 발생 - loading.tsx 대신 Suspense를 명시적으로 적용하여 fallback UI 제어

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,3 +1,8 @@
+import Loading from "@/components/commons/loading";
+import ProfileDetailSkeletonUI from "@/components/profile/detail/profile-detail-skeletonUI";
+import ProfileUserInfoSkeletonUI from "@/components/profile/user-info/profile-user-info-skeletonUI";
+import { Suspense } from "react";
+
 interface IProps {
   children: React.ReactNode;
   edit: React.ReactNode;
@@ -6,10 +11,19 @@ interface IProps {
 
 export default function Layout({ children, edit, passwordChange }: IProps) {
   return (
-    <>
+    <Suspense fallback={<Loading />}>
       {edit}
       {passwordChange}
-      {children}
-    </>
+      <Suspense
+        fallback={
+          <>
+            <ProfileUserInfoSkeletonUI my={true} />
+            <ProfileDetailSkeletonUI my={true} />
+          </>
+        }
+      >
+        {children}
+      </Suspense>
+    </Suspense>
   );
 }
