3a1e67616dc7441e8bfe2cbb22b94f09ed9b71be
MAIN6419 <dark9737@gmail.com>
2025-04-29 14:02:12 +0900

    Styles: popular-product-list-skeletonUI 스타일 수정

--- components/home/popular-product-list-skeletonUI.tsx
+++ components/home/popular-product-list-skeletonUI.tsx
@@ -1,9 +1,5 @@
 export default function PopularProductListSkeletonUI() {
   return (
-    <div className="flex gap-[30px] w-full h-[340px] animate-pulse">
-      {[...Array(4)].map((_, i) => (
-        <div key={i} className="flex-1 w-full h-full bg-gray-200" />
-      ))}
-    </div>
+    <div className="w-full h-[340px] bg-gray-200 animate-pulse" />
   );
 }
