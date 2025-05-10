5625b903541189856e0b35a39fe3f11c629a3878
MAIN6419 <dark9737@gmail.com>
2025-05-10 18:37:05 +0900

    Refactor: useProductManageStatus hook 수정 - 전달 받는 props initalManageStatus -> initialProductManageStatus로 이름 수정 - 재사용하지 않는 toQueryStatus 함수 외부로 분리하지 않고 내부에서 정의 - 사용하지 않는 productStatusMapper 파일 삭제

--- hooks/product-manage/useProductManageStatus.ts
+++ hooks/product-manage/useProductManageStatus.ts
@@ -1,24 +1,37 @@
 import { ProductManageStaus } from "@/components/product-manage/product-manage-page";
-import { toQueryStatus } from "@/lib/productStatusMapper";
 import { useState } from "react";
 import { useGetQuerys } from "../commons/useGetQuerys";
 import { useCustomRouter } from "../commons/useCustomRouter";
 
 interface IParams {
-  initialManageStatus: ProductManageStaus;
+  initialProductManageStatus: ProductManageStaus;
 }
 
 export default function useProductManageStatus({
-  initialManageStatus
+  initialProductManageStatus
 }: IParams) {
-  const [manageStatus, setManageStatus] =
-    useState<ProductManageStaus>(initialManageStatus);
+  const [manageStatus, setManageStatus] = useState<ProductManageStaus>(
+    initialProductManageStatus
+  );
   const { search } = useGetQuerys("search");
   const { navigate } = useCustomRouter();
 
-  const handleChangeManageStatus = (manageType: ProductManageStaus) => {
-    setManageStatus(manageType);
-    const status = toQueryStatus(manageType);
+  const toQueryStatus = (manageStatus: ProductManageStaus) => {
+    switch (manageStatus) {
+      case "취소/반품 내역":
+        return "CANCEL_END/RETURN_END";
+      case "거래완료 내역":
+        return "TRADING_END";
+      case "취소/반품 거절 내역":
+        return "CANCEL_REJECT/RETURN_REJECT";
+      default:
+        return "TRADING";
+    }
+  };
+
+  const handleChangeManageStatus = (manageStatus: ProductManageStaus) => {
+    setManageStatus(manageStatus);
+    const status = toQueryStatus(manageStatus);
 
     const newUrl = `/product/manage${
       search ? `?search=${search}&status=${status}` : `?status=${status}`
