53773b3698e2b4609243b881f5f7b0a35aab4eed
MAIN6419 <dark9737@gmail.com>
2025-05-11 21:56:11 +0900

    Fix: imgStyle 잘못된 삼항 연산자 수정 status -> productDetailData?.status로 수정

--- components/product-detail/content/product-detail-content-img.tsx
+++ components/product-detail/content/product-detail-content-img.tsx
@@ -14,7 +14,7 @@ export default function ProductDetailContentImg({ productDetailData }: IProps) {
   });
 
   const imgStyle = `before:absolute before:inset-0 before:bg-gray-700 before:bg-opacity-50 before:z-10 before:text-white before:text-3xl before:font-semibold before:flex before:justify-center before:items-center ${
-    status === ProductStatus.soldout
+    productDetailData?.status === ProductStatus.soldout
       ? "before:content-['판매완료']"
       : "before:content-['거래중']"
   }`;
