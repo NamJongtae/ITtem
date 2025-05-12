d5e3b6b2f8aebe073d579eaa541ae9f13f27034e
MAIN6419 <dark9737@gmail.com>
2025-05-12 16:10:05 +0900

    Styles: 공통 로딩 컴포넌트 스타일 수정

--- components/commons/loading.tsx
+++ components/commons/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="absolute z-50 inset-0 bg-white bg-opacity-50">
+    <div className="fixed z-[99] inset-0 bg-white bg-opacity-50">
       <div className="absolute center flex gap-2">
         <span className="sr-only">Loading...</span>
         <div className="h-3 w-3 rounded-full animate-bounceOpacity [animation-delay:-0.3s]"></div>
