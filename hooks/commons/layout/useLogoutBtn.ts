d1ea8b51ba10211dcd86a1629bfcd9746b4729b3
MAIN6419 <dark9737@gmail.com>
2025-05-12 16:08:44 +0900

    Fix: 불필요한 loading 제거 - 로그아웃 시 별도의 로그아웃 페이지에서 처리되므로 유저 정보 로딩 불필요

--- hooks/commons/layout/useLogoutBtn.ts
+++ hooks/commons/layout/useLogoutBtn.ts
@@ -1,11 +1,8 @@
 import useSignoutMutate from "@/hooks/react-query/mutations/auth/useSignoutMutate";
-import useAuthStore from "@/store/auth-store";
 
 export default function useLogoutBtn() {
   const { signoutMutate } = useSignoutMutate();
-  const { actions } = useAuthStore((state) => state);
   const handleClickLogout = () => {
-    actions.setIsLoading(true);
     signoutMutate();
   };
 
