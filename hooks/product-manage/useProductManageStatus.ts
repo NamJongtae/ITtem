faddd641d1dd64f60b8d62faf97744f12a768a06
MAIN6419 <dark9737@gmail.com>
2025-05-13 23:07:27 +0900

    Fix: useProductManageStatus hook 수정 - useProductManageUrlQuerys hook를 사용하여 query를 받아오도록 추가 - manageStatus를 useState 대신 url query로 관리하도록 수정 - toQueryStats 대신 productStatusEncoder 객체를 통해 queryStatus로 변환 - productStatusParser 객체를 이용하여 현재 stats를 변환하여 반환

--- hooks/product-manage/useProductManageStatus.ts
+++ hooks/product-manage/useProductManageStatus.ts
@@ -1,43 +1,28 @@
-import { ProductManageStaus } from "@/components/product-manage/product-manage-page";
-import { useState } from "react";
-import { useGetQuerys } from "../commons/useGetQuerys";
 import { useCustomRouter } from "../commons/useCustomRouter";
+import {
+  productStatusEncoder,
+  productStatusParser
+} from "@/lib/productManageStatusMapper";
+import useProductManageUrlQuerys from "./useProductManageUrlQuerys";
+import { ProductManageStatusType } from "@/types/product-types";
 
-interface IParams {
-  initialProductManageStatus: ProductManageStaus;
-}
-
-export default function useProductManageStatus({
-  initialProductManageStatus
-}: IParams) {
-  const [manageStatus, setManageStatus] = useState<ProductManageStaus>(
-    initialProductManageStatus
-  );
-  const { search } = useGetQuerys("search");
+export default function useProductManageStatus() {
+  const { menu, search, status } = useProductManageUrlQuerys();
   const { navigate } = useCustomRouter();
 
-  const toQueryStatus = (manageStatus: ProductManageStaus) => {
-    switch (manageStatus) {
-      case "취소/반품 내역":
-        return "CANCEL_END/RETURN_END";
-      case "거래완료 내역":
-        return "TRADING_END";
-      case "취소/반품 거절 내역":
-        return "CANCEL_REJECT/RETURN_REJECT";
-      default:
-        return "TRADING";
-    }
-  };
-
-  const handleChangeManageStatus = (manageStatus: ProductManageStaus) => {
-    setManageStatus(manageStatus);
-    const status = toQueryStatus(manageStatus);
+  const handleChangeManageStatus = (manageStatus: ProductManageStatusType) => {
+    const queryStatus = productStatusEncoder[manageStatus];
 
     const newUrl = `/product/manage${
-      search ? `?search=${search}&status=${status}` : `?status=${status}`
+      search
+        ? `?menu=${menu}&search=${search}&status=${status}`
+        : `?menu=${menu}&status=${queryStatus}`
     }`;
     navigate({ type: "push", url: newUrl });
   };
 
-  return { manageStatus, handleChangeManageStatus };
+  return {
+    productManageStatus: productStatusParser[status],
+    handleChangeManageStatus
+  };
 }
