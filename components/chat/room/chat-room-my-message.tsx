616d8a5b2d30f192d1f068bd1a2af1ad5eca16d9
MAIN6419 <dark9737@gmail.com>
2025-05-14 22:07:20 +0900

    Styles: 채팅 메세지 줄바꿈이 적용되도록 스타일 수정

--- components/chat/room/chat-room-my-message.tsx
+++ components/chat/room/chat-room-my-message.tsx
@@ -12,7 +12,7 @@ export default function ChatRoomMyMessage({ message }: IProps) {
       className="max-w-[80%] self-end flex items-center gap-3"
     >
       <div className="flex items-end flex-row-reverse gap-2">
-        <p className="text-sm border py-1 px-3 rounded-lg rounded-tr-none bg-gray-200 text-black">
+        <p className="text-sm border py-1 px-3 rounded-lg rounded-tr-none bg-gray-200 text-black whitespace-pre-wrap">
           {message.content}
         </p>
         <time className="text-[10px] text-gray-400 shrink-0">
