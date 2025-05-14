3c4922fc916d8399eee0a166ad8e6def21c67351
MAIN6419 <dark9737@gmail.com>
2025-05-14 23:58:28 +0900

    Fix: menu active 상태 조건부 연산자 수정 - menu query가 'sale', 'purchase' 모두 아닌 경우 'sale' menu가 기본값으로 활성화 되도록 조건부 수정

--- components/product-manage/product-manage-menu.tsx
+++ components/product-manage/product-manage-menu.tsx
@@ -6,12 +6,19 @@ interface IProps {
 }
 
 export default function ProductManageMenu({ menu, handleClickMenu }: IProps) {
+  const isSaleMenu = menu === "sale";
+  const isPurchaseManu = menu === "purchase";
+
   return (
     <ul className="flex gap-3 font-sm text-gray-400 font-semibold shrink-0 sm:pl-3">
       <li className="relative before:-right-[7px] before:absolute before:top-[5px] before:h-4 before:w-[1px] before:bg-gray-600">
         <button
           onClick={() => handleClickMenu("sale")}
-          className={menu === "sale" ? "text-red-500" : "text-inherit"}
+          className={
+            isSaleMenu || (!isSaleMenu && !isPurchaseManu)
+              ? "text-red-500"
+              : "text-inherit"
+          }
         >
           판매
         </button>
@@ -19,7 +26,7 @@ export default function ProductManageMenu({ menu, handleClickMenu }: IProps) {
       <li>
         <button
           onClick={() => handleClickMenu("purchase")}
-          className={menu === "purchase" ? "text-red-500" : "text-inherit"}
+          className={isPurchaseManu ? "text-red-500" : "text-inherit"}
         >
           구매
         </button>
