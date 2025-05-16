0f724b6f915fefc8eaf3383a944575c0b1b58fc3
MAIN6419 <dark9737@gmail.com>
2025-05-17 00:27:15 +0900

    Fix: PopularProductListSkeletonUI 수정

--- components/home/popular-product-list-skeletonUI.tsx
+++ components/home/popular-product-list-skeletonUI.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useSliderSkeletonUILogic from "@/hooks/home/useSliderSkeletonUILogic";
+import Image from "next/image";
 
 export default function PopularProductListSkeletonUI() {
   const { gap, slidesToShow } = useSliderSkeletonUILogic();
@@ -11,7 +12,29 @@ export default function PopularProductListSkeletonUI() {
       style={{ gap: `${gap}px` }}
     >
       {[...Array(slidesToShow)].map((_, i) => (
-        <div key={i} className="w-full h-full max-w-xs bg-gray-200" />
+        <div
+          key={i}
+          className="w-full h-full mx-auto rounded shadow-lg animate-pulse border max-w-xs"
+        >
+          <div className="mx-auto group flex w-full h-full flex-col overflow-hidden">
+            <div className="w-full h-full bg-gray-300">
+              <Image
+                src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                width={300}
+                height={300}
+                alt="loading..."
+              />
+            </div>
+            <div className="px-[10px] py-[15px]">
+              <div className="h-[14px] bg-gray-300 mb-2"></div>
+              <div className="h-[14px] bg-gray-300 w-2/3 mb-2"></div>
+              <div className="flex justify-between">
+                <div className="w-20 h-3 bg-gray-300"></div>
+                <div className="w-12 h-3 bg-gray-300"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
