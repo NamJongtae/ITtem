dd19f2d698d8cec06bfc9254ffe258ecd540f1b1
MAIN6419 <dark9737@gmail.com>
2025-05-16 23:50:13 +0900

    Refactor: ProductList 컴포넌트 리팩토링 - useInfiniteProductListQuery 제거 대신 props를 통해 받아도록 수정 - ProductList → ProductListUI로 컴포넌트 수정

---
+++ components/commons/product-list/product-list-ui.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import ProductListItem from "./product-list-item";
+import { ProductCategory, ProductData } from "@/types/product-types";
+import Empty from "../empty";
+import ProductListSkeletonUI from "./product-list-skeletonUI";
+import useInfiniteScrollObserver from "@/hooks/commons/useInfiniteScrollObserver";
+import InfiniteScrollTarget from "../InfiniteScrollTarget";
+import InfiniteScrollEndMessage from "../InfiniteScrollEndMessage";
+
+interface IProps {
+  data: ProductData[] | undefined;
+  fetchNextPage: () => void;
+  isFetchingNextPage: boolean;
+  hasNextPage: boolean;
+  emptyMessage: string;
+  productCategory?: ProductCategory;
+}
+
+export default function ProductListUI({
+  data,
+  fetchNextPage,
+  isFetchingNextPage,
+  hasNextPage,
+  emptyMessage,
+  productCategory
+}: IProps) {
+  const { ref } = useInfiniteScrollObserver({
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage
+  });
+
+  if (data?.length === 0) {
+    return <Empty message={emptyMessage} />;
+  }
+
+  return (
+    <div className="max-w-[1024px] mx-auto pb-12">
+      <ul className="grid gap-5 grid-cols-autoFill mt-6">
+        <>
+          {data?.map((item) => (
+            <ProductListItem
+              key={item._id}
+              data={item}
+              category={productCategory}
+            />
+          ))}
+          {isFetchingNextPage && <ProductListSkeletonUI listCount={12} />}
+          {<InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />}
+        </>
+      </ul>
+
+      <InfiniteScrollEndMessage
+        message="더 이상 상품이 없어요."
+        data={data}
+        hasNextPage={hasNextPage}
+      />
+    </div>
+  );
+}
