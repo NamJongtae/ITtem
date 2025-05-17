1fa5ea8ba56eb37aa2c49094d37baf4913e26bab
MAIN6419 <dark9737@gmail.com>
2025-05-17 13:54:16 +0900

    Feat: ProductItemSkeletonUI 컴포넌트 생성 ProductListSkleletonUI 컴포넌트 수정 - ProductListSkeletonUI 컴포넌트를 ProductItemSkeletonUI 컴포넌트로 분리

---
+++ components/commons/product-list/product-item-skeletonUI.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function ProductItemSkeletonUI() {
+  return (
+    <li className="w-full h-full mx-auto rounded shadow-lg animate-pulse border max-w-xs">
+      <div className="mx-auto group flex w-full h-full flex-col overflow-hidden">
+        <div className="w-full h-full bg-gray-300">
+          <Image
+            src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+            width={300}
+            height={300}
+            alt="loading..."
+          />
+        </div>
+        <div className="px-[10px] py-[15px]">
+          <div className="h-[14px] bg-gray-300 mb-2"></div>
+          <div className="h-[14px] bg-gray-300 w-2/3 mb-2"></div>
+          <div className="flex justify-between">
+            <div className="w-20 h-3 bg-gray-300"></div>
+            <div className="w-12 h-3 bg-gray-300"></div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
