165a912fc218cfd6c5e878cb1e747e1806d77939
MAIN6419 <dark9737@gmail.com>
2025-05-17 19:26:41 +0900

    Fix: ProductListSkeletonUI 컴포넌트 수정

--- components/commons/product-list/product-list-skeletonUI.tsx
+++ components/commons/product-list/product-list-skeletonUI.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 export default function ProductListSkeletonUI({ listCount = 10 }: IProps) {
   return (
-    <ul className="max-w-[1024px] mx-auto grid gap-5 grid-cols-autoFill mt-6 px-8">
+    <ul className="max-w-[1024px] mx-auto grid gap-5 grid-cols-autoFill my-6 px-8">
       {Array.from({ length: listCount }).map((_, index) => (
         <ProductItemSkeletonUI key={index} />
       ))}
