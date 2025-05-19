0315c34340c5358cc0b110be02cf4c856c995a27
MAIN6419 <dark9737@gmail.com>
2025-05-20 00:05:10 +0900

    Refactor: 로딩 및 에러 처리 제거 - Suspense 및 ErrorBoundary를 통해 로딩 및 에러를 처리하기 때문에 로딩 및 에러 처리 제거

--- components/profile/detail/review/profile-detail-review-list.tsx
+++ components/profile/detail/review/profile-detail-review-list.tsx
@@ -1,25 +1,18 @@
 import useProfileReviewsInfiniteQuery from "@/hooks/react-query/queries/profile/useProfileReviewsInfiniteQuery";
 import Empty from "@/components/commons/empty";
-import { isAxiosError } from "axios";
 import ProfileDetailReviewItem from "./profile-detail-review-item";
-import ProfileDetailReviewSkeletonUI from "./profile-detail-review-skeletonUI";
 import useInfiniteScrollObserver from "@/hooks/commons/useInfiniteScrollObserver";
 import InfiniteScrollTarget from "@/components/commons/InfiniteScrollTarget";
 import InfiniteScrollEndMessage from "@/components/commons/InfiniteScrollEndMessage";
+import ProfileDetailReviewItemSkeletonUI from "./profile-detail-review-item-skeletonUI";
 
 interface IProps {
   uid: string | undefined;
 }
 
 export default function ProfileDetailReviewList({ uid }: IProps) {
-  const {
-    data,
-    isLoading,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-    error
-  } = useProfileReviewsInfiniteQuery({ uid: uid || "" });
+  const { data, isFetchingNextPage, hasNextPage, fetchNextPage } =
+    useProfileReviewsInfiniteQuery({ uid: uid || "" });
 
   const { ref } = useInfiniteScrollObserver({
     fetchNextPage,
@@ -27,34 +20,25 @@ export default function ProfileDetailReviewList({ uid }: IProps) {
     isFetchingNextPage
   });
 
-  if ((error && !data) || data?.length === 0) {
-    return (
-      <Empty
-        message={
-          isAxiosError<{ message: string }>(error)
-            ? error.response?.data.message || ""
-            : "리뷰 목록이 없어요."
-        }
-      />
-    );
+  if (data?.length === 0) {
+    return <Empty message={"리뷰 목록이 없어요."} />;
   }
 
   return (
     <>
       <ul className="flex flex-col gap-5 mt-12">
-        {isLoading ? (
-          <ProfileDetailReviewSkeletonUI
-            listCount={data?.length || 0 < 10 ? data?.length : 10}
-          />
-        ) : (
-          <>
-            {data?.map((review) => (
-              <ProfileDetailReviewItem key={review._id} review={review} />
-            ))}
-            {isFetchingNextPage && <ProfileDetailReviewSkeletonUI />}
-            <InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />
-          </>
-        )}
+        <>
+          {data?.map((review) => (
+            <ProfileDetailReviewItem key={review._id} review={review} />
+          ))}
+          {isFetchingNextPage &&
+            Array(8)
+              .fill("")
+              .map((_, index) => (
+                <ProfileDetailReviewItemSkeletonUI key={index} />
+              ))}
+          <InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />
+        </>
       </ul>
       <InfiniteScrollEndMessage
         message="더 이상 리뷰가 없어요."
