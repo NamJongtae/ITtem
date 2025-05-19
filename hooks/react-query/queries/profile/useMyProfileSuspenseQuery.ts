07a21cf69c8c43d67fbc17b8567abfbc9e2640bc
MAIN6419 <dark9737@gmail.com>
2025-05-19 21:05:29 +0900

    Feat: useMyProfileSuspenseQuery hook 생성 - useMyProfile hook과 별도로 suspense 처리를 위해 hook 생성

---
+++ hooks/react-query/queries/profile/useMyProfileSuspenseQuery.ts
@@ -0,0 +1,32 @@
+import { queryKeys } from "@/query-keys/query-keys";
+import useAuthStore from "@/store/auth-store";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+
+export default function useMyProfileSuspenseQuery() {
+  const user = useAuthStore((state) => state.user);
+  const queryKeyConfing = queryKeys.profile.my;
+
+  const {
+    data,
+    isPending,
+    isError: myProfileError
+  } = useSuspenseQuery({
+    queryFn: queryKeyConfing.queryFn,
+    queryKey: queryKeyConfing.queryKey,
+    staleTime: 30 * 1000
+  });
+
+  useEffect(() => {
+    if (myProfileError) {
+      toast.warn(
+        "나의 프로필 정보를 가져오는데 실패했어요.\n로그인 정보를 확인해주세요."
+      );
+    }
+  }, [myProfileError]);
+
+  const myProfileLoading = !!user && isPending;
+
+  return { myProfileData: data, myProfileLoading, myProfileError };
+}
