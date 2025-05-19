02c7fb46781772020b683095ffca5dd14d717eda
MAIN6419 <dark9737@gmail.com>
2025-05-19 21:51:39 +0900

    Fix: next 쿼리 파라미터 처리 개선으로 모든 query 반영되도록 수정 - 기존에는 next 파라미터 외 추가 query가 포함된 경우, next 값만 반영되어 전체 query가 누락됨  - decodeURIComponent + replace 로직을 통해 전체 쿼리 문자열을 반영하도록 수정

--- app/refresh-token/page.tsx
+++ app/refresh-token/page.tsx
@@ -5,18 +5,18 @@ import { useRouter, useSearchParams } from "next/navigation";
 import axios, { isAxiosError } from "axios";
 import { BASE_URL } from "@/constants/constant";
 import { RegenerateAccessTokenResponseData } from "@/types/api-types";
-import Loading from '@/components/commons/loading';
-
+import Loading from "@/components/commons/loading";
 
 export default function RefreshToken() {
   const searchParams = useSearchParams();
-  const next = searchParams.get("next") || "/";
+  const next =
+    decodeURIComponent(searchParams.toString()).replace("next=", "") || "/";
   const router = useRouter();
 
   useEffect(() => {
     const fechAccessToken = async () => {
       try {
-        await axios.post(`${BASE_URL}/api/auth/refresh-token`, {
+        await axios.post(`${BASE_URL}/api/auth/refresh-token`, null, {
           withCredentials: true
         });
         // SSR 재요청을 위한 서버 리다이렉트
