8cec76976f431d316a1a521215bb6bd6003b8ef5
MAIN6419 <dark9737@gmail.com>
2025-05-20 00:32:54 +0900

    Refactor: 로딩 및 에러 처리, 빈 데이터 처리 제거 - Suspense 및 ErrorBoundary를 통해 로딩 및 에러를 처리하기 때문에 로딩 및 에러 처리 제거 - 상위 컴포넌트에서 빈 데이터 처리를 하므로 빈 데이터 처리 제거

--- components/profile/detail/follow/profile-detail-follow-list.tsx
+++ components/profile/detail/follow/profile-detail-follow-list.tsx
@@ -1,12 +1,10 @@
-import Empty from "@/components/commons/empty";
-import { isAxiosError } from "axios";
 import useFollowListInfiniteQuery from "@/hooks/react-query/queries/profile/useFollowListInfiniteQuery";
 import { ProfileData } from "@/types/auth-types";
 import ProfileDetailFollowItem from "./profile-detail-follow-Item";
-import ProfileDetailFollowSkeletonUI from "./profile-detail-follow-skeletonUI";
 import useInfiniteScrollObserver from "@/hooks/commons/useInfiniteScrollObserver";
 import InfiniteScrollTarget from "@/components/commons/InfiniteScrollTarget";
 import InfiniteScrollEndMessage from "@/components/commons/InfiniteScrollEndMessage";
+import ProfileDetailFollowItemSkeletonUI from "./profile-detail-follow-item-skeletonUI";
 
 interface IProps {
   isFollowers: boolean;
@@ -20,18 +18,12 @@ export default function ProfileDetailFollowList({
   const userIds = isFollowers
     ? profileData?.followers
     : profileData?.followings;
-  const {
-    data,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-    error,
-    isLoading
-  } = useFollowListInfiniteQuery({
-    isFollowers,
-    userIds,
-    uid: profileData?.uid
-  });
+  const { data, isFetchingNextPage, hasNextPage, fetchNextPage } =
+    useFollowListInfiniteQuery({
+      isFollowers,
+      userIds,
+      uid: profileData?.uid
+    });
 
   const { ref } = useInfiniteScrollObserver({
     fetchNextPage,
@@ -39,35 +31,23 @@ export default function ProfileDetailFollowList({
     isFetchingNextPage
   });
 
-  if (error && !data) {
-    <Empty
-      message={
-        isAxiosError<{ message: string }>(error)
-          ? error.response?.data.message || ""
-          : ""
-      }
-    />;
-  }
-  if (data?.length === 0) {
-    return (
-      <Empty message={`${isFollowers ? "팔로워" : "팔로잉"} 목록이 없어요.`} />
-    );
-  }
-
   return (
     <>
       <ul className="grid gap-8 grid-cols-autoFill_180">
-        {isLoading ? (
-          <ProfileDetailFollowSkeletonUI />
-        ) : (
+        {
           <>
             {data?.map((item) => (
               <ProfileDetailFollowItem key={item.uid} data={item} />
             ))}
-            {isFetchingNextPage && <ProfileDetailFollowSkeletonUI />}
+            {isFetchingNextPage &&
+              Array(8)
+                .fill("")
+                .map((_, index) => (
+                  <ProfileDetailFollowItemSkeletonUI key={index} />
+                ))}
             <InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />
           </>
-        )}
+        }
       </ul>
       <InfiniteScrollEndMessage
         message={`더 이상 ${isFollowers ? "팔로워" : "팔로잉"} 유저가 없어요.`}
