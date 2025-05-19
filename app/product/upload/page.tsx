e10c07d023b3687711a508981d7695cdd17e620f
MAIN6419 <dark9737@gmail.com>
2025-05-19 20:51:22 +0900

    Fix: ProductUpload 컴포넌트 Suspense 적용

--- app/product/upload/page.tsx
+++ app/product/upload/page.tsx
@@ -1,20 +1,22 @@
-import ProductUploadPage from '@/components/product-upload/product-upload-page';
+import ProductUploadPage from "@/components/product-upload/product-upload-page";
 import { BASE_URL } from "@/constants/constant";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import ProductUploadLoading from "./loading";
 
 export const metadata: Metadata = {
   metadataBase: new URL(`${BASE_URL}/product/upload`),
   title: "ITtem | 상품판매",
   openGraph: {
     url: `${BASE_URL}/product/upload`,
-    title: "ITtem | 상품판매",
-  },
+    title: "ITtem | 상품판매"
+  }
 };
 
-export default function Upload() {
+export default function ProductUpload() {
   return (
-    <>
-      <ProductUploadPage isEdit={false} />
-    </>
+    <Suspense fallback={<ProductUploadLoading isEdit={false} />}>
+      <ProductUploadPage />
+    </Suspense>
   );
 }
