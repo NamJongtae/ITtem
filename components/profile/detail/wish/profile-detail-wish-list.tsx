cc4f532ccf3dde39646bc521d9f33cea99b13f04
MAIN6419 <dark9737@gmail.com>
2025-05-20 00:03:12 +0900

    Refactor: 로딩 및 에러 처리 제거 - Suspense 및 ErrorBoundary를 통해 로딩 및 에러를 처리하기 때문에 로딩 및 에러 처리 제거

--- components/profile/detail/wish/profile-detail-wish-list.tsx
+++ components/profile/detail/wish/profile-detail-wish-list.tsx
@@ -1,29 +1,23 @@
 import Empty from "@/components/commons/empty";
-import { isAxiosError } from "axios";
 import ProfileDetailWishItem from "./profile-detail-wish-item";
-import ProfileDetailWishSkeletonUI from "./profile-detail-wish-skeletonUI";
+
 import ProfileDetailWishDelBtn from "./profile-detail-wish-del-btn";
 import useInfiniteScrollObserver from "@/hooks/commons/useInfiniteScrollObserver";
 import InfiniteScrollTarget from "@/components/commons/InfiniteScrollTarget";
 import InfiniteScrollEndMessage from "@/components/commons/InfiniteScrollEndMessage";
 import useProfileWishInfiniteQuery from "@/hooks/react-query/queries/profile/useProfileWishInfiniteQuery";
 import useWishDeleteSelector from "@/hooks/profile/useWishDeleteSelector";
+import ProfileDetailWishItemSkeltonUI from "./profile-detail-wish-item-skeletonUI";
 
 interface IProps {
   wishProductIds: string[] | undefined;
 }
 
 export default function ProfileDetailWishList({ wishProductIds }: IProps) {
-  const {
-    data,
-    isLoading,
-    isFetchingNextPage,
-    fetchNextPage,
-    hasNextPage,
-    error
-  } = useProfileWishInfiniteQuery({
-    wishProductIds: wishProductIds || []
-  });
+  const { data, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useProfileWishInfiniteQuery({
+      wishProductIds: wishProductIds || []
+    });
 
   const { selectedWish, onClickSelectAll, onClickCheckBox } =
     useWishDeleteSelector({
@@ -36,30 +30,19 @@ export default function ProfileDetailWishList({ wishProductIds }: IProps) {
     isFetchingNextPage
   });
 
+  if (data?.length === 0) {
+    return <Empty message={"찜 목록이 없어요."} />;
+  }
+
   return (
     <>
       <ProfileDetailWishDelBtn
         selectedWish={selectedWish}
         onClickSelectAll={onClickSelectAll}
       />
-      {(error && !data) || data?.length === 0 ? (
-        <Empty
-          message={
-            isAxiosError<{ message: string }>(error)
-              ? error.response?.data.message || ""
-              : "찜 목록이 없어요."
-          }
-        />
-      ) : null}
 
       <ul className="grid grid-col-1 md:grid-cols-2 gap-3">
-        {isLoading ? (
-          <ProfileDetailWishSkeletonUI
-            listCount={
-              wishProductIds?.length || 0 < 10 ? wishProductIds?.length : 10
-            }
-          />
-        ) : (
+        {
           <>
             {data?.map((data) => (
               <ProfileDetailWishItem
@@ -69,10 +52,15 @@ export default function ProfileDetailWishList({ wishProductIds }: IProps) {
                 selectedWish={selectedWish}
               />
             ))}
-            {isFetchingNextPage && <ProfileDetailWishSkeletonUI />}
+            {isFetchingNextPage &&
+              Array(8)
+                .fill("")
+                .map((_, index) => (
+                  <ProfileDetailWishItemSkeltonUI key={index} />
+                ))}
             <InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />
           </>
-        )}
+        }
       </ul>
       <InfiniteScrollEndMessage
         message="더 이상 찜한 상품이 없어요."
