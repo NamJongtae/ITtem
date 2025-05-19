e681dbcf93c508aae3c987dffa0876d51ae4195b
MAIN6419 <dark9737@gmail.com>
2025-05-20 00:31:34 +0900

    Refacotr: SuspenseErrorBoundary wrapper 컴포넌트 적용 - 로딩 및 에러 처리를 위해 SuspenseErrorBoundary wrapper 컴포넌트 적용 - 팔로우/팔로잉 목록이 없는 경우 Empty 컴포넌트를 반환하도록 조건부 렌더링 추가

--- components/profile/detail/follow/profile-detail-followers.tsx
+++ components/profile/detail/follow/profile-detail-followers.tsx
@@ -1,5 +1,8 @@
 import { ProfileData } from "@/types/auth-types";
 import ProfileDetailFollowList from "./profile-detail-follow-list";
+import SuspenseErrorBoundary from "@/components/commons/suspense-error-boundary";
+import Empty from "@/components/commons/empty";
+import ProfileDetailFollowSkeletonUI from "./profile-detail-follow-skeletonUI";
 
 interface IProps {
   profileData: ProfileData | undefined;
@@ -11,7 +14,25 @@ export default function ProfileDetailFollowers({ profileData }: IProps) {
       <h2 className="font-semibold border-b pb-3 mb-5">
         팔로워 {profileData?.followers?.length || 0}명
       </h2>
-      <ProfileDetailFollowList isFollowers={true} profileData={profileData} />
+      {profileData?.followers?.length === 0 ? (
+        <Empty message={"팔로워 목록이 없어요."} />
+      ) : (
+        <SuspenseErrorBoundary
+          suspenseFallback={<ProfileDetailFollowSkeletonUI />}
+          errorFallback={
+            <Empty
+              message={
+                "팔로워 목록을 불러오지 못했어요.\n 잠시후 다시 시도해주세요."
+              }
+            />
+          }
+        >
+          <ProfileDetailFollowList
+            isFollowers={true}
+            profileData={profileData}
+          />
+        </SuspenseErrorBoundary>
+      )}
     </div>
   );
 }
--- components/profile/detail/follow/profile-detail-followings.tsx
+++ components/profile/detail/follow/profile-detail-followings.tsx
@@ -1,5 +1,8 @@
+import SuspenseErrorBoundary from "@/components/commons/suspense-error-boundary";
 import ProfileDetailFollowList from "./profile-detail-follow-list";
 import { ProfileData } from "@/types/auth-types";
+import ProfileDetailFollowSkeletonUI from "./profile-detail-follow-skeletonUI";
+import Empty from "@/components/commons/empty";
 
 interface IProps {
   profileData: ProfileData | undefined;
@@ -11,7 +14,25 @@ export default function ProfileDetailFollowings({ profileData }: IProps) {
       <h2 className="font-semibold border-b pb-3 mb-5">
         팔로잉 {profileData?.followings?.length || 0}명
       </h2>
-      <ProfileDetailFollowList isFollowers={false} profileData={profileData} />
+      {profileData?.followers?.length === 0 ? (
+        <Empty message={"팔로잉 목록이 없어요."} />
+      ) : (
+        <SuspenseErrorBoundary
+          suspenseFallback={<ProfileDetailFollowSkeletonUI />}
+          errorFallback={
+            <Empty
+              message={
+                "팔로잉 목록을 불러오지 못했어요.\n 잠시후 다시 시도해주세요."
+              }
+            />
+          }
+        >
+          <ProfileDetailFollowList
+            isFollowers={true}
+            profileData={profileData}
+          />
+        </SuspenseErrorBoundary>
+      )}
     </div>
   );
 }
