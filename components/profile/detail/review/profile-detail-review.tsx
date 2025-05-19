c4dac84a777e8f8d4d69903713c8f37549e18cd0
MAIN6419 <dark9737@gmail.com>
2025-05-20 00:04:53 +0900

    Refacotr: SuspenseErrorBoundary wrapper 컴포넌트 적용 - 로딩 및 에러 처리를 위해 SuspenseErrorBoundary wrapper 컴포넌트 적용

--- components/profile/detail/review/profile-detail-review.tsx
+++ components/profile/detail/review/profile-detail-review.tsx
@@ -2,6 +2,8 @@ import { ReivewInfoData } from "@/types/auth-types";
 import ProfileDetailReviewInfo from "./profile-detail-review-info";
 import ProfileDetailReviewList from "./profile-detail-review-list";
 import Empty from "@/components/commons/empty";
+import SuspenseErrorBoundary from "@/components/commons/suspense-error-boundary";
+import ProfileDetailReviewSkeletonUI from "./profile-detail-review-skeletonUI";
 
 interface IProps {
   reviewInfo: ReivewInfoData | undefined;
@@ -16,10 +18,19 @@ export default function ProfileDetailReview({ reviewInfo, uid }: IProps) {
       </h2>
 
       {reviewInfo ? (
-        <>
+        <SuspenseErrorBoundary
+          suspenseFallback={<ProfileDetailReviewSkeletonUI />}
+          errorFallback={
+            <Empty
+              message={
+                "리뷰 목록을 불러올 수 없어요.\n잠시 후 다시 시도해주세요."
+              }
+            />
+          }
+        >
           <ProfileDetailReviewInfo reviewInfo={reviewInfo} />
           <ProfileDetailReviewList uid={uid} />
-        </>
+        </SuspenseErrorBoundary>
       ) : (
         <Empty message="리뷰가 존재하지 않아요." />
       )}
