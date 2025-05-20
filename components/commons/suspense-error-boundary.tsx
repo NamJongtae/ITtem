0aef4e144db8cf9d9d34938efa5c9ce26d98cdeb
MAIN6419 <dark9737@gmail.com>
2025-05-20 14:01:36 +0900

    Fix: suspenseFallback, errorFallback props를 옵셔널 속성으로 변경 - suspenseFallback, errorFallback를 사용하지 않는 경우가 있어 props를 옵셔널 속성으로 변경

--- components/commons/suspense-error-boundary.tsx
+++ components/commons/suspense-error-boundary.tsx
@@ -4,18 +4,20 @@ import React, { Suspense } from "react";
 import { ErrorBoundary } from "./ErrorBoundary";
 
 interface IProps {
-  suspenseFallback: React.ReactNode;
-  errorFallback: React.ReactNode;
+  suspenseFallback?: React.ReactNode;
+  errorFallback?: React.ReactNode;
   children: React.ReactNode;
+  errorMessage?: string;
 }
 
 export default function SuspenseErrorBoundary({
-  suspenseFallback,
-  errorFallback,
+  suspenseFallback = null,
+  errorFallback = null,
+  errorMessage,
   children
 }: IProps) {
   return (
-    <ErrorBoundary fallback={errorFallback}>
+    <ErrorBoundary fallback={errorFallback} errorMessage={errorMessage}>
       <Suspense fallback={suspenseFallback}>{children}</Suspense>
     </ErrorBoundary>
   );
