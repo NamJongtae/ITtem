597c1557cee23b2883a7bcd257cdb6dbc47e021a
MAIN6419 <dark9737@gmail.com>
2025-05-20 20:20:06 +0900

    Fix: 전역 error 컴포넌트 수정 - error.message를 사용자에게 직접적 으로 노출하지 않도록 수정 - 개발 모드에서 에러메세지가 출력되도록 수정 - Error 컴포넌트 UI 수정

--- app/error.tsx
+++ app/error.tsx
@@ -16,25 +16,29 @@ export default function Error({ error }: IProps) {
 
   useEffect(() => {
     if (error) {
-      console.log(error);
+      if (process.env.NODE_ENV === "development") {
+        console.log(error);
+      }
     }
   }, [error]);
+
   return (
-    <div className="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
-      <div className="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
-        <p className="text-2xl md:text-3xl lg:text-5xl font-bold tracking-wider text-gray-500 mt-4">
+    <div className="h-[calc(100vh-165px)] md:h-[calc(100vh-179px)] w-full px-16 md:px-0 flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8">
+        <Image
+          className="w-[60px] md:w-[80px]"
+          src={"/icons/error-icon.svg"}
+          alt=""
+          width={80}
+          height={80}
+        />
+        <p className="text-4xl md:text-5xl font-bold tracking-wider text-gray-500 mt-4">
           Error
         </p>
-        <p className="text-gray-500 mt-4 pb-4 text-center leading-7">
-          {error.message ? (
-            error.message
-          ) : (
-            <>
-              죄송합니다. <br />
-              알 수 없는 에러가 발생하였습니다. <br />
-              잠시 후 다시 시도해주세요.
-            </>
-          )}
+        <p className="text-gray-500 mt-6 pb-4 text-center text-base md:text-lg leading-7 md:leading-8 whitespace-pre border-b-2">
+          {
+            "죄송합니다.\n알 수 없는 문제가 발생하였습니다.\n잠시 후 다시 시도해주세요."
+          }
         </p>
         <button
           className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-4 rounded transition duration-150"
