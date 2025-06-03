ce59e2536299814715bac0ad8637d2102e206571
MAIN6419 <dark9737@gmail.com>
2025-06-03 12:21:02 +0900

    Refactor: 타입 검사 로직 추가 - totalReviewScore와 totalReviewCount의 타입이 number가 아닌 경우 에러를 던지도록 처리

--- domains/user/profile/utils/calculateReviewStar.ts
+++ domains/user/profile/utils/calculateReviewStar.ts
@@ -5,6 +5,12 @@ export default function calculateReviewStar({
   totalReviewScore: number;
   totalReviewCount: number;
 }) {
+  if (
+    typeof totalReviewScore !== "number" ||
+    typeof totalReviewCount !== "number"
+  ) {
+    throw Error("올바른 타입이 제공되지 않아 리뷰 점수 계산에 실패했습니다.");
+  }
   const reviewStar =
     (totalReviewScore || 0) > 0 && (totalReviewCount || 0) > 0
       ? (totalReviewScore || 0) / (totalReviewCount || 0)
