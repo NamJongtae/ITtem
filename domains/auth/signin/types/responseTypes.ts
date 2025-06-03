8ce9b9b49b7bb6f8ab48386eeca5e30d2f60703d
MAIN6419 <dark9737@gmail.com>
2025-06-03 16:53:50 +0900

    Chore: KaKaoAuthAccessTokenResponseData refresh_token_expires_in 타입 수정 - string 타입을 number 타입으로 수정

--- domains/auth/signin/types/responseTypes.ts
+++ domains/auth/signin/types/responseTypes.ts
@@ -30,7 +30,7 @@ export interface KaKaoAuthAccessTokenResponseData {
   refresh_token: string;
   id_token: string;
   expires_in: number;
-  refresh_token_expires_in: string;
+  refresh_token_expires_in: number;
   scope: string;
 }
 
