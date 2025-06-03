acba578e116931146b13136f63e909bcdc917ae7
MAIN6419 <dark9737@gmail.com>
2025-06-03 12:08:52 +0900

    Fix: 불필요한 인자 값 제거 - 이전 이미지 파일 이름만 전달되면 프로필 이미지를 제거할 수 있기 때문에 불필요한 프로필 수정 파일 이름값 제거

--- domains/user/profile/utils/deleteProfileImgToFirestore.ts
+++ domains/user/profile/utils/deleteProfileImgToFirestore.ts
@@ -1,20 +1,18 @@
 import { getStorageInstance } from "@/shared/common/utils/firebaseSetting";
 
 export default async function deleteProfileImgToFirestore(
-  profileDataImgName: string,
-  prevImgDataImgName: string
+  prevProfileImgDataImgName: string
 ) {
   try {
     const firebaseStorage = await import("firebase/storage");
     const storage = await getStorageInstance();
     const { deleteObject, ref: storageRef } = firebaseStorage;
 
-    if (!profileDataImgName || !prevImgDataImgName) return;
-    if (profileDataImgName !== prevImgDataImgName) {
-      await deleteObject(
-        storageRef(storage, `images/profile/${prevImgDataImgName}`)
-      );
-    }
+    if (!prevProfileImgDataImgName) return;
+
+    await deleteObject(
+      storageRef(storage, `images/profile/${prevProfileImgDataImgName}`)
+    );
   } catch (error) {
     console.error(error);
     throw error;
