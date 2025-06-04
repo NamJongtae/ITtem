fd2d7c7810a40a768f05e0eb68c389e904271d38
MAIN6419 <dark9737@gmail.com>
2025-06-04 23:52:43 +0900

    Test: useAddRecentProduct hook 단위 테스트 코드 추가

--- shared/layout/__test__/unit/hooks/useAddRecentProduct.test.ts
+++ shared/layout/__test__/unit/hooks/useAddRecentProduct.test.ts
@@ -20,7 +20,7 @@ beforeEach(() => {
 });
 
 describe("useAddRecentProduct", () => {
-  it("recentProductData를 localStorage에 저장한다", () => {
+  it("recentProductData를 localStorage에 저장합니다.", () => {
     const recentProductData: RecentProductData = {
       productId: "1",
       productName: "테스트 상품",
@@ -44,7 +44,7 @@ describe("useAddRecentProduct", () => {
     );
   });
 
-  it("새로운 상품이 추가되면 기존 상품목록 맨 앞에 추가된다.", () => {
+  it("새로운 상품이 추가되면 기존 상품목록 맨 앞에 추가됩니다.", () => {
     const recentProductData: RecentProductData = {
       productId: "2",
       productName: "새로운 상품",
@@ -71,7 +71,7 @@ describe("useAddRecentProduct", () => {
     );
   });
 
-  it("이미 있는 상품은 중복 제거하고 맨 앞에 추가해야 한다", () => {
+  it("이미 있는 상품은 중복 제거하고 맨 앞에 추가해야 합니다.", () => {
     const recentProductData: RecentProductData = {
       productId: "1",
       productName: "중복 상품",
@@ -112,7 +112,7 @@ describe("useAddRecentProduct", () => {
     );
   });
 
-  it("최근 상품이 10개일 경우 새 항목 추가 후 가장 오래된 항목을 제거해야 한다", () => {
+  it("최근 상품이 10개일 경우 새 항목 추가 후 가장 오래된 항목을 제거해야 합니다.", () => {
     const recentProduct: RecentProductData = {
       productId: "11",
       productName: "새로운 상품",
@@ -144,7 +144,7 @@ describe("useAddRecentProduct", () => {
     );
   });
 
-  it("productDetailData가 undefined면 아무 동작도 하지 않아야 한다", () => {
+  it("productDetailData가 undefined면 아무 동작도 하지 않아야 합니다.", () => {
     renderHook(() => useAddRecentProduct(undefined));
 
     expect(mockSetItem).not.toHaveBeenCalled();
