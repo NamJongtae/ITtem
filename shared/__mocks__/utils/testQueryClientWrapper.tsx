21b44428b8947b3f95e55bf425737917db7b1bb7
MAIN6419 <dark9737@gmail.com>
2025-06-06 19:12:00 +0900

    Fest: 테스트 코드용 createQueryClientWrapper 유틸 함수 생성

---
+++ shared/__mocks__/utils/testQueryClientWrapper.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+export const createQueryClientWrapper = () => {
+  const queryClient = new QueryClient();
+
+  const Wrapper = ({ children }: { children: ReactNode }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+
+  Wrapper.displayName = "QueryClientTestWrapper";
+  return Wrapper;
+};
