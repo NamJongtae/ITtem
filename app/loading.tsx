ac522c420d5ddd844cdad1472fb5b3a390e46c44
MAIN6419 <dark9737@gmail.com>
2025-06-10 19:18:43 +0900

    Fix: root(home) 페이지 및 product 페이지 최상위 로딩 컴포넌트 수정 - 페이지 상위 로딩 컴포넌트가 별도의 UI를 가진 컴포넌트인 경우 - 하위 세그먼트 페이지 생성 이전 상위 컴포넌트 로딩이 노출되고 이후 하위 세그먼트 로딩이 노출되어 UX 저하 - 페이지 상위 로딩 컴포넌트를 공통 Loading 컴포넌트로 수정하여 UX 저하 문제 해결

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,34 +1,5 @@
-import ProductListSkeletonUI from "@/domains/product/shared/components/product-list/ProductListSkeletonUI";
-import PopularProductListSkeletonUI from "@/domains/home/components/PopularProductListSkeletonUI";
-import Image from "next/image";
+import Loading from "@/shared/common/components/Loading";
 
 export default function HomeLoading() {
-  return (
-    <div className="max-w-[1024px] w-full h-full mx-auto px-8">
-      {/* 배너 */}
-      <div className="w-full h-ful bg-gray-200 aspect-[256/75] animate-pulse" />
-
-      {/* 인기 상품 */}
-      <div className="flex justify-center items-center font-semibold text-xl sm:text-2xl mt-8 mb-5 gap-2">
-        <Image src={"/icons/fire.svg"} alt="" width={21} height={28} /> 인기
-        상품
-      </div>
-      <div className="text-sm sm:text-base text-center mb-5">
-        언제 팔릴 지 모르는 조회수가 가장 높은 <strong>인기상품</strong>
-      </div>
-      <PopularProductListSkeletonUI />
-
-      {/* 추천 상품 */}
-      <div className="flex justify-center items-center font-semibold text-xl sm:text-2xl mt-20 mb-5 gap-2">
-        <Image src={"/icons/star.svg"} alt="" width={29} height={28} /> 오늘의
-        추천 상품
-      </div>
-
-      <div className="text-sm sm:text-base text-center mb-5">
-        원하는 상품이 있는지 확인해보세요.
-      </div>
-
-      <ProductListSkeletonUI listCount={12} />
-    </div>
-  );
+  return <Loading />;
 }
--- app/product/loading.tsx
+++ app/product/loading.tsx
@@ -1,26 +1,5 @@
-import ProductListSkeletonUI from "@/domains/product/shared/components/product-list/ProductListSkeletonUI";
+import Loading from "@/shared/common/components/Loading";
 
 export default function ProductLoading() {
-  return (
-    <div className="max-w-[1024px] mx-auto w-full h-full mt-8 px-8 animate-pulse">
-      {/* 네비게이션 */}
-      <div className="flex gap-5 justify-between w-full items-center mb-5">
-        <div className="bg-gray-200 w-40 h-5" />
-        <div className="bg-gray-200 w-28 h-5" />
-      </div>
-
-      {/* 카테고리 메뉴 PC */}
-      <div className="hidden sm:grid sm:grid-cols-5 my-6 gap-1">
-        {[...Array(14)].map((_, i) => (
-          <div key={i} className="bg-gray-200 p-5" />
-        ))}
-      </div>
-
-      {/* 카테고리 메뉴 모바일 */}
-      <div className="sm:hidden rounded-md w-[105px] h-9 bg-gray-200 ml-auto mr-[12px]" />
-
-      {/* 상품 목록 */}
-      <ProductListSkeletonUI listCount={12} />
-    </div>
-  );
+  return <Loading />;
 }
