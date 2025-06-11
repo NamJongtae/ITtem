ac5dd7ac7bb5955a666a8e75033a53c9a6479425
MAIN6419 <dark9737@gmail.com>
2025-06-11 14:30:11 +0900

    Fix: dbConnect 환경변수 체크 위치를 함수 내부로 이동하여 테스트 안정성 향상 - 기존에는 MONGODB_URI가 없을 경우 import 시점에 예외가 발생하여 테스트 불가

--- shared/common/utils/db/db.ts
+++ shared/common/utils/db/db.ts
@@ -1,17 +1,11 @@
 import _mongoose, { connect } from "mongoose";
 
-const MONGODB_URI = process.env.NEXT_SECRET_MONGODB_URI;
-
 declare global {
   // eslint-disable-next-line no-var
   var mongoose: {
     promise: ReturnType<typeof connect> | null;
     conn: typeof _mongoose | null;
   };
-}
-
-if (!MONGODB_URI) {
-  throw new Error("DB 접속 정보를 확인해주세요.");
 }
 
 let cached = global.mongoose;
@@ -21,15 +15,19 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  const MONGODB_URI = process.env.NEXT_SECRET_MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error("DB 접속 정보를 확인해주세요.");
+  }
   if (cached.conn) {
     console.log("캐싱된 커넥션을 사용합니다.");
     return cached.conn;
   }
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false,
+      bufferCommands: false
     };
-        
     cached.promise = connect(MONGODB_URI!, opts)
       .then((mongoose) => {
         console.log("DB 커넥션이 생성되었습니다.");
