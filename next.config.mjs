d533d2ff9b790b0dab18daa61c030d7867486239
NamJongtae <dark9737@gmail.com>
2025-12-31 17:10:21 +0900

    Chore: next.config.mjs turbopack 설정 수정 - experimental에서 정식 옵션으로 변경 되어 옵션 수정

--- next.config.mjs
+++ next.config.mjs
@@ -12,15 +12,14 @@ const nextConfig = {
     });
     return config;
   },
-  experimental: {
-    turbo: {
+    turbopack: {
       rules: {
         "*.svg": {
           loaders: ["@svgr/webpack"],
           as: "*.js"
         }
       }
-    }
+    
   },
   images: {
     dangerouslyAllowSVG: true,
@@ -46,35 +45,22 @@ const nextConfig = {
     ]
   }
 };
+
 const withBundleAnalyzerConfig = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true"
+  enabled: process.env.ANALYZE === "true",
 })(nextConfig);
 
 export default withSentryConfig(
-  withSentryConfig(withBundleAnalyzerConfig, {
+  withBundleAnalyzerConfig,
+  {
     org: "main-pg",
     project: "ittem",
     authToken: process.env.SENTRY_AUTH_TOKEN,
-    silent: false,
-    widenClientFileUpload: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-    sourcemaps: {
-      disable: true
-    }
-  }),
-  {
-    // 아래는 Sentry Webpack 플러그인 옵션
-    org: "main-pg", // Sentry 조직 슬러그
-    project: "ittem", // Sentry 프로젝트 슬러그
 
-    // CI 환경에서만 로그 출력 (로컬에서는 출력 X)
     silent: !process.env.CI,
 
-    widenClientFileUpload: true, // 더 많은 소스맵 업로드
-
-    disableLogger: true, // Sentry 로거 번들에서 제거
-
-    automaticVercelMonitors: true // Vercel Cron 자동 모니터링
+    sourcemaps: {
+      disable: true,
+    },
   }
 );
