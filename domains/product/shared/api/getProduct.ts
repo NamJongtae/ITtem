eeb6762048b40825f3271a9487f2665ca268e5ad
MAIN6419 <dark9737@gmail.com>
2026-01-03 22:26:02 +0900

    Refactor: Next.js 캐싱 적용을 위해 getProduct API를 customFetch로 전환

--- domains/product/shared/api/getProduct.ts
+++ domains/product/shared/api/getProduct.ts
@@ -1,14 +1,27 @@
-import { AxiosResponse } from "axios";
+import { notFound } from "next/navigation";
 import { ProductDetailResponseData } from "../../detail/types/responseTypes";
-import customAxios from "@/shared/common/utils/customAxios";
+import { BASE_URL } from "@/shared/common/constants/constant";
+import { customFetch } from "@/shared/common/utils/customFetch";
 
 export default async function getProduct(
   id: string
-): Promise<AxiosResponse<ProductDetailResponseData>> {
-  try {
-    const response = await customAxios(`/api/product/${id}`);
-    return response;
-  } catch (error) {
-    throw error;
+): Promise<ProductDetailResponseData> {
+  const res = await customFetch(`/api/product/${id}`, false, {
+    next: {
+      tags: [`product-${id}`]
+    }
+  });
+
+  if (!res.ok) {
+    if (res.status === 404) {
+      return notFound();
+    }
+    const errorData = await res.json().catch(() => ({}));
+    throw {
+      status: res.status,
+      message: errorData?.message ?? "상품 정보 조회에 실패했어요."
+    };
   }
+
+  return res.json();
 }
