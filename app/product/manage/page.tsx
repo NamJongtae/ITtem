a97b45afb15b6379bca925da3c1d7832b1829c2d
MAIN6419 <dark9737@gmail.com>
2026-01-03 23:49:36 +0900

    Refactor: 상품 상세 페이지 ISR 페이지로 전환 - generateMetadata 함수 제거

--- app/product/manage/page.tsx
+++ app/product/manage/page.tsx
@@ -1,34 +1,4 @@
 import ProductManagePage from "@/domains/product/manage/components/ProductManagePage";
-import { BASE_URL } from "@/shared/common/constants/constant";
-
-export async function generateMetadata({
-  searchParams
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  const { status } = await searchParams;
-
-  const transformStatus =
-    status === "CANCEL_END/RETURN_END"
-      ? "취소/반품 내역"
-      : status === "TRADING_END"
-        ? "거래완료 내역"
-        : status === "CANCEL_REJECT/RETURN_REJECT"
-          ? "취소/반품 거절 내역"
-          : "거래중";
-
-  const url = `${BASE_URL}/manage?status=${transformStatus}`;
-  const title = `ITtem | 상품관리-${transformStatus}`;
-
-  return {
-    metadataBase: new URL(url),
-    title,
-    openGraph: {
-      url,
-      title
-    }
-  };
-}
 
 export default function ProductManage() {
   return <ProductManagePage />;
