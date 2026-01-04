d595babcd08316e1afe10ec45d6d87d3fdd2a2cd
MAIN6419 <dark9737@gmail.com>
2026-01-04 17:26:07 +0900

    Fix: 채팅 페이지에서 불필요한 generateMetadata 제거

--- app/chat/[chatRoomId]/page.tsx
+++ app/chat/[chatRoomId]/page.tsx
@@ -1,24 +1,4 @@
-import ChatRoomPage from '@/domains/chat/room/components/ChatRoomPage';
-import { BASE_URL } from "@/shared/common/constants/constant";
-
-export async function generateMetadata({
-  params
-}: {
-  params: Promise<{ chatRoomId: string | undefined }>;
-}) {
-  const { chatRoomId } = await params;
-  const url = `${BASE_URL}/chat/${chatRoomId}`;
-  const title = "ITtem | 채팅";
-
-  return {
-    metadataBase: new URL(url),
-    title,
-    openGraph: {
-      url,
-      title
-    }
-  };
-}
+import ChatRoomPage from "@/domains/chat/room/components/ChatRoomPage";
 
 export default function ChatRoom() {
   return <ChatRoomPage />;
