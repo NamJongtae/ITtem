6f802156740b0aa97e497c882acc0d1a6f05964b
MAIN6419 <dark9737@gmail.com>
2026-01-04 16:01:39 +0900

    Refactor: Next.js 캐싱 적용을 위해 getMyProfile API를 customFetch로 전환

--- domains/user/profile/api/getMyProfile.ts
+++ domains/user/profile/api/getMyProfile.ts
@@ -1,13 +1,20 @@
-import { AxiosResponse } from "axios";
 import { ProfileResponseData } from "../types/responseTypes";
-import customAxios from "@/shared/common/utils/customAxios";
+import { customFetch } from "@/shared/common/utils/customFetch";
 
-export default async function getMyProfile(): Promise<
-  AxiosResponse<ProfileResponseData>
-> {
+export default async function getMyProfile(): Promise<ProfileResponseData> {
   try {
-    const response = await customAxios("/api/profile");
-    return response;
+    const response = await customFetch("/api/profile", true);
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(
+        errorData?.messsage
+          ? errorData?.messsage
+          : "프로필 데이터 조회에 실패했어요."
+      );
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
     throw error;
   }
