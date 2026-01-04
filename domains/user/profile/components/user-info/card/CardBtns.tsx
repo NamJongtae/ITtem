9c8b62e69bd4acfd34d6054f9282aa1f277d3cd7
MAIN6419 <dark9737@gmail.com>
2026-01-04 17:23:44 +0900

    Fix: 타 유저 프로필에서 프로필 수정 UI가 노출되는 문제 수정 - useSearchParams 기반 uid 추출 로직 제거 - route params(useParams) 기준으로 사용자 식별하도록 수정

--- domains/user/profile/components/user-info/card/CardBtns.tsx
+++ domains/user/profile/components/user-info/card/CardBtns.tsx
@@ -1,17 +1,19 @@
+import { useEffect } from "react";
 import { ProfileData } from "../../../types/profileTypes";
 import FollowBtn from "../FollowBtn";
 import ChangePasswordBtn from "./ChangePasswordBtn";
 import EditProfileBtn from "./EditProfileBtn";
-import { useGetQuerys } from "@/shared/common/hooks/useGetQuerys";
+import { useParams } from "next/navigation";
 
 interface IProps {
   profileData: ProfileData | undefined;
 }
 
 export default function UserInfoCardBtns({ profileData }: IProps) {
-  const { uid } = useGetQuerys("uid");
+  const { uid } = useParams();
 
   const isUserProfile = !!uid;
+
 
   return isUserProfile ? (
     <FollowBtn profileData={profileData} />
