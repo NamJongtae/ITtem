8b73d5e4740701ff256041af8894f4791aef8502
MAIN6419 <dark9737@gmail.com>
2026-01-06 16:08:56 +0900

    Test: getRecommendProductList 테스트 customAxios -> customFetch를 사용하도록 테스크 코드 수정

--- domains/product/shared/__test__/unit/api/getRecommendProductList.test.ts
+++ domains/product/shared/__test__/unit/api/getRecommendProductList.test.ts
@@ -1,26 +1,17 @@
 import getRecommendProductList from "@/domains/product/shared/api/getRecommendProductList";
-import customAxios from "@/shared/common/utils/customAxios";
-import { AxiosHeaders, AxiosResponse } from "axios";
+import { customFetch } from "@/shared/common/utils/customFetch";
 import { ProductListResponseData } from "../../../types/reponseTypes";
 import { ProductData } from "../../../types/productTypes";
 
-jest.mock("@/shared/common/utils/customAxios");
+jest.mock("@/shared/common/utils/customFetch");
 
 describe("getRecommendProductList API 함수 테스트", () => {
   const mockCursor = "cursor123";
   const mockLimit = 5;
 
-  const mockResponse: AxiosResponse<ProductListResponseData> = {
-    data: {
-      products: [{ _id: "product1", name: "추천상품1" } as ProductData],
-      message: "추천 상품 목록 조회에 성공했어요."
-    },
-    status: 200,
-    statusText: "ok",
-    headers: {},
-    config: {
-      headers: new AxiosHeaders()
-    }
+  const mockData: ProductListResponseData = {
+    products: [{ _id: "product1", name: "추천상품1" } as ProductData],
+    message: "추천 상품 목록 조회에 성공했어요."
   };
 
   beforeEach(() => {
@@ -28,46 +19,87 @@ describe("getRecommendProductList API 함수 테스트", () => {
   });
 
   it("cursor와 limit을 쿼리에 포함하여 GET 요청을 보냅니다.", async () => {
-    (customAxios as unknown as jest.Mock).mockResolvedValue(mockResponse);
+    const mockResponse = {
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockData)
+    };
+
+    (customFetch as jest.Mock).mockResolvedValue(mockResponse);
 
     const result = await getRecommendProductList(mockCursor, mockLimit);
 
-    expect(customAxios).toHaveBeenCalledWith(
-      `/api/product/recommend?cursor=${mockCursor}&limit=${mockLimit}`
+    expect(customFetch).toHaveBeenCalledWith(
+      `/api/product/recommend?cursor=${mockCursor}&limit=${mockLimit}`,
+      false,
+      {
+        next: {
+          revalidate: 86400,
+          tags: ["product-recommend"]
+        }
+      }
     );
-    expect(result).toEqual(mockResponse);
+    expect(result).toEqual(mockData);
   });
 
-  it("cursor가 null일 경우 cursor 없이 요청을 보냅니다..", async () => {
-    (customAxios as unknown as jest.Mock).mockResolvedValue(mockResponse);
+  it("cursor가 null일 경우 cursor 없이 요청을 보냅니다.", async () => {
+    const mockResponse = {
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockData)
+    };
+
+    (customFetch as jest.Mock).mockResolvedValue(mockResponse);
 
     await getRecommendProductList(null, mockLimit);
 
-    expect(customAxios).toHaveBeenCalledWith(
-      `/api/product/recommend?&limit=${mockLimit}`
+    expect(customFetch).toHaveBeenCalledWith(
+      `/api/product/recommend?&limit=${mockLimit}`,
+      false,
+      {
+        next: {
+          revalidate: 86400,
+          tags: ["product-recommend"]
+        }
+      }
     );
   });
 
   it("limit가 없는 경우 'limit=10'을 쿼리에 포함하여 요청을 보냅니다.", async () => {
-    (customAxios as unknown as jest.Mock).mockResolvedValue(mockResponse);
+    const mockResponse = {
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockData)
+    };
+
+    (customFetch as jest.Mock).mockResolvedValue(mockResponse);
 
     await getRecommendProductList(mockCursor);
 
-    expect(customAxios).toHaveBeenCalledWith(
-      `/api/product/recommend?cursor=${mockCursor}&limit=10`
+    expect(customFetch).toHaveBeenCalledWith(
+      `/api/product/recommend?cursor=${mockCursor}&limit=10`,
+      false,
+      {
+        next: {
+          revalidate: 86400,
+          tags: ["product-recommend"]
+        }
+      }
     );
   });
 
   it("요청 중 에러가 발생하면 예외를 던집니다.", async () => {
-    const error = new Error(
-      "추천 상품 목록 조회에 실패했어요.\n잠시 후 다시 시도해주세요."
-    );
-    (customAxios as unknown as jest.Mock).mockRejectedValue(error);
+    const errorMessage = "추천 상품 목록 조회에 실패했어요.";
+    const mockErrorResponse = {
+      ok: false,
+      status: 500,
+      json: jest.fn().mockResolvedValue({ message: errorMessage })
+    };
+
+    (customFetch as jest.Mock).mockResolvedValue(mockErrorResponse);
 
     await expect(
       getRecommendProductList(mockCursor, mockLimit)
-    ).rejects.toThrow(
-      "추천 상품 목록 조회에 실패했어요.\n잠시 후 다시 시도해주세요."
-    );
+    ).rejects.toEqual({
+      status: 500,
+      message: errorMessage
+    });
   });
 });
