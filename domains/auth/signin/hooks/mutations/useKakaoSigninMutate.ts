b8efbf041823933ebbc09e78f8c1bf019d0de301
MAIN6419 <dark9737@gmail.com>
2026-01-07 17:28:05 +0900

    Refactor: useKakaoSignupMutate 중복 로그인시 토큰 삭제 api 대신 세션 삭제 api 호출 하도록 수정

--- domains/auth/signin/hooks/mutations/useKakaoSigninMutate.ts
+++ domains/auth/signin/hooks/mutations/useKakaoSigninMutate.ts
@@ -8,8 +8,8 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse, isAxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
-import deleteAllToken from "../../../shared/common/api/deleteToken";
 import kakaoSignin from "../../../signin/api/kakaoSignin";
+import destoryDBSession from "@/domains/auth/shared/common/api/destoryDBSession";
 
 export default function useKakaoSigninMutate() {
   const queryClient = useQueryClient();
@@ -30,14 +30,23 @@ export default function useKakaoSigninMutate() {
     },
     onError: (error, variables) => {
       if (isAxiosError<{ message: string; email: string }>(error)) {
+        if (error.response?.status === 429) {
+          alert(
+            "너무 잦은 로그인으로 카카오 로그인이 일시적으로 제한 되었습니다.\n잠시 후 다시 시도해주세요."
+          );
+          router.replace("/");
+          return;
+        }
+
         if (error.response?.status == 409) {
           const isLogin = confirm(
             "제대로 로그아웃 하지 않았거나\n이미 로그인중인 아이디입니다.\n현재 사용중인 브라우저에서 로그아웃 하겠습니까?"
           );
           if (isLogin) {
             const user = variables;
-            // 토큰 삭제 로직
-            (async () => await deleteAllToken(user.id.toString()))();
+
+            // 세션 삭제 로직
+            (async () => await destoryDBSession(user.id.toString()))();
 
             router.push(
               `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&prompt=none`
