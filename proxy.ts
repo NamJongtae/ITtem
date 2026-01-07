b7e2c4672b40fd5b37b858de6965bc6ee531be0c
MAIN6419 <dark9737@gmail.com>
2026-01-07 17:29:38 +0900

    Refactor: 미들웨어 jwt 토큰 대신 db Session를 통해 페이지 접근 제한 설정 하도록 수정

--- proxy.ts
+++ proxy.ts
@@ -3,17 +3,16 @@ import {
   checkWithoutAuthPathname,
   withoutAuth
 } from "./shared/common/utils/withoutAuth";
-import { IronSession, getIronSession } from "iron-session";
-import { SESSION_OPTIONS } from "./domains/auth/shared/common/constants/constansts";
 import {
   checkWithAuthPathname,
   withAuth
 } from "./shared/common/utils/withAuth";
 
-export async function proxy(req: NextRequest, res: NextResponse) {
+export async function proxy(req: NextRequest) {
   const { pathname, href } = req.nextUrl;
 
   const response = NextResponse.next();
+
   response.cookies.set(
     "X-Requested-URL",
     encodeURIComponent(href.replace(req.nextUrl.origin, "")),
@@ -26,20 +25,17 @@ export async function proxy(req: NextRequest, res: NextResponse) {
   );
 
   const isWithOutAuth = checkWithoutAuthPathname(pathname);
-
   const isWithAuth = checkWithAuthPathname(pathname);
 
-  const session = await getIronSession<
-    IronSession<{ accessToken: string; refreshToken: string; exp: number }>
-  >(req, res, SESSION_OPTIONS);
+  const sessionId = req.cookies.get("sessionId")?.value;
 
-  const refreshToken = session.refreshToken;
-
-  if (isWithOutAuth && refreshToken) {
+  // 로그인 상태인데 비로그인 전용 페이지 접근
+  if (isWithOutAuth && sessionId) {
     return withoutAuth(req);
   }
 
-  if (isWithAuth && !refreshToken) {
+  // 로그인 필요 페이지인데 세션 없음
+  if (isWithAuth && !sessionId) {
     return withAuth(req);
   }
 
