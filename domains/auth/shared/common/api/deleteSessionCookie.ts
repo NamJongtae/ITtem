9e2e528bed2fe784fcea6003abb3d1d506f0d13c
MAIN6419 <dark9737@gmail.com>
2026-01-07 18:38:44 +0900

    Fix: customFetch 에러 처리 로직을 추가하여 deleteSession API 에러 처리 로직 제거

--- domains/auth/shared/common/api/deleteSessionCookie.ts
+++ domains/auth/shared/common/api/deleteSessionCookie.ts
@@ -2,17 +2,7 @@ import { ApiResponse } from "@/shared/common/types/responseTypes";
 import { customFetch } from "@/shared/common/utils/customFetch";
 
 export default async function deleteSessionCookie(): Promise<ApiResponse> {
-  const response = await customFetch("/api/auth/session-cookie", {
+  return customFetch<ApiResponse>("/api/auth/session-cookie", {
     method: "DELETE"
   });
-
-  if (!response.ok) {
-    const errorData = await response.json().catch(() => {});
-    throw {
-      status: response.status,
-      message: errorData?.message ?? "세션 쿠기 삭제에 실패했어요."
-    };
-  }
-
-  return response.json();
 }
