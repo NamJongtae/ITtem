c691624ed1c9d22bda66403dc0c4ed4941b57282
MAIN6419 <dark9737@gmail.com>
2026-01-07 18:40:04 +0900

    Fix: customFetch 수정에 따라 getMyProfile api 함수 수정 - jwt 재발급 로직 삭제로 withToken 옵션 제거 - 에러 처리 로직이 customFetch에 포함되어 에러 처리 로직 제거

--- domains/user/profile/api/getMyProfile.ts
+++ domains/user/profile/api/getMyProfile.ts
@@ -2,20 +2,5 @@ import { ProfileResponseData } from "../types/responseTypes";
 import { customFetch } from "@/shared/common/utils/customFetch";
 
 export default async function getMyProfile(): Promise<ProfileResponseData> {
-  try {
-    const response = await customFetch("/api/profile", true);
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(
-        errorData?.messsage
-          ? errorData?.messsage
-          : "프로필 데이터 조회에 실패했어요."
-      );
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  return customFetch<ProfileResponseData>("/api/profile");
 }
