8aa3273a269f0129abf65fd5f2ce87dacb841526
MAIN6419 <dark9737@gmail.com>
2026-01-07 16:13:19 +0900

    Refactor: customAxios jwt 인증 관련 로직 제거 - 세션 만료시 처리할 로직 추가

--- shared/common/utils/customAxios.ts
+++ shared/common/utils/customAxios.ts
@@ -1,9 +1,4 @@
-import axios, {
-  AxiosError,
-  InternalAxiosRequestConfig,
-  isAxiosError
-} from "axios";
-import tokenObservable from "./Observable";
+import axios, { AxiosError, isAxiosError } from "axios";
 import { ApiResponse } from "../types/responseTypes";
 import redirect from "./redirect";
 
@@ -14,76 +9,14 @@ const customAxios = axios.create({
   withCredentials: true
 });
 
-// accessToken 재발급 요청 확인 flag
-let isRefreshing = false;
-
 customAxios.interceptors.response.use(
   (response) => response,
-  async (error: AxiosError) => {
-    const originalRequest = error.config as InternalAxiosRequestConfig & {
-      _retry?: boolean;
-    };
-
-    if (
-      (typeof window === "undefined" || process.env.TEST_ENV === "SSR") &&
-      isAxiosError<ApiResponse>(error) &&
-      error.response?.status === 401 &&
-      error.response?.data.message === "만료된 토큰이에요."
-    ) {
-      throw new Error("Expired AccessToken.");
-    }
-
-    if (
-      isAxiosError<ApiResponse>(error) &&
-      error.response?.status === 401 &&
-      error.response?.data.message === "만료된 토큰이에요." &&
-      originalRequest &&
-      !originalRequest._retry
-    ) {
-      originalRequest._retry = true;
-
-      if (!isRefreshing) {
-        isRefreshing = true;
-
-        try {
-          const response = await axios.post(
-            `${BASE_URL}/api/auth/refresh-token`,
-            {},
-            {
-              withCredentials: true
-            }
-          );
-
-          const setCookies = response.headers["set-cookie"];
-          if (setCookies) {
-            originalRequest.headers["Cookie"] = setCookies.join("; ");
-          }
-
-          // 리프레시 성공 → 대기중인 요청들 실행
-          tokenObservable.notifyAll();
-          return customAxios.request(originalRequest);
-        } catch (refreshError) {
-          tokenObservable.removeAll(); // 에러 시 구독 제거
-          if (
-            isAxiosError<ApiResponse>(refreshError) &&
-            refreshError.response?.status === 401
-          ) {
-            if (typeof window !== "undefined") {
-              redirect("/session-expired");
-            }
-          }
-          return Promise.reject(refreshError);
-        } finally {
-          isRefreshing = false;
-        }
+  async (error: AxiosError<ApiResponse>) => {
+    if (isAxiosError(error) && error.response?.status === 401) {
+      if (typeof window !== "undefined" && process.env.TEST_ENV !== "SSR") {
+        await fetch("/api/auth/session-cookie", { method: "DELETE" });
+        redirect("/session-expired");
       }
-
-      // accessToken 재발급 중이면 받은 요청 구독
-      return new Promise((resolve) => {
-        tokenObservable.setObserver(() => {
-          resolve(customAxios.request(originalRequest));
-        });
-      });
     }
 
     return Promise.reject(error);
