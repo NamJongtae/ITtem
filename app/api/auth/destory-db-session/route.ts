60c8426482253ac5f840cddc2f2c99ae76f2a69e
MAIN6419 <dark9737@gmail.com>
2026-01-07 17:25:27 +0900

    Feact: db Session 제거 API Routes 생성

---
+++ app/api/auth/destory-db-session/route.ts
@@ -0,0 +1,45 @@
+import Session from "@/domains/auth/shared/common/models/Sessions";
+import User from "@/domains/auth/shared/common/models/User";
+import dbConnect from "@/shared/common/utils/db/db";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(req: NextRequest) {
+  const { email } = await req.json();
+
+  if (!email) {
+    return NextResponse.json(
+      { message: "유저 이메일이 없어요." },
+      {
+        status: 422
+      }
+    );
+  }
+
+  const dbUserData = await User.findOne({ email });
+
+  if (!dbUserData) {
+    return NextResponse.json(
+      { message: "존재하지 않는 유저입니다." },
+      { status: 404 }
+    );
+  }
+
+  const deleteResult = await Session.deleteMany({ uid: dbUserData._id });
+
+  if (deleteResult.deletedCount === 0) {
+    return NextResponse.json(
+      { message: "삭제할 세션이 존재하지 않습니다." },
+      { status: 404 }
+    );
+  }
+
+  const response = NextResponse.json(
+    { message: "세션 ID 삭제 성공" },
+    {
+      status: 200
+    }
+  );
+  response.cookies.delete("sessionId");
+  return response;
+}
