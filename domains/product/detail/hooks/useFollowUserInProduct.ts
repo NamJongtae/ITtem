044495fa2c1aa9548c9579eee60e97c9fe0561fb
MAIN6419 <dark9737@gmail.com>
2026-01-11 22:48:25 +0900

    Fix: useFollowUserInProduct 훅 followHandler 함수 미로그인시 early return 되도록 수정

--- domains/product/detail/hooks/useFollowUserInProduct.ts
+++ domains/product/detail/hooks/useFollowUserInProduct.ts
@@ -1,6 +1,8 @@
+import { toast } from "react-toastify";
 import useProductDetailFollowMutate from "./mutations/useProductDetailFollowMutate";
 import useProductDetailUnFollowMutate from "./mutations/useProductDetailUnFollowMutate";
 import { ProfileData } from "@/domains/user/profile/types/profileTypes";
+import useAuthStore from "@/domains/auth/shared/common/store/authStore";
 
 interface IParams {
   uid: string;
@@ -13,11 +15,16 @@ export default function useFollowUserInProduct({
   isFollow,
   myProfileData
 }: IParams) {
+  const myUid = useAuthStore((s) => s.user?.uid);
   const { productDetailfollowMutate } = useProductDetailFollowMutate(uid);
 
   const { productDetailUnfollowMutate } = useProductDetailUnFollowMutate(uid);
 
   const followHandler = () => {
+    if (!myUid) {
+      toast.warn("로그인이 필요해요.");
+      return;
+    }
     if (isFollow) {
       productDetailUnfollowMutate();
     } else {
