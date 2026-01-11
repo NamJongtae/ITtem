9b51b22139a4b81b1cc6cf886f075f80913b77a3
MAIN6419 <dark9737@gmail.com>
2026-01-11 19:13:01 +0900

    Fix: profile FollowInfo 컴포넌트 팔로잉, 팔로워 UI 위치 변경

--- domains/user/profile/components/user-info/card/FollowInfo.tsx
+++ domains/user/profile/components/user-info/card/FollowInfo.tsx
@@ -9,21 +9,21 @@ export default function FollowInfo({ profileData, handleClickMenu }: IProps) {
   return (
     <div className="flex gap-5 text-sm font-medium">
       <button
-        onClick={() => handleClickMenu("팔로잉")}
-        className="relative font-semibold before:absolute before:bg-gray-400 before:top-1/2 before:-translate-y-1/2 before:-right-[10px] before:w-[1px] before:h-7"
-      >
-        <span>팔로잉</span>
-        <span className="block w-full text-center">
-          {profileData?.followings.length || 0}
-        </span>
-      </button>
-      <button
         onClick={() => handleClickMenu("팔로워")}
-        className="font-semibold"
+        className="relative font-semibold before:absolute before:bg-gray-400 before:top-1/2 before:-translate-y-1/2 before:-right-[10px] before:w-[1px] before:h-7"
       >
         <span>팔로워</span>
         <span className="block w-full text-center">
-          {profileData?.followers.length || 0}
+          {profileData?.followersCount || 0}
+        </span>
+      </button>
+      <button
+        onClick={() => handleClickMenu("팔로잉")}
+        className="font-semibold"
+      >
+        <span>팔로잉</span>
+        <span className="block w-full text-center">
+          {profileData?.followingsCount || 0}
         </span>
       </button>
     </div>
