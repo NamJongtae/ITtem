88d1e2783a6731518d546733419e2f4eaddd0216
MAIN6419 <dark9737@gmail.com>
2026-01-11 19:05:31 +0900

    Fix: useUserProfilePageQueries 제거 useProfileQuery 사용 - showCSRSkeleton를 사용하여 skeleton UI를 렌더링 하도록 분기 처리 추가

--- domains/user/profile/components/UserProfilePage.tsx
+++ domains/user/profile/components/UserProfilePage.tsx
@@ -3,11 +3,21 @@
 import UserInfo from "./user-info/UserInfo";
 import Detail from "./detail/ProfileDetail";
 import useProfileMenu from "../hooks/useProfileMenu";
-import useUserProfilePageQueries from "../hooks/queries/useUserProfilePageQueries";
+import UserInfoSkeletonUI from "./user-info/UserInfoSkeletonUI";
+import ProfileDetailSkeletonUI from "./detail/ProfileDetailSkeletonUI";
+import useProfileQuery from "../hooks/queries/useProfileQuery";
 
 export default function UserProfilePage() {
-  const { profileData } = useUserProfilePageQueries();
+  const { profileData, showCSRSkeleton } = useProfileQuery();
   const { profileMenu, onClickMenu } = useProfileMenu();
+
+  if (showCSRSkeleton)
+    return (
+      <>
+        <UserInfoSkeletonUI />
+        <ProfileDetailSkeletonUI />
+      </>
+    );
 
   return (
     <>
