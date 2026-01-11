7db30fe32145a7ba0aed6611c8e61d898ea18ff9
MAIN6419 <dark9737@gmail.com>
2026-01-11 18:44:16 +0900

    Refactor: 프로필 페이지 followers/followings 리스트 컴포넌 수정 - uid Params를 통해 현재 타 유저 프로필 페이지를 구분 - 페이지 구분에 따라 useUserFollowListInfiniteQuery, useMyFollowListInfiniteQuery를 호출 - isFollowers props를 listType으로 수정

--- domains/user/profile/components/detail/follow/List.tsx
+++ domains/user/profile/components/detail/follow/List.tsx
@@ -1,26 +1,27 @@
-import useFollowListInfiniteQuery from "../../../hooks/queries/useFollowListInfiniteQuery";
 import { ProfileData } from "../../../types/profileTypes";
 import FollowItem from "./Item";
 import useInfiniteScrollObserver from "@/shared/common/hooks/useInfiniteScrollObserver";
 import InfiniteScrollTarget from "@/shared/common/components/InfiniteScrollTarget";
 import InfiniteScrollEndMessage from "@/shared/common/components/InfiniteScrollEndMessage";
 import ItemSkeletonUI from "./ItemSkeletonUI";
+import useUserFollowListInfiniteQuery from "../../../hooks/queries/useUserFollowListInfiniteQuery";
+import { useParams } from "next/navigation";
+import useMyFollowListInfiniteQuery from "../../../hooks/queries/useMyFollowListInfiniteQuery";
 
 interface IProps {
-  isFollowers: boolean;
+  listType: "followers" | "followings";
   profileData: ProfileData | undefined;
 }
 
-export default function List({ isFollowers, profileData }: IProps) {
-  const userIds = isFollowers
-    ? profileData?.followers
-    : profileData?.followings;
+export default function List({ listType, profileData }: IProps) {
+  const { uid } = useParams();
+
+  const isUserProfilePage = !!uid;
+
   const { data, isFetchingNextPage, hasNextPage, fetchNextPage } =
-    useFollowListInfiniteQuery({
-      isFollowers,
-      userIds,
-      uid: profileData?.uid
-    });
+    isUserProfilePage
+      ? useUserFollowListInfiniteQuery({ listType, uid: profileData?.uid })
+      : useMyFollowListInfiniteQuery({ listType });
 
   const { ref } = useInfiniteScrollObserver({
     fetchNextPage,
@@ -33,7 +34,9 @@ export default function List({ isFollowers, profileData }: IProps) {
       <ul className="grid gap-8 grid-cols-autoFill_180">
         {
           <>
-            {data?.map((item) => <FollowItem key={item.uid} data={item} />)}
+            {data?.map((item) => (
+              <FollowItem key={item.uid} data={item} listType={listType} />
+            ))}
             {isFetchingNextPage &&
               Array(8)
                 .fill("")
@@ -43,7 +46,7 @@ export default function List({ isFollowers, profileData }: IProps) {
         }
       </ul>
       <InfiniteScrollEndMessage
-        message={`더 이상 ${isFollowers ? "팔로워" : "팔로잉"} 유저가 없어요.`}
+        message={`더 이상 ${listType === "followers" ? "팔로워" : "팔로잉"} 유저가 없어요.`}
         data={data}
         hasNextPage={hasNextPage}
       />
