91bc23381805d75e0f25ad2e1d593bb598e89117
MAIN6419 <dark9737@gmail.com>
2026-01-11 19:50:10 +0900

    Refactor: useProductDetailFollowMutate, useProductDetailUnfollowMutate 낙관적 업데이트 로직 수정

--- domains/product/detail/hooks/mutations/useProductDetailFollowMutate.ts
+++ domains/product/detail/hooks/mutations/useProductDetailFollowMutate.ts
@@ -1,71 +1,81 @@
 import followUser from "@/domains/user/shared/api/followUser";
 import { queryKeys } from "@/shared/common/query-keys/queryKeys";
-import { ProfileData } from "@/domains/user/profile/types/profileTypes";
 import { ProductDetailData } from "../../types/productDetailTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { isAxiosError } from "axios";
 import { useParams } from "next/navigation";
 import { toast } from "react-toastify";
+import { isFetchError } from "@/shared/common/utils/isFetchError";
+import useAuthStore from "@/domains/auth/shared/common/store/authStore";
 
 export default function useProductDetailFollowMutate(uid: string) {
   const params = useParams();
   const productId = params?.productId || "";
   const queryClient = useQueryClient();
+  const myUid = useAuthStore((s) => s.user?.uid ?? "");
+
   const myProfileQueryKey = queryKeys.profile.my.queryKey;
+  const myFollowingsQueryKey = queryKeys.profile.my._ctx.followings({
+    uid: myUid,
+    limit: 10
+  }).queryKey;
+  const userProfileQueryKey = queryKeys.profile.user(uid).queryKey;
+  const userFollowersQueryKey = queryKeys.profile.user(uid)._ctx.followers({
+    uid,
+    limit: 10
+  }).queryKey;
+  const userFolloingsQueryKey = queryKeys.profile.user(uid)._ctx.followings({
+    uid,
+    limit: 10
+  }).queryKey;
+
   const productQueryKey = queryKeys.product.detail(
     productId as string
   ).queryKey;
 
   const { mutate: productDetailfollowMutate } = useMutation({
     mutationFn: () => followUser(uid),
+
     onMutate: async () => {
       await queryClient.cancelQueries({ queryKey: productQueryKey });
-
-      await queryClient.cancelQueries({ queryKey: myProfileQueryKey });
 
       const previousProduct = queryClient.getQueryData(productQueryKey) as
         | ProductDetailData
         | undefined;
 
-      const previousMyProfile = queryClient.getQueryData(myProfileQueryKey) as
-        | ProfileData
-        | undefined;
+      /** ✅ 상품 상세 데이터에서 isFollow = true */
+      if (previousProduct?.auth) {
+        queryClient.setQueryData(productQueryKey, {
+          ...previousProduct,
+          auth: {
+            ...previousProduct.auth,
+            isFollow: true
+          }
+        });
+      }
 
-      const newProduct = {
-        ...previousProduct,
-        auth: {
-          ...previousProduct?.auth,
-          followers: [
-            ...(previousProduct?.auth.followers || []),
-            previousMyProfile?.uid
-          ]
-        }
-      };
-
-      const newMyProfile = {
-        ...previousMyProfile,
-        followings: [...(previousMyProfile?.followings || []), uid]
-      };
-
-      queryClient.setQueryData(productQueryKey, newProduct);
-
-      queryClient.setQueryData(myProfileQueryKey, newMyProfile);
-
-      return { previousProduct, previousMyProfile };
+      return { previousProduct };
     },
-    onError: (error, data, ctx) => {
+
+    onError: (error, _, ctx) => {
       queryClient.setQueryData(productQueryKey, ctx?.previousProduct);
-      queryClient.setQueryData(myProfileQueryKey, ctx?.previousMyProfile);
-      if (isAxiosError<{ message: string }>(error)) {
-        toast.warn("유저 팔로우에 실패했어요.\n잠시 후에 다시 시도해주세요.");
+
+      if (isFetchError(error)) {
+        if (error.status === 409) {
+          toast.warn("이미 팔로우한 유저 입니다.");
+        } else {
+          toast.warn("유저 팔로우에 실패했어요.\n잠시 후에 다시 시도해주세요.");
+        }
       }
     },
+
     onSettled: () => {
-      queryClient.invalidateQueries({
-        queryKey: productQueryKey
-      });
       queryClient.invalidateQueries({ queryKey: myProfileQueryKey });
+      queryClient.invalidateQueries({ queryKey: userProfileQueryKey });
+      queryClient.invalidateQueries({ queryKey: myFollowingsQueryKey });
+      queryClient.invalidateQueries({ queryKey: userFollowersQueryKey });
+      queryClient.invalidateQueries({ queryKey: userFolloingsQueryKey });
     }
   });
+
   return { productDetailfollowMutate };
 }
--- domains/product/detail/hooks/mutations/useProductDetailUnfollowMutate.ts
+++ domains/product/detail/hooks/mutations/useProductDetailUnfollowMutate.ts
@@ -1,71 +1,78 @@
 import unfollowUser from "@/domains/user/shared/api/unfollowUser";
 import { queryKeys } from "@/shared/common/query-keys/queryKeys";
-import { ProfileData } from "@/domains/user/profile/types/profileTypes";
 import { ProductDetailData } from "../../types/productDetailTypes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { isAxiosError } from "axios";
 import { useParams } from "next/navigation";
 import { toast } from "react-toastify";
+import { isFetchError } from "@/shared/common/utils/isFetchError";
+import useAuthStore from "@/domains/auth/shared/common/store/authStore";
 
-export default function useProductDetailUnfollowMutate(uid: string) {
+export default function useProductDetailUnFollowMutate(uid: string) {
   const params = useParams();
   const productId = params?.productId || "";
   const queryClient = useQueryClient();
-  const myProfileQueryKey = queryKeys.profile.my.queryKey;
+  const myUid = useAuthStore((s) => s.user?.uid ?? "");
+
   const productQueryKey = queryKeys.product.detail(
     productId as string
   ).queryKey;
+  const myProfileQueryKey = queryKeys.profile.my.queryKey;
+  const myFollowingsQueryKey = queryKeys.profile.my._ctx.followings({
+    uid: myUid,
+    limit: 10
+  }).queryKey;
+  const userProfileQueryKey = queryKeys.profile.user(uid).queryKey;
+  const userFollowersQueryKey = queryKeys.profile.user(uid)._ctx.followers({
+    uid,
+    limit: 10
+  }).queryKey;
+  const userFolloingsQueryKey = queryKeys.profile.user(uid)._ctx.followings({
+    uid,
+    limit: 10
+  }).queryKey;
 
   const { mutate: productDetailUnfollowMutate } = useMutation({
     mutationFn: () => unfollowUser(uid),
+
     onMutate: async () => {
       await queryClient.cancelQueries({ queryKey: productQueryKey });
-
-      await queryClient.cancelQueries({ queryKey: myProfileQueryKey });
 
       const previousProduct = queryClient.getQueryData(
         productQueryKey
       ) as ProductDetailData;
 
-      const previousMyProfile = queryClient.getQueryData(
-        myProfileQueryKey
-      ) as ProfileData;
+      /** ✅ 상품 상세에서 auth.isFollow = false 처리 */
+      if (previousProduct?.auth) {
+        queryClient.setQueryData(productQueryKey, {
+          ...previousProduct,
+          auth: {
+            ...previousProduct.auth,
+            isFollow: false
+          }
+        });
+      }
 
-      const newProduct = {
-        ...previousProduct,
-        auth: {
-          ...previousProduct.auth,
-          followers: [
-            ...previousProduct.auth.followers.filter(
-              (id) => id !== previousMyProfile.uid
-            )
-          ]
-        }
-      };
-
-      const newMyProfile = {
-        ...previousMyProfile,
-        followings: [...previousMyProfile.followings.filter((id) => id !== uid)]
-      };
-
-      await queryClient.setQueryData(productQueryKey, newProduct);
-
-      await queryClient.setQueryData(myProfileQueryKey, newMyProfile);
-
-      return { previousProduct, previousMyProfile };
+      return { previousProduct };
     },
-    onError: (error, data, ctx) => {
+
+    onError: (error, _, ctx) => {
       queryClient.setQueryData(productQueryKey, ctx?.previousProduct);
-      queryClient.setQueryData(myProfileQueryKey, ctx?.previousMyProfile);
-      if (isAxiosError<{ message: string }>(error)) {
-        toast.warn(
-          "유저 언팔로우에 실패했어요.\n잠시 후에 다시 시도해주세요."
-        );
+
+      if (isFetchError(error)) {
+        if (error.status === 409) {
+          toast.warn("이미 언팔로우한 유저 입니다.");
+        } else {
+          toast.warn("유저 팔로우에 실패했어요.\n잠시 후에 다시 시도해주세요.");
+        }
       }
     },
+
     onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: productQueryKey });
       queryClient.invalidateQueries({ queryKey: myProfileQueryKey });
+      queryClient.invalidateQueries({ queryKey: userProfileQueryKey });
+      queryClient.invalidateQueries({ queryKey: myFollowingsQueryKey });
+      queryClient.invalidateQueries({ queryKey: userFollowersQueryKey });
+      queryClient.invalidateQueries({ queryKey: userFolloingsQueryKey });
     }
   });
 
