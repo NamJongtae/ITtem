70e1c6f2da1b0448f119b5e9a781657f700173e5
MAIN6419 <dark9737@gmail.com>
2026-01-13 17:39:31 +0900

    Refactor: useProductReportHandler 훅 reportUserIds 파라미터 제거 및 isReported 파라미터 추가 - isReported를 통해 이미 신고한 상품을 구별 하도록 분기 처리 수정

--- domains/product/detail/hooks/useProductReportHandler.ts
+++ domains/product/detail/hooks/useProductReportHandler.ts
@@ -3,20 +3,19 @@ import useProductReportMutate from "./mutations/useProductReportMutate";
 import useMyProfileQuery from "@/domains/user/profile/hooks/queries/useMyProfileQuery";
 
 interface IParams {
-  reportUserIds: string[] | undefined;
+  isReported: boolean | undefined;
 }
 
-export default function useProductReportHandler({ reportUserIds }: IParams) {
+export default function useProductReportHandler({ isReported }: IParams) {
   const { productReportMutate } = useProductReportMutate();
   const { myProfileData, myProfilePending } = useMyProfileQuery();
-  const isReport = reportUserIds?.includes(myProfileData?.uid || "");
 
   const handleClickReport = () => {
     if (!myProfileData) {
       toast.warn("로그인 후 이용해주세요.");
       return;
     }
-    if (isReport) {
+    if (isReported) {
       toast.warn("이미 신고한 상품이에요.");
     } else {
       const isReport = confirm("정말 신고하겠습니까?");
