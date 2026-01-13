ccdc35c922f1e17f22944853707f1a0813bae937
NamJongtae <dark9737@gmail.com>
2026-01-13 21:09:33 +0900

    Style: 모바일 로그인 아이콘 크기 변경

--- shared/layout/components/sub-nav/MoblieMenuLoginBtn.tsx
+++ shared/layout/components/sub-nav/MoblieMenuLoginBtn.tsx
@@ -11,7 +11,7 @@ export default function MoblieMenuLoginBtn() {
       }
       onClick={() => navigate({ type: "push", url: "/signin" })}
     >
-      <Image src={"/icons/user-icon.svg"} alt="logout" width={20} height={20} />
+      <Image src={"/icons/user-icon.svg"} alt="logout" width={28} height={28} />
       로그인
     </button>
   );
