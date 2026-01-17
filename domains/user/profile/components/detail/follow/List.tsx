bbb2db518fd4bbf48ae87b306446d222e1b5928f
NamJongtae <dark9737@gmail.com>
2026-01-17 15:36:31 +0900

    Fix: 프로필 FollowList 컴포넌트에서 훅 조건부 호출 제거 위해 UserFollowList/MyFollowList 컴포넌트 분리

--- domains/user/profile/components/detail/follow/List.tsx
+++ domains/user/profile/components/detail/follow/List.tsx
@@ -5,23 +5,23 @@ import InfiniteScrollTarget from "@/shared/common/components/InfiniteScrollTarge
 import InfiniteScrollEndMessage from "@/shared/common/components/InfiniteScrollEndMessage";
 import ItemSkeletonUI from "./ItemSkeletonUI";
 import useUserFollowListInfiniteQuery from "../../../hooks/queries/useUserFollowListInfiniteQuery";
-import { useParams } from "next/navigation";
 import useMyFollowListInfiniteQuery from "../../../hooks/queries/useMyFollowListInfiniteQuery";
+import { useParams } from "next/navigation";
 
 interface IProps {
   listType: "followers" | "followings";
   profileData: ProfileData | undefined;
 }
 
-export default function List({ listType, profileData }: IProps) {
-  const { uid } = useParams();
-
-  const isUserProfilePage = !!uid;
-
+function UserFollowList({
+  listType,
+  uid
+}: {
+  listType: "followers" | "followings";
+  uid: string | undefined;
+}) {
   const { data, isFetchingNextPage, hasNextPage, fetchNextPage } =
-    isUserProfilePage
-      ? useUserFollowListInfiniteQuery({ listType, uid: profileData?.uid })
-      : useMyFollowListInfiniteQuery({ listType });
+    useUserFollowListInfiniteQuery({ listType, uid });
 
   const { ref } = useInfiniteScrollObserver({
     fetchNextPage,
@@ -32,24 +32,70 @@ export default function List({ listType, profileData }: IProps) {
   return (
     <>
       <ul className="grid gap-8 grid-cols-autoFill_180">
-        {
-          <>
-            {data?.map((item) => (
-              <FollowItem key={item.uid} data={item} listType={listType} />
-            ))}
-            {isFetchingNextPage &&
-              Array(8)
-                .fill("")
-                .map((_, index) => <ItemSkeletonUI key={index} />)}
-            <InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />
-          </>
-        }
+        <>
+          {data?.map((item) => (
+            <FollowItem key={item.uid} data={item} listType={listType} />
+          ))}
+          {isFetchingNextPage &&
+            Array(8)
+              .fill("")
+              .map((_, index) => <ItemSkeletonUI key={index} />)}
+          <InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />
+        </>
       </ul>
+
       <InfiniteScrollEndMessage
         message={`더 이상 ${listType === "followers" ? "팔로워" : "팔로잉"} 유저가 없어요.`}
         data={data}
         hasNextPage={hasNextPage}
       />
     </>
+  );
+}
+
+function MyFollowList({ listType }: { listType: "followers" | "followings" }) {
+  const { data, isFetchingNextPage, hasNextPage, fetchNextPage } =
+    useMyFollowListInfiniteQuery({ listType });
+
+  const { ref } = useInfiniteScrollObserver({
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage
+  });
+
+  return (
+    <>
+      <ul className="grid gap-8 grid-cols-autoFill_180">
+        <>
+          {data?.map((item) => (
+            <FollowItem key={item.uid} data={item} listType={listType} />
+          ))}
+          {isFetchingNextPage &&
+            Array(8)
+              .fill("")
+              .map((_, index) => <ItemSkeletonUI key={index} />)}
+          <InfiniteScrollTarget ref={ref} hasNextPage={hasNextPage} />
+        </>
+      </ul>
+
+      <InfiniteScrollEndMessage
+        message={`더 이상 ${listType === "followers" ? "팔로워" : "팔로잉"} 유저가 없어요.`}
+        data={data}
+        hasNextPage={hasNextPage}
+      />
+    </>
+  );
+}
+
+export default function List({ listType, profileData }: IProps) {
+  const params = useParams<{ uid?: string }>();
+  const uidParam = params?.uid;
+
+  const isUserProfilePage = Boolean(uidParam);
+
+  return isUserProfilePage ? (
+    <UserFollowList listType={listType} uid={profileData?.uid} />
+  ) : (
+    <MyFollowList listType={listType} />
   );
 }
