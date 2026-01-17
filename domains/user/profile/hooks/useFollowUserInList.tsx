9a6fff4423300b96253e0a18af5ab715e0a35a77
NamJongtae <dark9737@gmail.com>
2026-01-17 15:40:27 +0900

    Fix: useFollowUserInList 훅 myProfileData 파라미터 추가하여 이 값을 사용하도록 수정 - queryClient.getQueryData로 프로필 데이터를 가져올 때 캐시에 없으면 undefined가 되는 케이스를 방지

--- domains/user/profile/hooks/useFollowUserInList.tsx
+++ domains/user/profile/hooks/useFollowUserInList.tsx
@@ -2,17 +2,17 @@ import useMyProfileFollowInListMutate from "./mutations/useMyProfileFollowInList
 import useMyProfileUnfollowInListMutate from "./mutations/useMyProfileUnfollowInListMutate";
 import { FollowUserData, ProfileData } from "../types/profileTypes";
 import { toast } from "react-toastify";
-import { useQueryClient } from "@tanstack/react-query";
-import { queryKeys } from "@/shared/common/query-keys/queryKeys";
 import useUserProfileFollowInListMutate from "./mutations/useUserProfileFollowInListMutate";
 import useUserProfileUnFollowInListMutate from "./mutations/useUserProfileUnFollowInListMutate";
 import { useParams } from "next/navigation";
 
 export default function useFollowUserInList({
   followProfileData,
+  myProfileData,
   listType
 }: {
   followProfileData: FollowUserData | undefined;
+  myProfileData: ProfileData | undefined;
   listType: "followers" | "followings";
 }) {
   const { myProfilefollowMutate } = useMyProfileFollowInListMutate(
@@ -31,16 +31,12 @@ export default function useFollowUserInList({
     listType
   });
 
-  const queryClient = useQueryClient();
-  const myProfileData = queryClient.getQueryData(
-    queryKeys.profile.my.queryKey
-  ) as ProfileData | undefined;
 
   const isFollow = followProfileData?.isFollow;
 
   const { uid } = useParams();
 
-  const isMyProfilePage = !!!uid;
+  const isMyProfilePage = !!uid === false;
 
   const isNotMyProfile = myProfileData?.uid !== followProfileData?.uid;
 
