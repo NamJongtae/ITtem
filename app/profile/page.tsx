c5c9c6481e9d4c0b9c5f4832d8484b5282d7ce22
MAIN6419 <dark9737@gmail.com>
2026-01-18 16:15:06 +0900

    Fix: MyProfilePage Suspense 및 fallback 추가

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,19 @@
-import MyProfilePage from "@/domains/user/profile/components/MyProfilePage";
+import MyProfileScreen from "@/domains/user/profile/components/MyProfileScreen";
+import ProfileTabsSSRSkeletonUI from "@/domains/user/profile/components/tabs/ProfileTabsSSRSkeletonUI";
+import UserInfoSkeletonUI from "@/domains/user/profile/components/user-info/UserInfoSkeletonUI";
+import { Suspense } from "react";
 
 export default function MyProfile() {
-  return <MyProfilePage />;
+  return (
+    <Suspense
+      fallback={
+        <>
+          <UserInfoSkeletonUI />
+          <ProfileTabsSSRSkeletonUI isMyProfile={true} />
+        </>
+      }
+    >
+      <MyProfileScreen />
+    </Suspense>
+  );
 }
