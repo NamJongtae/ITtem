d1875a83396eb7dab0af0f8ae97242fa7ab669de
MAIN6419 <dark9737@gmail.com>
2026-01-18 21:25:49 +0900

    Fix: NavAuth 로딩 상태에 스켈레톤 UI 추가

--- shared/layout/components/nav/NavAuth.tsx
+++ shared/layout/components/nav/NavAuth.tsx
@@ -10,7 +10,13 @@ export default function NavAuth() {
   const isLoginUser = user?.uid;
 
   if (authIsLoading)
-    return <div className="w-[36px] md:w-[192px] lg:w-[246px]"></div>;
+    return (
+      <div className="flex w-[36px] md:w-[192px] lg:w-[246px] animate-pulse justify-end items-center gap-3">
+        <div className="w-[36px] h-[36px] rounded-full bg-gray-200"></div>
+        <div className="w-12 h-5 rounded bg-gray-200 hidden lg:inline"></div>
+        <div className="w-20 h-5 rounded bg-gray-200 hidden md:block"></div>
+      </div>
+    );
 
   return (
     <div className="flex items-center gap-3 flex-shrink-0 md:basis-1/4 justify-end">
@@ -20,7 +26,10 @@ export default function NavAuth() {
           <LogoutBtn />
         </div>
       ) : (
-        <SigninBtn />
+        <>
+          <div className="w-[36px] md:hidden" />
+          <SigninBtn />
+        </>
       )}
     </div>
   );
