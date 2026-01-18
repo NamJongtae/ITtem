72e95f169b81cbc0c72135fc009d2a5c1de7cf91
MAIN6419 <dark9737@gmail.com>
2026-01-18 16:35:55 +0900

    Fix: UserInfo 컴포넌트 isMyProfile props 추가 및 UserInfoCard 컴포넌트 props에 전달

--- domains/user/profile/components/user-info/UserInfo.tsx
+++ domains/user/profile/components/user-info/UserInfo.tsx
@@ -5,16 +5,22 @@ import { ProfileData, ProfileMenu } from "../../types/profileTypes";
 interface IProps {
   handleClickMenu: (menu: ProfileMenu) => void;
   profileData: ProfileData | undefined;
+  isMyProfile: boolean;
 }
 
-export default function UserInfo({ handleClickMenu, profileData }: IProps) {
-  return (
+export default function UserInfo({
+  handleClickMenu,
+  profileData,
+  isMyProfile
+}: IProps) {
+  return ( 
     <div className="mt-5 max-w-[1024px] px-4 md:px-8 mx-auto">
       <h2 className="sr-only">유저 프로필</h2>
       <div className="flex flex-col gap-5 md:flex-row border py-5">
         <UserInfoCard
           handleClickMenu={handleClickMenu}
           profileData={profileData}
+          isMyProfile={isMyProfile}
         />
         <Introduce introuduce={profileData?.introduce} />
       </div>
