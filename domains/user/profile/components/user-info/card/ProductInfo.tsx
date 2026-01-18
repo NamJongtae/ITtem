76c11fb6baa2d5f8964565b4fe2f4b5e1b0554bc
MAIN6419 <dark9737@gmail.com>
2026-01-18 16:49:44 +0900

    Refactor: ProductInfo 컴포넌트 productCount, reviewPercentage, saleCount props 받도록 수정

--- domains/user/profile/components/user-info/card/ProductInfo.tsx
+++ domains/user/profile/components/user-info/card/ProductInfo.tsx
@@ -1,11 +1,18 @@
-import { ProfileData, ProfileMenu } from "../../../types/profileTypes";
+import { ProfileMenu } from "../../../types/profileTypes";
 
 interface IProps {
+  productCount: number;
+  reviewPercentage: number;
+  saleCount: number;
   handleClickMenu: (menu: ProfileMenu) => void;
-  profileData: ProfileData | undefined;
 }
 
-export default function ProductInfo({ profileData, handleClickMenu }: IProps) {
+export default function ProductInfo({
+  productCount,
+  reviewPercentage,
+  saleCount,
+  handleClickMenu
+}: IProps) {
   return (
     <>
       <div className="text-sm font-medium">
@@ -13,17 +20,14 @@ export default function ProductInfo({ profileData, handleClickMenu }: IProps) {
           onClick={() => handleClickMenu("products")}
           className="relative mr-4 before:w-[1px] before:h-3 before:absolute before:bg-gray-400 before:-right-[9px] before:top-1/2 before:-translate-y-1/2"
         >
-          상품 {profileData?.productIds.length || 0}
+          상품 {productCount || 0}
         </button>
 
         <button onClick={() => handleClickMenu("reviews")}>
-          평가{" "}
-          {profileData?.reviewInfo?.reviewPercentage
-            ? `${profileData?.reviewInfo?.reviewPercentage}%`
-            : "없음"}
+          평가 {reviewPercentage === 0 ? `${reviewPercentage}%` : "없음"}
         </button>
       </div>
-      <span className="text-sm">상품판매 {profileData?.saleCount || 0}회</span>
+      <span className="text-sm">상품판매 {saleCount || 0}회</span>
     </>
   );
 }
