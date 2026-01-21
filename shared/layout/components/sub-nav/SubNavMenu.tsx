f906a4687764ba0019022369f244b82d555a7419
NamJongtae <dark9737@gmail.com>
2026-01-21 16:29:51 +0900

    Fix: NotificationBtn dynamic import loading 추가

--- shared/layout/components/sub-nav/SubNavMenu.tsx
+++ shared/layout/components/sub-nav/SubNavMenu.tsx
@@ -4,11 +4,18 @@ import MenuChatBtn from "./ChatBtn";
 import MenuProductBtn from "./ProductBtn";
 import MenuSellBtn from "./SellBtn";
 import dynamic from "next/dynamic";
+import NotificatonIcon from "@/public/icons/notification-icon.svg";
 
 const NotificationBtn = dynamic(
   () => import("@/shared/layout/components/sub-nav/NotificationBtn"),
   {
-    ssr: false
+    ssr: false,
+    loading: () => (
+      <div className="inline-flex flex-col items-center gap-[2px] text-xs text-gary-600">
+        <NotificatonIcon className={"w-6 h-6 md:w-5 md:h-5 stroke-black"} />
+        <span className="hidden md:inline">알림</span>
+      </div>
+    )
   }
 );
 
