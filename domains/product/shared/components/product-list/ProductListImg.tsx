b71575566f37f85b6357ad6e0ba8329fc98ec08c
MAIN6419 <dark9737@gmail.com>
2026-01-22 23:38:20 +0900

    Fix: 상품 리스트 이미지 슬라이더 분기 제거

--- domains/product/shared/components/product-list/ProductListImg.tsx
+++ domains/product/shared/components/product-list/ProductListImg.tsx
@@ -16,9 +16,6 @@ const status = {
 export default function ProductListImg({ data }: IProps) {
   const soldoutStyles = `before:absolute before:inset-0 before:bg-gray-700 before:bg-opacity-50 before:z-10 before:text-white before:text-xl before:font-semibold before:content-[attr(data-status)] before:flex before:justify-center before:items-center`;
   const dataStatus = status[data.status];
-  const isSoldout = data.status !== ProductStatus.soldout;
-  const isTrading = data.status === ProductStatus.trading;
-  const isOneImg = data.imgData.length === 1;
 
   return (
     <div
@@ -27,25 +24,16 @@ export default function ProductListImg({ data }: IProps) {
         data.status !== ProductStatus.sold ? soldoutStyles : ""
       } relative flex h-full w-full`}
     >
-      {isOneImg || isSoldout || isTrading ? (
-        <FallbackImage
-          className="w-full h-full object-cover object-center aspect-square"
-          src={data.imgData[0].url}
-          alt={data.status !== ProductStatus.sold ? "soldout" : data.name}
-          width={300}
-          height={300}
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
-        />
-      ) : (
-        <ImgSlider
-          imgData={data.imgData}
-          imgWidth={300}
-          imgHeight={300}
-          isAutoPlay={true}
-          isLoop={true}
-        />
-      )}
+      <FallbackImage
+        className="w-full h-full object-cover object-center aspect-square"
+        src={data.imgData[0].url}
+        alt={data.status !== ProductStatus.sold ? "soldout" : data.name}
+        width={300}
+        height={300}
+        placeholder="blur"
+        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+      />
+      )
     </div>
   );
 }
