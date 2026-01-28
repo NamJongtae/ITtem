7a77d7b23b4c10414845f5d967059c03e5071ec7
MAIN6419 <dark9737@gmail.com>
2026-01-28 23:42:58 +0900

    Fix: RecommendProductFirstList 내부 ProductListItem 컴포넌트 <li> 부모 태그 추가

--- domains/home/components/RecommendProductFirstList.tsx
+++ domains/home/components/RecommendProductFirstList.tsx
@@ -13,11 +13,9 @@ export default function RecommendProductFirstList({
     <div className="max-w-[1024px] mx-auto px-8 mt-6">
       <ul className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map((item) => (
-          <ProductListItem
-            key={item._id}
-            data={item}
-            category={ProductCategory.전체}
-          />
+          <li key={item._id}>
+            <ProductListItem data={item} category={ProductCategory.전체} />
+          </li>
         ))}
       </ul>
     </div>
