84da204ebd5de4c053b0c6ca913747420f6f6eaa
MAIN6419 <dark9737@gmail.com>
2026-01-28 20:49:31 +0900

    Feat: 서버 전용 유저 프로필 조회 함수 추가

---
+++ domains/user/profile/server/getUserProfileServer.ts
@@ -0,0 +1,142 @@
+import User from "@/domains/auth/shared/common/models/User";
+import dbConnect from "@/shared/common/utils/db/db";
+import mongoose from "mongoose";
+
+export async function getUserProfileServer(uid: string) {
+  try {
+    await dbConnect();
+
+    const targetUserId = new mongoose.Types.ObjectId(uid);
+
+    const aggregation: any[] = [
+      { $match: { _id: targetUserId } },
+
+      /** 리뷰 정보 */
+      {
+        $lookup: {
+          from: "reviewScores",
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$uid", uid] }
+              }
+            },
+            {
+              $addFields: {
+                reviewPercentage: {
+                  $round: [
+                    {
+                      $multiply: [
+                        {
+                          $cond: [
+                            { $eq: ["$totalReviewCount", 0] },
+                            0,
+                            {
+                              $divide: [
+                                "$totalReviewScore",
+                                "$totalReviewCount"
+                              ]
+                            }
+                          ]
+                        },
+                        20
+                      ]
+                    },
+                    1
+                  ]
+                }
+              }
+            },
+            { $project: { _id: 0, uid: 0 } }
+          ],
+          as: "reviewInfo"
+        }
+      },
+      {
+        $unwind: {
+          path: "$reviewInfo",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+
+      /** followersCount */
+      {
+        $lookup: {
+          from: "follows",
+          let: { userId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$followingId", "$$userId"] }
+              }
+            },
+            { $count: "count" }
+          ],
+          as: "followersCount"
+        }
+      },
+      {
+        $addFields: {
+          followersCount: {
+            $ifNull: [{ $arrayElemAt: ["$followersCount.count", 0] }, 0]
+          }
+        }
+      },
+
+      /** followingsCount */
+      {
+        $lookup: {
+          from: "follows",
+          let: { userId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$followerId", "$$userId"] }
+              }
+            },
+            { $count: "count" }
+          ],
+          as: "followingsCount"
+        }
+      },
+      {
+        $addFields: {
+          followingsCount: {
+            $ifNull: [{ $arrayElemAt: ["$followingsCount.count", 0] }, 0]
+          }
+        }
+      },
+
+      /** 불필요 필드 제거 */
+      {
+        $project: {
+          email: 0,
+          password: 0,
+          __v: 0,
+          loginType: 0,
+          createdAt: 0,
+          chatRoomList: 0,
+          profileImgFilename: 0,
+          wishProductIds: 0
+        }
+      }
+    ];
+
+    const result = await User.aggregate(aggregation);
+
+    const doc = result[0];
+    
+    if (!doc) return undefined;
+
+    const profile = {
+      ...result[0],
+      uid: result[0]._id
+    };
+    delete profile._id;
+
+    return JSON.parse(JSON.stringify(profile));
+  } catch (error) {
+    console.error("유저 프로필 조회 에러:", error);
+    throw new Error("유저 프로필 조회 에러가 발생했어요.");
+  }
+}
