ddb28d02c84ea28a7ce6fde99defae8e9ec33d2f
MAIN6419 <dark9737@gmail.com>
2026-01-28 23:43:29 +0900

    Style: InfiniteScrollEndMessage 텍스트 색상 수정

--- shared/common/components/InfiniteScrollEndMessage.tsx
+++ shared/common/components/InfiniteScrollEndMessage.tsx
@@ -11,7 +11,7 @@ export default function InfiniteScrollEndMessage({
 }) {
   return data && data.length > 0 && !hasNextPage ? (
     <div className="flex justify-center items-center my-8 border-b mx-8">
-      <p className="absolute text-center text-xs sm:text-sm text-gray-400 bg-white px-4 sm:px-8">
+      <p className="absolute text-center text-xs sm:text-sm text-gray-500 bg-white px-4 sm:px-8">
         {message}
       </p>
     </div>
