cdba23bb1b5cc127bc405bbc1e68d994c78424b9
MAIN6419 <dark9737@gmail.com>
2026-01-30 18:39:01 +0900

    Fix: RecommendProductFirstList 컴포넌트 내부 빈 데이터 처리 추가

--- domains/home/components/RecommendProductFirstList.tsx
+++ domains/home/components/RecommendProductFirstList.tsx
@@ -3,6 +3,7 @@ import {
   ProductCategory,
   type ProductData
 } from "@/domains/product/shared/types/productTypes";
+import Empty from "@/shared/common/components/Empty";
 
 export default function RecommendProductFirstList({
   products
@@ -11,13 +12,17 @@ export default function RecommendProductFirstList({
 }) {
   return (
     <div className="max-w-[1024px] mx-auto px-8 mt-6">
-      <ul className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {products.map((item) => (
-          <li key={item._id}>
-            <ProductListItem data={item} category={ProductCategory.전체} />
-          </li>
-        ))}
-      </ul>
+      {products.length === 0 ? (
+        <Empty message="오늘의 추천 상품이 없어요." />
+      ) : (
+        <ul className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {products.map((item) => (
+            <li key={item._id}>
+              <ProductListItem data={item} category={ProductCategory.전체} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
