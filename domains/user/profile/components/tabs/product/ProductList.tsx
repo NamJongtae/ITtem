87abbd5c0f07cb2f32e0cfa79429b0e016af7526
MAIN6419 <dark9737@gmail.com>
2026-01-30 17:47:16 +0900

    Fix: 유저 프로필 ProdutList 컴포넌트 ProductListUI 컴포넌트의 Empty 처리 위임

--- domains/user/profile/components/tabs/product/ProductList.tsx
+++ domains/user/profile/components/tabs/product/ProductList.tsx
@@ -1,6 +1,7 @@
 import ProductListUI from "@/domains/product/shared/components/product-list/ProductListUI";
 import useProfileProductListInfiniteQuery from "../../../hooks/queries/useProfileProductListInfiniteQuery";
 import { ProductCategory } from "@/domains/product/shared/types/productTypes";
+import Empty from "@/shared/common/components/Empty";
 
 interface IProps {
   isMyProfile?: boolean;
@@ -19,13 +20,21 @@ export default function List({
       productCategory,
       productIds: productIds || []
     });
+
+  if (data.length === 0) {
+    return (
+      <div className="max-w-[1024px] mx-auto pb-12 px-8 mt-6">
+        <Empty message="상품이 존재하지 않아요." />
+      </div>
+    );
+  }
+
   return (
     <ProductListUI
       data={data}
       isFetchingNextPage={isFetchingNextPage}
       fetchNextPage={fetchNextPage}
       hasNextPage={hasNextPage}
-      emptyMessage="상품이 존재하지 않아요."
     />
   );
 }
