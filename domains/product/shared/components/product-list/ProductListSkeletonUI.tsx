fe41ec0ab02d7264f2a3a089e69291a1fc50a4ed
MAIN6419 <dark9737@gmail.com>
2026-01-30 19:33:03 +0900

    Fix: ProductListSkeletonUI 컴포넌트 listCount 기본 값 12로 수정

--- domains/product/shared/components/product-list/ProductListSkeletonUI.tsx
+++ domains/product/shared/components/product-list/ProductListSkeletonUI.tsx
@@ -4,7 +4,7 @@ interface IProps {
   listCount?: number;
 }
 
-export default function ProductListSkeletonUI({ listCount = 8 }: IProps) {
+export default function ProductListSkeletonUI({ listCount = 12 }: IProps) {
   return (
     <div className="max-w-[1024px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6 px-8">
       {Array.from({ length: listCount }).map((_, index) => (
