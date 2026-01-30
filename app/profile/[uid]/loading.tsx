da273e1b332575043f3268edda3fa53fe6fff1ef
MAIN6419 <dark9737@gmail.com>
2026-01-30 19:35:15 +0900

    Fix: MyProfile, UserProfile 로딩 탭 스켈레톤 CSR -> SSR로 교체

--- app/profile/[uid]/loading.tsx
+++ app/profile/[uid]/loading.tsx
@@ -1,11 +1,11 @@
-import ProfileTabsCSRSkeletonUI from "@/domains/user/profile/components/tabs/ProfileTabsCSRSkeletonUI";
+import ProfileTabsSSRSkeletonUI from "@/domains/user/profile/components/tabs/ProfileTabsSSRSkeletonUI";
 import UserInfoSkeletonUI from "@/domains/user/profile/components/user-info/UserInfoSkeletonUI";
 
 export default function UserProfileLoading() {
   return (
     <>
       <UserInfoSkeletonUI isMyProfile={false} />
-      <ProfileTabsCSRSkeletonUI isMyProfile={false} />
+      <ProfileTabsSSRSkeletonUI isMyProfile={false} />
     </>
   );
 }
--- app/profile/loading.tsx
+++ app/profile/loading.tsx
@@ -1,11 +1,11 @@
-import ProfileTabsCSRSkeletonUI from "@/domains/user/profile/components/tabs/ProfileTabsCSRSkeletonUI";
+import ProfileTabsSSRSkeletonUI from "@/domains/user/profile/components/tabs/ProfileTabsSSRSkeletonUI";
 import UserInfoSkeletonUI from "@/domains/user/profile/components/user-info/UserInfoSkeletonUI";
 
 export default function MyProfileLoading() {
   return (
     <>
       <UserInfoSkeletonUI isMyProfile={true} />
-      <ProfileTabsCSRSkeletonUI isMyProfile={true} />
+      <ProfileTabsSSRSkeletonUI isMyProfile={true} />
     </>
   );
 }
