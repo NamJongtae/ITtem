c8a6d4ce9a1cbc3c1109186ad0669152da9c8e5b
MAIN6419 <dark9737@gmail.com>
2026-01-30 22:09:33 +0900

    Remove: useCategoruList 및 useCurrentCategoru 훅 및 테스트 코드 제거

--- domains/product/shared/components/categoryNav/hooks/useCategoryList.ts
+++
@@ -1,9 +0,0 @@
-import { usePathname, useSearchParams } from "next/navigation";
-
-export default function useCategoryList() {
-  const pathname = usePathname();
-  const search = useSearchParams();
-  const keyword = search.get("keyword");
-
-  return { pathname, keyword };
-}
