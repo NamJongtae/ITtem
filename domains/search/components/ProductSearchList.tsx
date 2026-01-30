c47fc196ee755a97e9a543b9d3df05241659677c
MAIN6419 <dark9737@gmail.com>
2026-01-30 18:02:57 +0900

    Fix: ProductSerachList 컴포넌트 빈 데이터 처리 추가

--- domains/search/components/ProductSearchList.tsx
+++ domains/search/components/ProductSearchList.tsx
@@ -3,11 +3,20 @@
 import useSearchProductListInfiniteQuery from "@/domains/product/shared/hooks/queries/useSearchProductListInfiniteQuery";
 import ProductListUI from "../../product/shared/components/product-list/ProductListUI";
 import { useGetQuerys } from "@/shared/common/hooks/useGetQuerys";
+import Empty from "@/shared/common/components/Empty";
 
 export default function ProductSearchList() {
   const { keyword } = useGetQuerys("keyword");
   const { data, isFetchingNextPage, fetchNextPage, hasNextPage } =
     useSearchProductListInfiniteQuery();
+
+  if (data.length === 0) {
+    return (
+      <div className="max-w-[1024px] mx-auto pb-12 px-8 mt-6">
+        <Empty message={`"${keyword}"에 대한 검색결과가 없어요.`} />
+      </div>
+    );
+  }
 
   return (
     <ProductListUI
@@ -15,7 +24,6 @@ export default function ProductSearchList() {
       isFetchingNextPage={isFetchingNextPage}
       fetchNextPage={fetchNextPage}
       hasNextPage={hasNextPage}
-      emptyMessage={`"${keyword}"에 대한 검색결과가 없어요.`}
     />
   );
 }
