289f79f1bfaa800a899dc58632b7442fed4cd3d7
MAIN6419 <dark9737@gmail.com>
2026-01-30 18:38:11 +0900

    Fix: CategoryProductList 컴포넌트 빈 데이터 처리 제거 - CategoryFirst 컴포넌트에 이미 빈 데이터를 처리하기 때문에 중복 처리 제거

--- domains/product/category/CategoryProductList.tsx
+++ domains/product/category/CategoryProductList.tsx
@@ -3,7 +3,6 @@
 import useCategoryProductListInfiniteQuery from "../shared/hooks/queries/useCategoryProductListInfiniteQuery";
 import { ProductCategory } from "../shared/types/productTypes";
 import ProductListUI from "../shared/components/product-list/ProductListUI";
-import Empty from "@/shared/common/components/Empty";
 
 interface IProps {
   category: ProductCategory;
@@ -16,14 +15,6 @@ export default function CategoryProductList({ category, nextCursor }: IProps) {
       category: category ? (category as ProductCategory) : ProductCategory.전체,
       nextCursor
     });
-
-  if (data.length === 0) {
-    return (
-      <div className="max-w-[1024px] mx-auto pb-12 px-8 mt-6">
-        <Empty message="상품이 존재하지 않아요." />
-      </div>
-    );
-  }
 
   return (
     <ProductListUI
