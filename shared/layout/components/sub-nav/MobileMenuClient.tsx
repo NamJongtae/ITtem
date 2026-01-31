6bc91494d84c84d0919bb6dc90c573fecf487888
MAIN6419 <dark9737@gmail.com>
2026-01-31 16:51:50 +0900

    Fix: MobileMenuClient 컴포넌트  MobileMenu dynamic import loading 추가

--- shared/layout/components/sub-nav/MobileMenuClient.tsx
+++ shared/layout/components/sub-nav/MobileMenuClient.tsx
@@ -2,7 +2,12 @@
 
 import dynamic from "next/dynamic";
 
-const MobileMenu = dynamic(() => import("./MobileMenu"), { ssr: false });
+const MobileMenu = dynamic(() => import("./MobileMenu"), {
+  ssr: false,
+  loading: () => (
+    <div className="z-30 md:hidden fixed bottom-0 w-full h-16 p-2 bg-gray-200 shadow-[0_-2px_4px_0_rgba(33,37,41,.08)]" />
+  )
+});
 
 export default function MobileMenuClient() {
   return <MobileMenu />;
