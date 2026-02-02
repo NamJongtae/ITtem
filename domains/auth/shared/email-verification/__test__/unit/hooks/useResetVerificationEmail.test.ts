910d4ab5ebc87d0cde683ee5c9607ab00b987a6e
MAIN6419 <dark9737@gmail.com>
2026-02-02 16:32:45 +0900

    Test: useResetVerificationEmail 테스트 인증 초기화 로직 변경에 따른 테스트 수정

--- domains/auth/shared/email-verification/__test__/unit/hooks/useResetVerificationEmail.test.ts
+++ domains/auth/shared/email-verification/__test__/unit/hooks/useResetVerificationEmail.test.ts
@@ -1,30 +1,44 @@
 import { renderHook, act } from "@testing-library/react";
 import { useContext } from "react";
 import useResetVerificationEmail from "../../../hooks/useResetVerificationEmail";
+import { useFormContext } from "react-hook-form";
 
 jest.mock("react", () => ({
   ...jest.requireActual("react"),
   useContext: jest.fn()
 }));
 
-describe("useResetVerificationEmail.test 훅 테스트", () => {
-  const mockSetEmailStatus = jest.fn();
+jest.mock("react-hook-form", () => ({
+  useFormContext: jest.fn()
+}));
+
+describe("useResetVerificationEmail 훅 테스트", () => {
+  const mockReset = jest.fn();
+  const mockClearErrors = jest.fn();
+
   const mockUseContext = useContext as jest.Mock;
+  const mockUseFormContext = useFormContext as jest.Mock;
 
   beforeEach(() => {
     jest.clearAllMocks();
+
     mockUseContext.mockReturnValue({
-      setEmailStatus: mockSetEmailStatus
+      reset: mockReset
+    });
+
+    mockUseFormContext.mockReturnValue({
+      clearErrors: mockClearErrors
     });
   });
 
-  it("resetSendToVerificationEmail: setEmailStatus 함수가 호출되어야 합니다.", async () => {
+  it("resetSendToVerificationEmail 호출 시 context.reset과 clearErrors('verificationCode')가 호출된다", () => {
     const { result } = renderHook(() => useResetVerificationEmail());
 
-    await act(async () => {
+    act(() => {
       result.current.resetSendToVerificationEmail();
     });
 
-    expect(mockSetEmailStatus).toHaveBeenCalledWith("INITIAL");
+    expect(mockReset).toHaveBeenCalledTimes(1);
+    expect(mockClearErrors).toHaveBeenCalledWith("verificationCode");
   });
 });
