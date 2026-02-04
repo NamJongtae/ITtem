f1324773f9ab31b485695550af394dba5aa5c06b
NamJongtae <dark9737@gmail.com>
2026-02-04 17:45:21 +0900

    Fix: BASE_URL 기본값 추가

--- shared/common/constants/constant.ts
+++ shared/common/constants/constant.ts
@@ -1,6 +1,5 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
 
 export const ERROR_MESSAGE =
   "알 수 없는 에러가 발생했어요.\n잠시 후 다시 시도해주세요.";
-
-
