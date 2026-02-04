c884718323fa0b492b1267fe919a27e2752362cc
NamJongtae <dark9737@gmail.com>
2026-02-04 17:03:51 +0900

    Feat: BASE_METADATA constant 생성

--- domains/auth/shared/common/constants/constansts.ts
+++ domains/auth/shared/common/constants/constansts.ts
@@ -1,3 +1,6 @@
+import { BASE_URL } from "@/shared/common/constants/constant";
+import { Metadata } from "next";
+
 export const EMAIL_REGEX = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 export const EMAIL_REGEX_ERRORMSG = "이메일 형식을 확인해주세요.";
 
@@ -19,3 +22,28 @@ export const VERIFICATION_EMAIL_EXP = 60 * 5;
 export const VERIFIED_EMAIL_EXP = 60 * 30;
 
 export const SESSION_TTL = 60 * 60 * 24 * 7 * 1000;
+
+export const BASE_METADATA: Metadata = {
+  title: "ITtem",
+  metadataBase: new URL(BASE_URL),
+  description:
+    "중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요.",
+  verification: {
+    google: "c6hLQ83ILZIlJb0fwdyrw71DGHnTFaI7hmmORy7fpk0",
+    other: {
+      "naver-site-verification": "8a189164663805b9eeea137c43336f57fa7bdcf2"
+    }
+  },
+  icons: {
+    icon: "/icons/logo.svg"
+  },
+  openGraph: {
+    url: BASE_URL,
+    title: "ITtem",
+    description:
+      "중고거래 플랫폼, 잇템. 안전하고 쉬운 중고거래, 다양한 상품과 빠른 거래를 경험하세요.",
+      images: {
+        url:"/images/opengraph-image.png"
+      }
+  }
+};
